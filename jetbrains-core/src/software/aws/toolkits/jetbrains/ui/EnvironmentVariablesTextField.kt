52ea49535ba7a31859a39daaf7dedc0b1b496fcc
Michael Golubev <borlander@gmail.com>
2018-11-15 16:42:49 +0300

    #386 - Environment substitution for SAM Local run-config
    - support `protected` env vars in EnvironmentVariableTextField
    - - above the rest
    - - can't edit key
    - - can't be removed from table
    - - ignored in storage when empty
    + tests

--- jetbrains-core/src/software/aws/toolkits/jetbrains/ui/EnvironmentVariablesTextField.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/ui/EnvironmentVariablesTextField.kt
@@ -8,23 +8,30 @@ import com.intellij.execution.util.EnvVariablesTable
 import com.intellij.execution.util.EnvironmentVariable
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
+import org.jetbrains.annotations.TestOnly
 import software.aws.toolkits.resources.message
 import java.awt.Component
-import java.util.LinkedHashMap
+import java.util.*
 import javax.swing.JComponent
 
 /**
  * Our version of [com.intellij.execution.configuration.EnvironmentVariablesTextFieldWithBrowseButton] to fit our
- * needs but with same UX so users are used to it. Namely we do not support inheriting system env vars, but rest
- * of UX is the same
+ * needs but with similar UX so users are used to it. Namely we
+ * <ul>
+ * <li>do not support inheriting system env vars</li>
+ * <li>have an optional notion of "protected" variables, which have read-only names, can't be removed and should be shown above the others</li>
+ * </ul>
+ * When there are no protection, UX is the same
  */
-class EnvironmentVariablesTextField : TextFieldWithBrowseButton() {
+open class EnvironmentVariablesTextField : TextFieldWithBrowseButton() {
+    private var protectedVarNames = emptyList<String>()
+
     private var data = EnvironmentVariablesData.create(emptyMap(), false)
     var envVars: Map<String, String>
         get() = data.envs
         set(value) {
             data = EnvironmentVariablesData.create(value, false)
-            text = stringify(data.envs)
+            text = stringify()
         }
 
     init {
@@ -34,31 +41,50 @@ class EnvironmentVariablesTextField : TextFieldWithBrowseButton() {
         }
     }
 
-    private fun convertToVariables(envVars: Map<String, String>, readOnly: Boolean): List<EnvironmentVariable> = envVars.map { (key, value) ->
-        object : EnvironmentVariable(key, value, readOnly) {
-            override fun getNameIsWriteable(): Boolean = !readOnly
-        }
+    fun protectVariables(varNames: List<String>) {
+        protectedVarNames = varNames
+        text = stringify()
     }
 
-    private fun stringify(envVars: Map<String, String>): String {
-        if (envVars.isEmpty()) {
-            return ""
-        }
+    private fun isProtectedVar(varName: String) = protectedVarNames.contains(varName)
 
+    private fun protectedEntries(): List<Pair<String, String>> =
+            protectedVarNames.map { name -> name to envVars.getOrDefault(name, "") }
+
+    private fun unprotectedEntries(): List<Pair<String, String>> =
+            envVars.filter { (name, _) -> !isProtectedVar(name) }
+                    .map { (name, value) -> name to value }
+
+    fun convertToVariables(): List<EnvironmentVariable> =
+            protectedEntries().map { (name, value) -> VariableWithProtection(name, value, true) } +
+                    unprotectedEntries().map { (name, value) -> VariableWithProtection(name, value, false) }
+
+    private fun stringify(): String {
         val buf = StringBuilder()
-        for ((key, value) in envVars) {
-            if (buf.isNotEmpty()) {
-                buf.append(";")
-            }
-            buf.append(key).append("=").append(value)
-        }
 
-        return buf.toString()
+        val entries = protectedEntries().filter { (_, value) -> value.isNotBlank() } + unprotectedEntries()
+        return entries.joinTo(buf, ";") { (key, value) -> "$key=$value" }
+                .toString()
     }
+
+    private fun acceptEditedVariables(editedVariables: List<EnvironmentVariable>) {
+        val newEnvVars = LinkedHashMap<String, String>()
+        editedVariables
+                .filter { it -> !isProtectedVar(it.name) || it.value.isNotBlank() }
+                .forEach { it -> newEnvVars[it.name] = it.value.trim() }
+
+        envVars = newEnvVars
+    }
+
+    @TestOnly
+    fun acceptEditedVariablesForTesting(testVariables: List<EnvironmentVariable>) =
+            acceptEditedVariables(testVariables)
+
+    protected fun createDialogTable(): EnvVariablesTable = EnvVariablesTable()
 
     private inner class EnvironmentVariablesDialog(private val parent: Component) : DialogWrapper(parent, true) {
-        private val envVarTable = EnvVariablesTable().apply {
-            setValues(convertToVariables(data.envs, false))
+        private val envVarTable = createDialogTable().apply {
+            setValues(convertToVariables())
         }
 
         init {
@@ -70,12 +96,16 @@ class EnvironmentVariablesTextField : TextFieldWithBrowseButton() {
 
         override fun doOKAction() {
             envVarTable.stopEditing()
-            val newEnvVars = LinkedHashMap<String, String>()
-            for (variable in envVarTable.environmentVariables) {
-                newEnvVars[variable.name] = variable.value
-            }
-            envVars = newEnvVars
+            acceptEditedVariables(envVarTable.environmentVariables)
             super.doOKAction()
         }
+    }
+}
+
+private class VariableWithProtection(name: String, value: String, val nameProtected: Boolean)
+    : EnvironmentVariable(name, value, nameProtected) {
+
+    override fun getNameIsWriteable(): Boolean {
+        return !nameProtected
     }
 }
---
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/ui/EnvironmentVariablesTextFieldTest.kt
@@ -0,0 +1,114 @@
+package software.aws.toolkits.jetbrains.ui
+
+import com.intellij.execution.util.EnvironmentVariable
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Test
+
+class EnvironmentVariablesTextFieldTest {
+
+    @Test
+    fun protectedVars() {
+        val field = EnvironmentVariablesTextField()
+        field.protectVariables(listOf("A", "B"))
+        field.envVars = mapOf("xx" to "1", "yy" to "2")
+
+        val shownInTable: List<EnvironmentVariable> = field.convertToVariables()
+        assertThat(shownInTable).hasSize(4)
+        assertThat(shownInTable.asKeyValueMap())
+                .isEqualTo(mapOf("A" to "", "B" to "", "xx" to "1", "yy" to "2"))
+
+        val a = shownInTable.require("A")
+        val b = shownInTable.require("B")
+
+        assertThat(a.nameIsWriteable).isFalse()
+        assertThat(b.nameIsWriteable).isFalse()
+
+        //can't be removed
+        assertThat(a.isPredefined).isTrue()
+        assertThat(b.isPredefined).isTrue()
+
+        assertThat(shownInTable.require("xx").nameIsWriteable).isTrue()
+        assertThat(shownInTable.require("yy").isPredefined).isFalse()
+    }
+
+    @Test
+    fun protectedVarsWithData() {
+        val field = EnvironmentVariablesTextField()
+        field.protectVariables(listOf("A", "B"))
+        field.envVars = mapOf("X" to "xx", "B" to "bb")
+
+        val shownInTable = field.convertToVariables()
+        assertThat(shownInTable).hasSize(3)
+        assertThat(shownInTable.asKeyValueMap())
+                .isEqualTo(mapOf("A" to "", "B" to "bb", "X" to "xx"))
+    }
+
+    @Test
+    fun thatEmptyProtectedVarsExcludedFromStringify() {
+        val field = EnvironmentVariablesTextField()
+
+        field.protectVariables(listOf("AA", "BB", "CC"))
+        field.envVars = mapOf("AA" to "aa", "CC" to "", "XX" to "xx")
+
+        assertThat(field.text).isEqualTo("AA=aa;XX=xx")
+    }
+
+    @Test
+    fun thatStringifyDoesNotDependOnCallOrder() {
+        val protectedNames = listOf("AA", "BB", "CC")
+        val data = mapOf("AA" to "aa", "CC" to "", "XX" to "xx")
+
+        val protectThanSet = EnvironmentVariablesTextField()
+        protectThanSet.protectVariables(protectedNames)
+        protectThanSet.envVars = data
+
+        val setThenProtect = EnvironmentVariablesTextField()
+        setThenProtect.envVars = data
+        setThenProtect.protectVariables(protectedNames)
+    }
+
+    @Test
+    fun testAfterEditing() {
+        val protectedNames = listOf("<clear>", "<edit>", "<empty>")
+        val data = mapOf(
+                "<clear>" to "AA",
+                "<edit>" to "AA",
+                "<unprotected-edit>" to "AA",
+                "<unprotected-remove>" to "AA",
+                "<unprotected-clear>" to "AA"
+        )
+
+        val field = EnvironmentVariablesTextField()
+        field.envVars = data
+        field.protectVariables(protectedNames)
+
+        val vars = field.convertToVariables()
+
+        vars.require("<clear>").value = ""
+        vars.require("<edit>").value = "BB"
+
+        vars.require("<unprotected-edit>").value = "BB"
+        vars.require("<unprotected-clear>").value = ""
+
+        field.acceptEditedVariablesForTesting(vars.filter { it -> it.name != "<unprotected-remove>" })
+
+        assertThat(field.text).doesNotContain("<clear>=") //cleared protected values considered as kinda removed
+        assertThat(field.text).contains("<unprotected-clear>=")
+
+        assertThat(field.text).isEqualTo("<edit>=BB;<unprotected-edit>=BB;<unprotected-clear>=")
+        assertThat(field.envVars).isEqualTo(mapOf(
+                "<edit>" to "BB",
+                "<unprotected-edit>" to "BB",
+                "<unprotected-clear>" to ""))
+    }
+
+    companion object {
+        private fun List<EnvironmentVariable>.asKeyValueMap(): Map<String, String> =
+                this.associate { it -> it.name to it.value }
+
+        private fun List<EnvironmentVariable>.require(name: String): EnvironmentVariable =
+                this.first { it -> it.name == name }
+
+    }
+
+}
