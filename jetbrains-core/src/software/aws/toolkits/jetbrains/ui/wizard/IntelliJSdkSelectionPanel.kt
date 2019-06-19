737c93d190a62948cc35e4a1a9debc07b40330bb
Zhaoxi Zhang <zhaoxiz@amazon.com>
2019-06-19 11:23:03 -0700

    Fix compile error after merging master

--- jetbrains-core/src/software/aws/toolkits/jetbrains/ui/wizard/IntelliJSdkSelectionPanel.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/ui/wizard/IntelliJSdkSelectionPanel.kt
@@ -43,7 +43,7 @@ class IntelliJSdkSelectionPanel(val builder: SamProjectBuilder, val runtimeGroup
         return when (runtimeGroup) {
             RuntimeGroup.JAVA, RuntimeGroup.PYTHON -> SdkBasedSdkSettings(sdk = currentSdk)
             // TODO add this line when supporting node
-//            else -> throw RuntimeException("Unrecognized runtime group: $runtimeGroup")
+            else -> throw RuntimeException("Unrecognized runtime group: $runtimeGroup")
         }
     }
 
