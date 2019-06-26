c40f5b6197dae8253ac38b3e7744aa5adc564332
Austin Brooks <alfredbr@amazon.com>
2019-06-26 14:05:39 -0700

    Move message to localization

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/python/PythonLambdaBuilder.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/python/PythonLambdaBuilder.kt
@@ -10,6 +10,7 @@ import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import software.aws.toolkits.jetbrains.services.lambda.LambdaBuilder
+import software.aws.toolkits.resources.message
 
 class PythonLambdaBuilder : LambdaBuilder() {
     override fun baseDirectory(module: Module, handlerElement: PsiElement): String {
@@ -39,6 +40,6 @@ class PythonLambdaBuilder : LambdaBuilder() {
             }
         }
 
-        throw IllegalStateException("Failed to locate the root of the handler")
+        throw IllegalStateException(message("lambda.build.unable_to_locate_handler_root"))
     }
 }
