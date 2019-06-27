ce5ec2e07a9596c188417419fb3b7f08b477b41b
Zhaoxi Zhang <zhangzhx@users.noreply.github.com>
2019-06-27 11:30:30 -0700

    Fix PyCharm crashes for not returning the correct nodejs module. (#1067)

--- jetbrains-ultimate/src/software/aws/toolkits/jetbrains/services/lambda/nodejs/NodeJsRuntimeGroup.kt
+++ jetbrains-ultimate/src/software/aws/toolkits/jetbrains/services/lambda/nodejs/NodeJsRuntimeGroup.kt
@@ -8,9 +8,12 @@ import com.intellij.lang.javascript.JavaScriptSupportLoader
 import com.intellij.lang.javascript.JavascriptLanguage
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleType
+import com.intellij.openapi.module.ModuleTypeId.WEB_MODULE
+import com.intellij.openapi.module.ModuleTypeManager
 import com.intellij.openapi.module.WebModuleTypeBase
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.webcore.moduleType.WebModuleTypeManager
 import software.amazon.awssdk.services.lambda.model.Runtime
 import software.aws.toolkits.jetbrains.services.lambda.SdkBasedRuntimeGroupInformation
 
@@ -41,7 +44,10 @@ class NodeJsRuntimeGroup : SdkBasedRuntimeGroupInformation() {
      */
     override fun runtimeForSdk(sdk: Sdk): Runtime? = null
 
-    override fun getModuleType(): ModuleType<*>? = WebModuleTypeBase.getInstance()
+    // TODO: FIX_WHEN_MIN_IS_192 WebModuleType.getInstance() is deprecated in 191, use WebModuleTypeBase#getInstance for version 191+
+    override fun getModuleType(): ModuleType<*>? =
+        ModuleTypeManager.getInstance().findByID(WEB_MODULE) as? WebModuleTypeBase
+            ?: WebModuleTypeManager.getInstance().defaultModuleType
 
     override fun supportsSamBuild(): Boolean = true
 }
