b3df767bda57765ad00a881eeeea287e8a06a4ee
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2019-07-22 16:18:51 -0700

    Fixed indentation issues in S3Virtual Fields

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/S3VirtualFields.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/S3VirtualFields.kt
@@ -1,3 +1,5 @@
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 package software.aws.toolkits.jetbrains.services.s3
 
 import com.intellij.openapi.vfs.VirtualFile
@@ -6,11 +8,11 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.time.Instant
 
-//base class to represent a virtual file
+// base class to represent a virtual file
 abstract class BaseS3VirtualFile(
-        val fileSystem: S3VFS,
-        private val parent: VirtualFile?,
-        open val key: S3Key
+    val fileSystem: S3VFS,
+    private val parent: VirtualFile?,
+    open val key: S3Key
 ) : VirtualFile() {
     override fun getName(): String = key.key
 
@@ -26,25 +28,25 @@ abstract class BaseS3VirtualFile(
     override fun getFileSystem(): VirtualFileSystem = fileSystem
     override fun getLength(): Long = 0
 
-    override fun getTimeStamp(): Long = 0 //not sure what exactly is it?
+    override fun getTimeStamp(): Long = 0
 
     override fun contentsToByteArray(): ByteArray {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getInputStream(): InputStream {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getOutputStream(requestor: Any?, newModificationStamp: Long, newTimeStamp: Long): OutputStream {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 }
 
 class S3VirtualFile(
-        s3Vfs: S3VFS,
-        val file: S3Object,
-        parent: VirtualFile
+    s3Vfs: S3VFS,
+    val file: S3Object,
+    parent: VirtualFile
 ) : BaseS3VirtualFile(s3Vfs, parent, file) {
 
     override fun isDirectory(): Boolean = false
@@ -56,23 +58,23 @@ class S3VirtualFile(
     override fun getTimeStamp(): Long = file.lastModified.toEpochMilli()
 
     override fun contentsToByteArray(): ByteArray {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getInputStream(): InputStream {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getOutputStream(requestor: Any?, newModificationStamp: Long, newTimeStamp: Long): OutputStream {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun refresh(asynchronous: Boolean, recursive: Boolean, postRunnable: Runnable?) {}
 }
 
 open class S3VirtualBucket(
-        fileSystem: S3VFS,
-        val s3Bucket: S3Bucket
+    fileSystem: S3VFS,
+    val s3Bucket: S3Bucket
 ) : BaseS3VirtualFile(fileSystem, parent = null, key = s3Bucket) {
 
     override fun getTimeStamp(): Long = s3Bucket.creationDate.toEpochMilli()
@@ -84,38 +86,31 @@ open class S3VirtualBucket(
     override fun refresh(asynchronous: Boolean, recursive: Boolean, postRunnable: Runnable?) {}
 
     override fun getChildren(): Array<VirtualFile> =
-            s3Bucket.children()!!.sortedBy { it.name }
-                    .map {
-                        when (it) {
-                            is S3Object -> S3VirtualFile(fileSystem, it, this)
-                            is S3Directory -> S3VirtualDirectory(fileSystem, it, this)
-
-                        }
-                    }.toTypedArray()
+        s3Bucket.children()!!.sortedBy { it.name }
+            .map {
+                when (it) {
+                    is S3Object -> S3VirtualFile(fileSystem, it, this)
+                    is S3Directory -> S3VirtualDirectory(fileSystem, it, this)
+                }
+            }.toTypedArray()
 
     override fun isDirectory(): Boolean = true
-
 }
 
 class S3VirtualDirectory(
-        s3filesystem: S3VFS,
-        private val directory: S3Directory,
-        parent: VirtualFile
-
+    s3filesystem: S3VFS,
+    private val directory: S3Directory,
+    parent: VirtualFile
 ) : BaseS3VirtualFile(s3filesystem, parent, directory) {
-    override fun getChildren(): Array<VirtualFile> {
+    override fun getChildren(): Array<VirtualFile> =
+        directory.children()!!.sortedBy { it.name }.filterNot { it.key == directory.key }
+            .map {
+                when (it) {
+                    is S3Object -> S3VirtualFile(fileSystem, it, this)
+                    is S3Directory -> S3VirtualDirectory(fileSystem, it, this)
+                }
+            }.toTypedArray()
 
-        return directory.children()!!.sortedBy { it.name }.filterNot { it.key == directory.key }
-                .map {
-                    when (it) {
-                        is S3Object -> S3VirtualFile(fileSystem, it, this)
-                        is S3Directory -> S3VirtualDirectory(fileSystem, it, this)
-
-                    }
-                }.toTypedArray()
-    }
-
-    override fun refresh(asynchronous: Boolean, recursive: Boolean, postRunnable: Runnable?){}
+    override fun refresh(asynchronous: Boolean, recursive: Boolean, postRunnable: Runnable?) {}
     override fun isDirectory(): Boolean = true
-
 }
