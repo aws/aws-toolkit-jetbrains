be5f663e6ae0ac072a696e3ed85acd58d9f57671
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2019-07-22 16:25:04 -0700

    Added service for S3

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerService.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/AwsExplorerService.kt
@@ -7,8 +7,10 @@ import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.Project
 import software.amazon.awssdk.services.cloudformation.CloudFormationClient
 import software.amazon.awssdk.services.lambda.LambdaClient
+import software.amazon.awssdk.services.s3.S3Client
 import software.aws.toolkits.jetbrains.services.cloudformation.CloudFormationServiceNode
 import software.aws.toolkits.jetbrains.services.lambda.LambdaServiceNode
+import software.aws.toolkits.jetbrains.services.s3.S3ServiceNode
 
 enum class AwsExplorerService(val serviceId: String) {
     CLOUDFORMATION(CloudFormationClient.SERVICE_NAME) {
@@ -17,6 +19,10 @@ enum class AwsExplorerService(val serviceId: String) {
     LAMBDA(LambdaClient.SERVICE_NAME) {
         override fun buildServiceRootNode(project: Project): LambdaServiceNode = LambdaServiceNode(project)
     },
+    S3(S3Client.SERVICE_NAME) {
+        override fun buildServiceRootNode(project: Project): S3ServiceNode = S3ServiceNode(project)
+    },
+
     ;
 
     abstract fun buildServiceRootNode(project: Project): AbstractTreeNode<String>
