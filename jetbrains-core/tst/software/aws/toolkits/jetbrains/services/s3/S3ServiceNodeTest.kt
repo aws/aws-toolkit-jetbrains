07cb32c8a32ffe969b3fd98a1a5c4b714da25ac2
Shruti Sinha <44882001+shruti0085@users.noreply.github.com>
2019-07-29 12:04:12 -0700

    Added couple more test to ServiceNode

--- jetbrains-core/tst/software/aws/toolkits/jetbrains/services/s3/S3ServiceNodeTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/services/s3/S3ServiceNodeTest.kt
@@ -3,22 +3,19 @@
 package software.aws.toolkits.jetbrains.services.s3
 
 import com.intellij.testFramework.ProjectRule
+import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.stub
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Rule
-import com.nhaarman.mockitokotlin2.any
 import org.junit.Test
 import software.amazon.awssdk.http.SdkHttpResponse
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.services.s3.model.HeadBucketRequest
-import software.amazon.awssdk.services.s3.model.HeadBucketResponse
-import software.amazon.awssdk.services.s3.model.ListBucketsRequest
-import software.amazon.awssdk.services.s3.model.ListBucketsResponse
-import software.amazon.awssdk.services.s3.model.Bucket
-
+import software.amazon.awssdk.services.s3.model.*
 import software.aws.toolkits.jetbrains.core.MockClientManagerRule
+import software.aws.toolkits.jetbrains.core.explorer.nodes.AwsExplorerEmptyNode
+import software.aws.toolkits.jetbrains.core.explorer.nodes.AwsExplorerErrorNode
 import software.aws.toolkits.jetbrains.utils.delegateMock
 import java.time.Instant
 
@@ -36,24 +33,33 @@ class S3ServiceNodeTest {
 
     @Test
     fun s3BucketsSortedAlphabetically() {
-
-        val mockSdkResponse = mock<SdkHttpResponse>()
-        mockSdkResponse.stub {
-            on { headers() } doReturn (mapOf("x-amz-bucket-region" to listOf("us-east-1")))
-        }
-
+        createMocks()
         mockClient.stub {
-            on { headBucket(any<HeadBucketRequest>()) } doReturn (HeadBucketResponse.builder().sdkHttpResponse(mockSdkResponse).build() as HeadBucketResponse)
-        }
-
-        mockClient.stub {
-            on { listBuckets(any<ListBucketsRequest>()) } doReturn (ListBucketsResponse.builder().apply {
-                this.buckets(bucketData("BBB"), bucketData("AAA"), bucketData("ZZZ"))
-            }.build())
+            on { listBuckets(any<ListBucketsRequest>()) } doReturn ListBucketsResponse.builder()
+                .apply { this.buckets(bucketData("BBB"),
+                    bucketData("AAA"),
+                    bucketData("ZZZ")) }.build()
         }
         val children = S3ServiceNode(projectRule.project).children
+
         assertThat(children).allMatch { it is S3BucketNode }
         assertThat(children.map { it.displayName() }).containsExactly("AAA", "BBB", "ZZZ")
+    }
+
+    @Test
+    fun noBucketsInTheRegion() {
+        createMocks()
+        mockClient.stub { on {
+            listBuckets(any<ListBucketsRequest>()) } doReturn ListBucketsResponse.builder().build() }
+        val children = S3ServiceNode(projectRule.project).children
+        assertThat(children).allMatch{it is AwsExplorerEmptyNode}
+    }
+
+    @Test
+    fun errorLoadingBuckets() {
+        createMocks()
+        val children = S3ServiceNode(projectRule.project).children
+        assertThat(children).allMatch{it is AwsExplorerErrorNode}
     }
 
     private fun bucketData(bucketName: String) =
@@ -61,4 +67,15 @@ class S3ServiceNodeTest {
                     .creationDate(Instant.parse("1995-10-23T10:12:35Z"))
                     .name(bucketName)
                     .build()
+
+    private fun createMocks(){
+        val mockSdkResponse = mock<SdkHttpResponse>()
+        mockSdkResponse.stub {
+            on { headers() } doReturn mapOf("x-amz-bucket-region" to listOf("us-east-1"))
+        }
+
+        mockClient.stub {
+            on { headBucket(any<HeadBucketRequest>()) } doReturn HeadBucketResponse.builder()
+                .sdkHttpResponse(mockSdkResponse).build() as HeadBucketResponse }
+    }
 }
