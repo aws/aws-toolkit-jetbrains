19febfb4e14c15c0624b106e938f2a5918997114
Austin Brooks <alfredbr@amazon.com>
2019-07-31 13:34:16 -0700

    Move Copy Arn to localized file

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/actions/CopyArn.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/actions/CopyArn.kt
@@ -6,9 +6,10 @@ package software.aws.toolkits.jetbrains.core.explorer.actions
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ide.CopyPasteManager
+import software.aws.toolkits.resources.message
 import java.awt.datatransfer.StringSelection
 
-class CopyArn(private val arn: String) : AnAction("Copy ARN") {
+class CopyArn(private val arn: String) : AnAction(message("explorer.copy_arn")) {
     override fun actionPerformed(e: AnActionEvent) {
         CopyPasteManager.getInstance().setContents(StringSelection(arn))
     }
