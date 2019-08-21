eeffc5e3d31dafb8d36af00028bd3607e14fa9a1
shruti0085 <shruti85@seas.upenn.edu>
2019-08-21 12:00:38 -0700

    Added top-level search action and refactored bucket icons

--- jetbrains-core/tst/software/aws/toolkits/jetbrains/services/s3/UploadObjectTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/services/s3/UploadObjectTest.kt
@@ -3,11 +3,7 @@
 package software.aws.toolkits.jetbrains.services.s3
 
 import com.intellij.openapi.vfs.VirtualFile
-import com.nhaarman.mockitokotlin2.stub
-import com.nhaarman.mockitokotlin2.argumentCaptor
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.*
 import org.assertj.core.api.Assertions
 import org.junit.Rule
 import org.junit.Test
@@ -22,6 +18,8 @@ import software.aws.toolkits.jetbrains.utils.delegateMock
 import software.aws.toolkits.jetbrains.utils.rules.JavaCodeInsightTestFixtureRule
 import java.io.ByteArrayInputStream
 import java.time.Instant
+import javax.swing.JButton
+import javax.swing.JTextField
 
 class UploadObjectTest {
 
@@ -49,12 +47,14 @@ class UploadObjectTest {
         val virtualBucket =
             S3VirtualBucket(fileSystemMock, S3Bucket("TestBucket", s3Client, Instant.parse("1995-10-23T10:12:35Z")))
         val treeTableMock = delegateMock<S3TreeTable>()
+        val mockButton = delegateMock<JButton>()
+        val mockTextField = delegateMock<JTextField>()
 
         val testFile = delegateMock<VirtualFile> { on { name } doReturn "TestFile" }
         testFile.stub { on { length } doReturn 341 }
         testFile.stub { on { inputStream } doReturn ByteArrayInputStream("Hello".toByteArray()) }
 
-        val uploadObjectMock = UploadObjectAction(virtualBucket, treeTableMock)
+        val uploadObjectMock = UploadObjectAction(virtualBucket, treeTableMock, mockButton, mockTextField)
 
         uploadObjectMock.uploadObjectAction(s3Client, projectRule.project, testFile, null)
         verify(s3Client).putObject(any<PutObjectRequest>(), any<RequestBody>())
