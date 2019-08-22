282c77f451f77a4ee5b58bdac0876bd433b32b95
shruti0085 <shruti85@seas.upenn.edu>
2019-08-21 17:51:59 -0700

    Added S3ColumnInfo

---
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/bucketEditor/S3ColumnInfo.kt
@@ -0,0 +1,42 @@
+package software.aws.toolkits.jetbrains.services.s3.bucketEditor
+
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.treeStructure.treetable.TreeTableModel
+import com.intellij.util.ui.ColumnInfo
+import software.aws.toolkits.jetbrains.services.s3.S3VirtualDirectory
+import software.aws.toolkits.jetbrains.services.s3.S3VirtualFile
+import javax.swing.tree.DefaultMutableTreeNode
+
+open class S3ColumnInfo(columnTitle: String, private val valueGetter: (S3VirtualFile) -> String?) :
+    ColumnInfo<Object, String>(columnTitle) {
+
+    override fun valueOf(obj: Object): String? {
+        val file = getVirtualFileFromNode(obj)
+        return when (file) {
+            is S3VirtualFile -> valueGetter.invoke(file)
+            else -> ""
+        }
+    }
+
+    override fun isCellEditable(item: Object?): Boolean = true
+
+    fun getVirtualFileFromNode(obj: Object): VirtualFile? {
+        val userObject = (obj as? DefaultMutableTreeNode)?.userObject
+        return (userObject as? S3KeyNode)?.virtualFile
+    }
+}
+
+class S3KeyColumnInfo(columnTitle: String, private val valueGetter: (S3VirtualFile) -> String?) :
+    S3ColumnInfo(columnTitle, valueGetter) {
+
+    override fun valueOf(obj: Object): String? {
+        val file = super.getVirtualFileFromNode(obj)
+        return when (file) {
+            is S3VirtualFile -> valueGetter.invoke(file)
+            is S3VirtualDirectory -> file.name
+            else -> ""
+        }
+    }
+
+    override fun getColumnClass(): Class<*> = TreeTableModel::class.java
+}
