35917199c555f423cc889da55d9d77e25e84d3e6
Austin Brooks <abrooksv@users.noreply.github.com>
2019-10-17 10:42:21 -0700

    Update jetbrains-core/src/software/aws/toolkits/jetbrains/utils/CompatibilityUtils.kt
    Co-Authored-By: justinmk3 <55561878+justinmk3@users.noreply.github.com>

--- jetbrains-core/src/software/aws/toolkits/jetbrains/utils/CompatibilityUtils.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/utils/CompatibilityUtils.kt
@@ -20,7 +20,7 @@ import java.nio.file.Path
  */
 object CompatibilityUtils {
     /**
-     * Can be removed when min-version is 19.1 FIX_WHEN_MIN_IS_193
+     * Can be removed when min-version is 19.3 FIX_WHEN_MIN_IS_193
      */
     @TestOnly
     fun <T> registerExtension(name: ExtensionPointName<T>, extension: T, disposable: Disposable) {
