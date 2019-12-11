c9f7f0702f7c60c0c64a51d8eb80aa316cfc066c
Hunter Werlla <werlla@amazon.com>
2019-12-11 15:39:06 -0800

    PR comments

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/bucketEditor/S3TreeTable.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/bucketEditor/S3TreeTable.kt
@@ -11,6 +11,8 @@ import software.amazon.awssdk.services.s3.S3Client
 import software.aws.toolkits.core.utils.getLogger
 import software.aws.toolkits.jetbrains.services.s3.S3VirtualBucket
 import software.aws.toolkits.jetbrains.services.s3.objectActions.UploadObjectAction
+import software.aws.toolkits.jetbrains.utils.notifyError
+import software.aws.toolkits.resources.message
 import java.awt.datatransfer.DataFlavor
 import java.awt.datatransfer.UnsupportedFlavorException
 import java.awt.dnd.DnDConstants
@@ -27,16 +29,38 @@ class S3TreeTable(
     private val bucketVirtual: S3VirtualBucket,
     private val project: Project,
     private val s3Client: S3Client
-) :
-    TreeTable(treeTableModel) {
-    init {
-        dropTarget = createDropTarget()
-    }
+) : TreeTable(treeTableModel) {
 
-    fun refresh() {
-        runInEdt {
-            clearSelection()
-            treeTableModel.structureTreeModel.invalidate()
+    private val dropTargetListener = object : DropTargetAdapter() {
+        override fun drop(dropEvent: DropTargetDropEvent) {
+            val row = rowAtPoint(dropEvent.location).takeIf { it >= 0 } ?: return
+            val node = getNodeForRow(row) ?: return
+            val data = try {
+                dropEvent.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE)
+                dropEvent.transferable.getTransferData(DataFlavor.javaFileListFlavor) as List<File>
+            } catch (e: UnsupportedFlavorException) {
+                // When the drag and drop data is not what we expect (like when it is text) this is thrown and can be safey ignored
+                LOG.info("Unsupported flavor attempted to be dragged and dropped", e)
+                return
+            }
+
+            val lfs = LocalFileSystem.getInstance()
+            val virtualFiles = data.mapNotNull {
+                lfs.findFileByIoFile(it)
+            }
+
+            ApplicationManager.getApplication().executeOnPooledThread {
+                val action = UploadObjectAction(bucketVirtual, this@S3TreeTable)
+                virtualFiles.forEach {
+                    try {
+                        action.uploadObjectAction(s3Client, project, it, node)
+                        invalidateLevel(node)
+                        refresh()
+                    } catch (e: Exception) {
+                        e.notifyError(message("s3.upload.object.failed"))
+                    }
+                }
+            }
         }
     }
 
@@ -57,6 +81,18 @@ class S3TreeTable(
         ApplicationManager.getApplication().executeOnPooledThread {
             parent.loadMore(continuationNode.token)
             refresh()
+        }
+    }
+
+    init {
+        // Associate the drop target listener with this instance which will allow uploading by drag and drop
+        DropTarget(this, dropTargetListener)
+    }
+
+    fun refresh() {
+        runInEdt {
+            clearSelection()
+            treeTableModel.structureTreeModel.invalidate()
         }
     }
 
@@ -84,49 +120,6 @@ class S3TreeTable(
 
     fun invalidateLevel(node: S3TreeNode) {
         node.parent?.removeAllChildren()
-    }
-
-    private fun createDropTarget(): DropTarget {
-        val dropTarget = DropTarget()
-        try {
-            dropTarget.addDropTargetListener(object : DropTargetAdapter() {
-                override fun drop(dropEvent: DropTargetDropEvent) {
-                    try {
-                        dropEvent.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE)
-                        val data = dropEvent.transferable.getTransferData(DataFlavor.javaFileListFlavor) as List<File>
-                        val row = rowAtPoint(dropEvent.location).takeIf { it >= 0 } ?: return
-                        val node = getNodeForRow(row) ?: return
-
-                        val lfs = LocalFileSystem.getInstance()
-                        val virtualFiles = data.mapNotNull {
-                            lfs.findFileByIoFile(it)
-                        }
-
-                        ApplicationManager.getApplication().executeOnPooledThread {
-                            val action = UploadObjectAction(bucketVirtual, this@S3TreeTable)
-                            virtualFiles.forEach {
-                                try {
-                                    action.uploadObjectAction(s3Client, project, it, node)
-                                } catch (e: Exception) {
-                                    LOG.error("unable to upload to s3 from drag and drop", e)
-                                }
-                            }
-
-                            invalidateLevel(node)
-                            refresh()
-                        }
-                    } catch (e: UnsupportedFlavorException) {
-                        LOG.info("Unsupported flavor attempted to be dragged and dropped", e)
-                        // When the drag and drop data is not what we expect (like when it is text) this is thrown and can be safey ignored
-                    } catch (e: Exception) {
-                        LOG.error("Drag and drop threw", e)
-                    }
-                }
-            })
-        } catch (e: Exception) {
-            println(e)
-        }
-        return dropTarget
     }
 
     companion object {
