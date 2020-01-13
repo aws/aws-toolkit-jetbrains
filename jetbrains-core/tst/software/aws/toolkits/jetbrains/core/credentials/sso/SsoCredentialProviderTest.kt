a59476b9cb98c76f773d9d605f3353b14578c6eb
Austin Brooks <alfredbr@amazon.com>
2020-01-13 12:16:36 -0800

    Add tests for SSO credential provider

--- jetbrains-core/tst/software/aws/toolkits/jetbrains/core/credentials/sso/SsoCredentialProviderTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/core/credentials/sso/SsoCredentialProviderTest.kt
@@ -4,39 +4,66 @@
 package software.aws.toolkits.jetbrains.core.credentials.sso
 
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.stub
+import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials
 import software.amazon.awssdk.services.sso.SsoClient
 import software.amazon.awssdk.services.sso.model.GetRoleCredentialsRequest
 import software.amazon.awssdk.services.sso.model.GetRoleCredentialsResponse
 import software.amazon.awssdk.services.sso.model.RoleCredentials
 import software.aws.toolkits.jetbrains.utils.delegateMock
-import java.time.Clock
 import java.time.Instant
-import java.time.ZoneOffset
+import java.time.temporal.ChronoUnit
+
 
 class SsoCredentialProviderTest {
-    private val accessToken = "access123"
+    private val accessTokenId = "access123"
     private val accountId = "111222333444"
     private val roleName = "role123"
     private val accessKey = "accessKey"
     private val secretKey = "secretKey"
     private val sessionToken = "sessionToken"
-
-    private val clock = Clock.fixed(Instant.now(), ZoneOffset.UTC)
+    private val credentials = AwsSessionCredentials.create(accessKey, secretKey, sessionToken)
+    private val accessToken = AccessToken("ssoUrl", "ssoRegion", accessTokenId, Instant.now().plusSeconds(10))
 
     private lateinit var ssoClient: SsoClient
+    private lateinit var ssoAccessTokenProvider: SsoAccessTokenProvider
+    private lateinit var credProvider: SsoCredentialProvider
 
     @Before
     fun setUp() {
         ssoClient = delegateMock()
+        ssoAccessTokenProvider = mock() {
+            on(
+                it.accessToken()
+            ).thenReturn(
+                accessToken
+            )
+        }
+        credProvider = SsoCredentialProvider(accountId, roleName, ssoClient, ssoAccessTokenProvider)
     }
 
     @Test
     fun cachingDoesNotApplyToExpiredSession() {
         createSsoResponse(Instant.now().minusSeconds(10))
+
+        assertThat(credProvider.resolveCredentials()).usingRecursiveComparison().isEqualTo(credentials)
+        assertThat(credProvider.resolveCredentials()).usingRecursiveComparison().isEqualTo(credentials)
+
+        verify(ssoClient, times(2)).getRoleCredentials(any<GetRoleCredentialsRequest>())
+    }
+
+    @Test
+    fun cachingDoesApplyToExpiredSession() {
+        createSsoResponse(Instant.now().plus(2, ChronoUnit.HOURS))
+
+        assertThat(credProvider.resolveCredentials()).usingRecursiveComparison().isEqualTo(credentials)
+        assertThat(credProvider.resolveCredentials()).usingRecursiveComparison().isEqualTo(credentials)
 
         verify(ssoClient).getRoleCredentials(any<GetRoleCredentialsRequest>())
     }
@@ -46,7 +73,7 @@ class SsoCredentialProviderTest {
             on(
                 ssoClient.getRoleCredentials(
                     GetRoleCredentialsRequest.builder()
-                        .accessToken(accessToken)
+                        .accessToken(accessTokenId)
                         .accountId(accountId)
                         .roleName(roleName)
                         .build()
@@ -59,7 +86,8 @@ class SsoCredentialProviderTest {
                             .secretAccessKey(secretKey)
                             .sessionToken(sessionToken)
                             .expiration(expirationTime.toEpochMilli())
-                            .build())
+                            .build()
+                    )
                     .build()
             )
         }
