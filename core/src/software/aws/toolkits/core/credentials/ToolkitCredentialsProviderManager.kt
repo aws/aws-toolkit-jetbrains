493f54336fe031891f1e64ce8c2907a6af2a6b62
Austin Brooks <alfredbr@amazon.com>
2020-01-14 14:53:59 -0800

    Start laying out the newer API

--- core/src/software/aws/toolkits/core/credentials/ToolkitCredentialsProviderManager.kt
+++ core/src/software/aws/toolkits/core/credentials/ToolkitCredentialsProviderManager.kt
@@ -3,104 +3,10 @@
 
 package software.aws.toolkits.core.credentials
 
-import org.slf4j.LoggerFactory
-import software.aws.toolkits.core.utils.tryOrNull
-import software.aws.toolkits.core.utils.warn
-import java.util.concurrent.ConcurrentHashMap
-
 interface ToolkitCredentialsChangeListener {
     fun providerAdded(provider: ToolkitCredentialsProvider) {}
     fun providerModified(provider: ToolkitCredentialsProvider) {}
     fun providerRemoved(providerId: String) {}
-}
-
-interface ToolkitCredentialsProviderManager : ToolkitCredentialsChangeListener {
-    @Throws(CredentialProviderNotFound::class)
-    fun getCredentialProvider(id: String): ToolkitCredentialsProvider
-
-    /**
-     * Returns a list of all the registered providers. This list should never change for the life of the toolkit
-     */
-    fun getCredentialProviders(): List<ToolkitCredentialsProvider>
-
-    fun addChangeListener(listener: ToolkitCredentialsChangeListener)
-
-    fun removeChangeListener(listener: ToolkitCredentialsChangeListener)
-
-    /**
-     * Shuts down the manager and all registered factories
-     */
-    fun shutDown()
-}
-
-class DefaultToolkitCredentialsProviderManager(registry: ToolkitCredentialsProviderRegistry) :
-    ToolkitCredentialsProviderManager {
-    private val listeners = ConcurrentHashMap.newKeySet<ToolkitCredentialsChangeListener>()
-    private val factories = mutableListOf<ToolkitCredentialsProviderFactory<*>>()
-
-    init {
-        reloadFactories(registry)
-    }
-
-    @Throws(CredentialProviderNotFound::class)
-    override fun getCredentialProvider(id: String): ToolkitCredentialsProvider = factories.asSequence().mapNotNull { it.get(id) }.firstOrNull()
-        ?: throw CredentialProviderNotFound("No ToolkitCredentialsProvider found represented by $id")
-
-    override fun getCredentialProviders(): List<ToolkitCredentialsProvider> = factories.flatMap { it.listCredentialProviders() }.toList()
-
-    override fun addChangeListener(listener: ToolkitCredentialsChangeListener) {
-        listeners.add(listener)
-    }
-
-    override fun removeChangeListener(listener: ToolkitCredentialsChangeListener) {
-        listeners.remove(listener)
-    }
-
-    override fun providerAdded(provider: ToolkitCredentialsProvider) {
-        listeners.forEach {
-            LOG.tryOrNull("Failed to notify listener that provider was added") {
-                it.providerAdded(provider)
-            }
-        }
-    }
-
-    override fun providerModified(provider: ToolkitCredentialsProvider) {
-        listeners.forEach {
-            LOG.tryOrNull("Failed to notify listener that provider was modified") {
-                it.providerModified(provider)
-            }
-        }
-    }
-
-    override fun providerRemoved(providerId: String) {
-        listeners.forEach {
-            LOG.tryOrNull("Failed to notify listener that provider was removed") {
-                it.providerRemoved(providerId)
-            }
-        }
-    }
-
-    fun reloadFactories(registry: ToolkitCredentialsProviderRegistry) {
-        factories.clear()
-        factories.addAll(registry.listFactories(this))
-    }
-
-    /**
-     * Shuts down the manager and all registered factories
-     */
-    override fun shutDown() {
-        factories.forEach {
-            try {
-                it.shutDown()
-            } catch (e: Exception) {
-                LOG.warn(e) { "ToolkitCredentialsProviderFactory '${it::class.qualifiedName}' threw exception when shutting down" }
-            }
-        }
-    }
-
-    companion object {
-        private val LOG = LoggerFactory.getLogger(ToolkitCredentialsProviderManager::class.java)
-    }
 }
 
 class CredentialProviderNotFound(msg: String) : RuntimeException(msg)
--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/ExtensionPointCredentialsProviderRegistry.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/ExtensionPointCredentialsProviderRegistry.kt
@@ -4,26 +4,21 @@
 package software.aws.toolkits.jetbrains.core.credentials
 
 import com.intellij.openapi.extensions.AbstractExtensionPointBean
-import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.util.LazyInstance
 import com.intellij.util.xmlb.annotations.Attribute
-import software.aws.toolkits.core.credentials.ToolkitCredentialsProviderFactory
-import software.aws.toolkits.core.credentials.ToolkitCredentialsProviderManager
-import software.aws.toolkits.core.credentials.ToolkitCredentialsProviderRegistry
-import software.aws.toolkits.core.utils.getLogger
-import software.aws.toolkits.core.utils.tryOrNull
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
+import software.aws.toolkits.core.region.AwsRegion
 
 /**
  * Extension point for adding new credential providers to the internal registry
  */
 interface CredentialProviderFactory {
-    /**
-     * Creates the [ToolkitCredentialsProviderFactory], this should return the same instance each time
-     */
-    fun createToolkitCredentialProviderFactory(manager: ToolkitCredentialsProviderManager): ToolkitCredentialsProviderFactory<*>
+    fun setupToolkitCredentialProviderFactory(manager: CredentialManager)
+
+    fun createAwsCredentialProvider(region: AwsRegion): AwsCredentialsProvider
 }
 
-class CredentialProviderFactoryEP : AbstractExtensionPointBean() {
+class CredentialProviderFactoryExtensionPoint : AbstractExtensionPointBean() {
     @Attribute("implementation")
     lateinit var implementation: String
 
@@ -31,21 +26,5 @@ class CredentialProviderFactoryEP : AbstractExtensionPointBean() {
         override fun getInstanceClass(): Class<CredentialProviderFactory> = findClass(implementation)
     }
 
-    fun getHandler(): CredentialProviderFactory = instance.value
-}
-
-class ExtensionPointCredentialsProviderRegistry : ToolkitCredentialsProviderRegistry {
-    override fun listFactories(manager: ToolkitCredentialsProviderManager) = EXTENSION_POINT.extensions
-        .mapNotNull {
-            LOG.tryOrNull("Failed td load CredentialProviderFactory") {
-                it.getHandler()
-            }
-        }
-        .map { it.createToolkitCredentialProviderFactory(manager) }
-
-    companion object {
-        private val LOG = getLogger<ExtensionPointCredentialsProviderRegistry>()
-        private const val EP_NAME = "aws.toolkit.credentialProviderFactory"
-        private val EXTENSION_POINT = ExtensionPointName.create<CredentialProviderFactoryEP>(EP_NAME)
-    }
+    fun getInstance(): CredentialProviderFactory = instance.value
 }
