11b6480f2f16fbe2a62f0f390493286ffea2c5e7
Hunter Werlla <werlla@amazon.com>
2020-03-18 09:11:34 -0700

    PR comments

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/actions/ShowLogsAround.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/actions/ShowLogsAround.kt
@@ -25,9 +25,9 @@ class ShowLogsAroundGroup(
     }
 
     override fun getChildren(e: AnActionEvent?): Array<AnAction> = arrayOf(
-        ShowLogsAround(logGroup, logStream, treeTable, message("general.time.one_minute"), Duration.ofMinutes(1).toMillis()),
-        ShowLogsAround(logGroup, logStream, treeTable, message("general.time.five_minutes"), Duration.ofMinutes(5).toMillis()),
-        ShowLogsAround(logGroup, logStream, treeTable, message("general.time.ten_minutes"), Duration.ofMinutes(10).toMillis())
+        ShowLogsAround(logGroup, logStream, treeTable, message("general.time.one_minute"), Duration.ofMinutes(1)),
+        ShowLogsAround(logGroup, logStream, treeTable, message("general.time.five_minutes"), Duration.ofMinutes(5)),
+        ShowLogsAround(logGroup, logStream, treeTable, message("general.time.ten_minutes"), Duration.ofMinutes(10))
     )
 }
 
@@ -36,13 +36,13 @@ private class ShowLogsAround(
     private val logStream: String,
     private val treeTable: TableView<OutputLogEvent>,
     time: String,
-    private val durationMilliseconds: Long
+    private val duration: Duration
 ) :
     AnAction(time, null, null), DumbAware {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.getRequiredData(PlatformDataKeys.PROJECT)
         val window = CloudWatchLogWindow.getInstance(project)
         val selectedObject = treeTable.listTableModel.getItem(treeTable.selectedRow) ?: return
-        window.showLogStream(logGroup, logStream, selectedObject.timestamp(), durationMilliseconds)
+        window.showLogStream(logGroup, logStream, selectedObject.timestamp(), duration)
     }
 }
