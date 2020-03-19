97fafdd98dc441c2c2253c6a82e0047c235d3dbc
Hunter Werlla <werlla@amazon.com>
2020-03-19 12:57:09 -0700

    progress

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/actions/OpenLogStreamInEditor.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/actions/OpenLogStreamInEditor.kt
@@ -7,21 +7,26 @@ import com.intellij.icons.AllIcons
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.fileChooser.FileChooserFactory
+import com.intellij.openapi.fileChooser.FileSaverDescriptor
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.io.FileUtilRt.getUserContentLoadLimit
+import com.intellij.openapi.ui.Messages
 import com.intellij.ui.table.JBTable
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient
 import software.amazon.awssdk.services.cloudwatchlogs.model.OutputLogEvent
 import software.aws.toolkits.jetbrains.core.awsClient
 import software.aws.toolkits.jetbrains.utils.ApplicationThreadPoolScope
 import software.aws.toolkits.jetbrains.utils.getCoroutineUiContext
 import software.aws.toolkits.resources.message
+import java.io.File
+import java.time.Instant
 
 // TODO check resulting file size/ size of log stream and have a pop up if > max editor size
 // TODO add progress bar for loading
@@ -44,14 +49,35 @@ private class DownloadTask(project: Project, val client: CloudWatchLogsClient, v
     private val edt = getCoroutineUiContext(ModalityState.defaultModalityState())
 
     override fun run(indicator: ProgressIndicator) {
+        runBlocking {
+            runSuspend(indicator)
+        }
+    }
+
+    suspend fun runSuspend(indicator: ProgressIndicator) {
+        val startTime = Instant.now()
         var events: List<OutputLogEvent>
         var nextToken: String? = null
         val buffer = StringBuilder()
-        val maxFileSize = getUserContentLoadLimit()
+        val maxFileSize = 1000 //getUserContentLoadLimit()
         do {
             if (buffer.length > maxFileSize) {
-                println("way too big yo")
-                return
+                when (promptWriteToFile()) {
+                    Messages.OK -> {
+                        val descriptor = FileSaverDescriptor(message("s3.download.object.action"), message("s3.download.object.description"))
+                        val saveLocation = withContext(edt) {
+                            val destination = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, project)
+                            destination.save(null, null)
+                        }
+                        if (saveLocation != null) {
+                            writeToFile(indicator, startTime, nextToken, saveLocation.file, buffer)
+                        }
+                        return
+                    }
+                    Messages.CANCEL -> {
+                        indicator.cancel()
+                    }
+                }
             }
             indicator.checkCanceled()
             val response = client.getLogEvents {
@@ -59,6 +85,7 @@ private class DownloadTask(project: Project, val client: CloudWatchLogsClient, v
                     .startFromHead(true)
                     .logGroupName(logGroup)
                     .logStreamName(logStream)
+                    .endTime(startTime.toEpochMilli())
                     .nextToken(nextToken)
             }
             events = response.events()
@@ -66,10 +93,39 @@ private class DownloadTask(project: Project, val client: CloudWatchLogsClient, v
             val str = events.joinToString("") { if (it.message().endsWith("\n")) it.message() else "${it.message()}\n" }
             buffer.append(str)
         } while (events.isNotEmpty())
-        launch {
-            OpenStreamInEditor.open(project, edt, logStream, buffer.toString())
-        }
+
+        OpenStreamInEditor.open(project, edt, logStream, buffer.toString())
     }
 
-    override fun onCancel() {}
+    suspend fun promptWriteToFile(): Int = withContext(edt) {
+        return@withContext Messages.showOkCancelDialog(
+            project,
+            message("cloudwatch.logs.stream_too_big_message"),
+            message("cloudwatch.logs.stream_too_big"),
+            Messages.OK_BUTTON,
+            Messages.CANCEL_BUTTON,
+            AllIcons.General.QuestionDialog
+        )
+    }
+
+    fun writeToFile(indicator: ProgressIndicator, startTime: Instant, next: String?, file: File, buffer: StringBuilder) {
+        file.appendText(buffer.toString())
+        var events: List<OutputLogEvent>
+        var nextToken: String? = next
+        do {
+            indicator.checkCanceled()
+            val response = client.getLogEvents {
+                it
+                    .startFromHead(true)
+                    .logGroupName(logGroup)
+                    .logStreamName(logStream)
+                    .endTime(startTime.toEpochMilli())
+                    .nextToken(nextToken)
+            }
+            events = response.events()
+            nextToken = response.nextForwardToken()
+            val str = events.joinToString("") { if (it.message().endsWith("\n")) it.message() else "${it.message()}\n" }
+            file.appendText(str)
+        } while (events.isNotEmpty())
+    }
 }
