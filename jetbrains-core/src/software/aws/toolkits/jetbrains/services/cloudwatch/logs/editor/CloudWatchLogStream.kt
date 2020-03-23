31ce11daef598df50657deb9a77bea9188ae8e09
Hunter Werlla <werlla@amazon.com>
2020-03-23 11:58:07 -0700

    Add forgotten breadcrumb

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/editor/CloudWatchLogStream.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/editor/CloudWatchLogStream.kt
@@ -55,7 +55,7 @@ class CloudWatchLogStream(
     init {
         tablePanel.setContent(logStreamTable.component)
 
-        val locationCrumbs = LocationCrumbs(project, logGroup)
+        val locationCrumbs = LocationCrumbs(project, logGroup, logStream)
         locationInformation.crumbs = locationCrumbs.crumbs
         breadcrumbHolder.border = locationCrumbs.border
 
