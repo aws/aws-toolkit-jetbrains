0d9627fd2f4fd0d96e657b50cb8f8ebb98c5156a
Hunter Werlla <werlla@amazon.com>
2020-05-13 10:08:42 -0700

    Add refresh

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/editor/S3ViewerPanel.java
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/editor/S3ViewerPanel.java
@@ -3,101 +3,91 @@
 
 package software.aws.toolkits.jetbrains.services.s3.editor;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.Disposable;
-import com.intellij.openapi.actionSystem.ActionGroup;
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.ActionPlaces;
-import com.intellij.openapi.actionSystem.ActionToolbar;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.actionSystem.Separator;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.SimpleToolWindowPanel;
 import com.intellij.ui.PopupHandler;
-import com.intellij.ui.ScrollPaneFactory;
 import com.intellij.ui.ToolbarDecorator;
 import com.intellij.ui.treeStructure.SimpleTreeStructure;
 import com.intellij.util.ui.ColumnInfo;
-import java.awt.BorderLayout;
-import javax.swing.JComponent;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.table.DefaultTableCellRenderer;
-import software.aws.toolkits.core.region.AwsRegion;
-import software.aws.toolkits.jetbrains.core.credentials.ProjectAccountSettingsManager;
-import software.aws.toolkits.jetbrains.services.s3.S3UtilsKt;
-import software.aws.toolkits.jetbrains.services.s3.objectActions.CopyPathAction;
-import software.aws.toolkits.jetbrains.services.s3.objectActions.DeleteObjectAction;
-import software.aws.toolkits.jetbrains.services.s3.objectActions.DownloadObjectAction;
-import software.aws.toolkits.jetbrains.services.s3.objectActions.NewFolderAction;
-import software.aws.toolkits.jetbrains.services.s3.objectActions.RenameObjectAction;
-import software.aws.toolkits.jetbrains.services.s3.objectActions.UploadObjectAction;
-import software.aws.toolkits.jetbrains.services.s3.resources.S3Resources;
+import org.jetbrains.annotations.NotNull;
+import software.aws.toolkits.jetbrains.services.s3.objectActions.*;
 import software.aws.toolkits.jetbrains.ui.tree.AsyncTreeModel;
 import software.aws.toolkits.jetbrains.ui.tree.StructureTreeModel;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 
-import static com.intellij.openapi.actionSystem.ActionToolbarPosition.TOP;
+import static software.aws.toolkits.resources.Localization.message;
 
 @SuppressWarnings("unchecked")
 public class S3ViewerPanel {
     private Disposable disposable;
     private JPanel content;
-    private JTextField name;
-    private JTextField date;
     private JPanel mainPanel;
     private S3TreeTable treeTable;
-    private S3TreeNode s3TreeNode;
-    private S3TreeTableModel model;
-    private Project project;
+    private final Project project;
+    private final ColumnInfo<Object, String>[] columns;
+    private S3VirtualBucket virtualBucket;
 
     public S3ViewerPanel(Disposable disposable, Project project, S3VirtualBucket bucketVirtual) {
-        this.disposable = disposable;
         this.project = project;
-
-        name.setText(bucketVirtual.getName());
-        date.setText(S3Resources.formatDate(bucketVirtual.getS3Bucket().creationDate()));
-
-        s3TreeNode = new S3TreeDirectoryNode(bucketVirtual, null, "");
+        this.disposable = disposable;
+        this.virtualBucket = bucketVirtual;
 
         ColumnInfo<Object, String> key = new S3Column(S3ColumnType.NAME);
         ColumnInfo<Object, String> size = new S3Column(S3ColumnType.SIZE);
         ColumnInfo<Object, String> modified = new S3Column(S3ColumnType.LAST_MODIFIED);
-        ColumnInfo<Object, String>[] columns = new ColumnInfo[] {key, size, modified};
-        model = createTreeTableModel(columns);
-
-        DefaultTableCellRenderer tableRenderer = new DefaultTableCellRenderer();
-        tableRenderer.setHorizontalAlignment(SwingConstants.LEFT);
-
+        columns = new ColumnInfo[] {key, size, modified};
+        S3TreeTableModel model = createTreeTableModel(columns, new S3TreeDirectoryNode(bucketVirtual, null, ""));
         treeTable = new S3TreeTable(model, bucketVirtual, project);
-        treeTable.setRootVisible(false);
-        treeTable.setDefaultRenderer(Object.class, tableRenderer);
-        S3TreeCellRenderer treeRenderer = new S3TreeCellRenderer(treeTable);
-        treeTable.setTreeCellRenderer(treeRenderer);
-        treeTable.setCellSelectionEnabled(false);
+        applyTreeStyle(treeTable);
+        addTreeActions(treeTable);
 
-        treeTable.setRowSelectionAllowed(true);
-        treeTable.setAutoCreateRowSorter(true);
-        treeTable.setRowSorter(new S3RowSorter(treeTable.getModel()));
-
-        addTreeActions();
-
-        treeTable.getColumnModel().getColumn(1).setMaxWidth(120);
-        //SimpleToolWindowPanel tablePanel = new SimpleToolWindowPanel(true, false);
         ToolbarDecorator panel = addToolbar(treeTable);
-        //tablePanel.setContent(ScrollPaneFactory.createScrollPane(treeTable));
-
-        //mainPanel.add(tablePanel, BorderLayout.CENTER);
         mainPanel.add(panel.createPanel());
     }
 
     private void createUIComponents() {
     }
 
-    private ToolbarDecorator addToolbar(S3TreeTable treeTable) {
+    private ToolbarDecorator addToolbar(S3TreeTable table) {
+        DefaultActionGroup group = makeActionGroup(table);
+        group.addAction(new AnAction(message("explorer.refresh.title"), null, AllIcons.Actions.Refresh) {
+            @Override
+            public boolean isDumbAware() {
+                return true;
+            }
+
+            @Override
+            public void actionPerformed(@NotNull AnActionEvent e) {
+                table.setModel(createTreeTableModel(columns, new S3TreeDirectoryNode(virtualBucket, null, "")));
+                // we have to apply style again because changing the model breaks the style
+                applyTreeStyle(table);
+                table.invalidate();
+            }
+        });
         return ToolbarDecorator
-            .createDecorator(treeTable)
-            .setActionGroup(makeActionGroup())
-            .setToolbarPosition(TOP);
+            .createDecorator(table)
+            .setActionGroup(group);
+    }
+
+    private void applyTreeStyle(S3TreeTable table) {
+        DefaultTableCellRenderer tableRenderer = new DefaultTableCellRenderer();
+        tableRenderer.setHorizontalAlignment(SwingConstants.LEFT);
+        table.setDefaultRenderer(Object.class, tableRenderer);
+        table.setRootVisible(false);
+        S3TreeCellRenderer treeRenderer = new S3TreeCellRenderer(table);
+        table.setTreeCellRenderer(treeRenderer);
+        table.setCellSelectionEnabled(false);
+        table.setRowSelectionAllowed(true);
+        table.setRowSorter(new S3RowSorter(table.getModel()));
+        // prevent accidentally moving the columns around. We don't account for the ability
+        // to do this anywhere so better be safe than sorry. TODO audit logic to allow this
+        table.getTableHeader().setReorderingAllowed(false);
+        table.getColumnModel().getColumn(1).setMaxWidth(120);
     }
 
     public JComponent getComponent() {
@@ -108,30 +98,26 @@ public class S3ViewerPanel {
         return treeTable;
     }
 
-    public JTextField getName() {
-        return name;
-    }
-
-    private S3TreeTableModel createTreeTableModel(ColumnInfo[] columns) {
+    private S3TreeTableModel createTreeTableModel(ColumnInfo[] columns, S3TreeDirectoryNode s3TreeNode) {
         SimpleTreeStructure treeStructure = new SimpleTreeStructure.Impl(s3TreeNode);
         StructureTreeModel<SimpleTreeStructure> myTreeModel = new StructureTreeModel(treeStructure, disposable);
         return new S3TreeTableModel(new AsyncTreeModel(myTreeModel, true, disposable), columns, myTreeModel);
     }
 
-    private void addTreeActions() {
-        PopupHandler.installPopupHandler(treeTable, makeActionGroup(), ActionPlaces.EDITOR_POPUP, ActionManager.getInstance());
+    private void addTreeActions(S3TreeTable table) {
+        PopupHandler.installPopupHandler(table, makeActionGroup(table), ActionPlaces.EDITOR_POPUP, ActionManager.getInstance());
     }
 
-    private DefaultActionGroup makeActionGroup() {
+    private DefaultActionGroup makeActionGroup(S3TreeTable table) {
         DefaultActionGroup actionGroup = new DefaultActionGroup();
-        actionGroup.add(new DownloadObjectAction(project, treeTable));
-        actionGroup.add(new UploadObjectAction(project, treeTable));
+        actionGroup.add(new DownloadObjectAction(project, table));
+        actionGroup.add(new UploadObjectAction(project, table));
         actionGroup.add(new Separator());
-        actionGroup.add(new NewFolderAction(project, treeTable));
-        actionGroup.add(new RenameObjectAction(project, treeTable));
-        actionGroup.add(new CopyPathAction(project, treeTable));
+        actionGroup.add(new NewFolderAction(project, table));
+        actionGroup.add(new RenameObjectAction(project, table));
+        actionGroup.add(new CopyPathAction(project, table));
         actionGroup.add(new Separator());
-        actionGroup.add(new DeleteObjectAction(project, treeTable));
+        actionGroup.add(new DeleteObjectAction(project, table));
         return actionGroup;
     }
 }
