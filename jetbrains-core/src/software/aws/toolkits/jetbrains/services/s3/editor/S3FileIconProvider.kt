4014307e80bf38a4832b8fecf5903791232e6cad
Hunter Werlla <werlla@amazon.com>
2020-05-26 08:14:26 -0700

    Add test

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/editor/S3FileIconProvider.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/s3/editor/S3FileIconProvider.kt
@@ -10,5 +10,9 @@ import icons.AwsIcons.Resources.S3_BUCKET
 import javax.swing.Icon
 
 class S3FileIconProvider : FileIconProvider {
-    override fun getIcon(file: VirtualFile, flags: Int, project: Project?): Icon? = if (file is S3VirtualBucket) { S3_BUCKET } else { null }
+    override fun getIcon(file: VirtualFile, flags: Int, project: Project?): Icon? = if (file is S3VirtualBucket) {
+        S3_BUCKET
+    } else {
+        null
+    }
 }
