65f89eeeed6d3c921a5d33c4d2716618ddc6ef6a
Hunter Werlla <werlla@amazon.com>
2020-06-17 11:51:36 -0700

    Add tests

--- jetbrains-ultimate/tst-201/software/aws/toolkits/jetbrains/core/datagrip/auth/SecretsManagerAuthTest.kt
+++ jetbrains-ultimate/tst-201/software/aws/toolkits/jetbrains/core/datagrip/auth/SecretsManagerAuthTest.kt
@@ -3,6 +3,130 @@
 
 package software.aws.toolkits.jetbrains.core.datagrip.auth
 
-class SecretsManagerAuthTest {
+import com.intellij.database.dataSource.DatabaseConnectionInterceptor
+import com.intellij.database.dataSource.DatabaseConnectionPoint
+import com.intellij.database.dataSource.LocalDataSource
+import com.intellij.testFramework.ProjectRule
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.stub
+import org.assertj.core.api.Assertions
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse
+import software.amazon.awssdk.services.secretsmanager.model.SecretListEntry
+import software.aws.toolkits.core.region.AwsRegion
+import software.aws.toolkits.core.utils.RuleUtils
+import software.aws.toolkits.jetbrains.core.MockClientManagerRule
+import software.aws.toolkits.jetbrains.core.MockResourceCacheRule
+import software.aws.toolkits.jetbrains.core.credentials.MockCredentialsManager
+import software.aws.toolkits.jetbrains.core.datagrip.CREDENTIAL_ID_PROPERTY
+import software.aws.toolkits.jetbrains.core.datagrip.REGION_ID_PROPERTY
+import software.aws.toolkits.jetbrains.core.region.MockRegionProvider
+import software.aws.toolkits.jetbrains.services.secretsmanager.SecretsManagerResources
+import java.util.concurrent.ExecutionException
 
+class SecretsManagerAuthTest {
+    @Rule
+    @JvmField
+    val projectRule = ProjectRule()
+
+    @Rule
+    @JvmField
+    val resourceCache = MockResourceCacheRule(projectRule)
+
+    @Rule
+    @JvmField
+    val clientManager = MockClientManagerRule(projectRule)
+
+    private val sAuth = SecretsManagerAuth()
+    private val username = RuleUtils.randomName()
+    private val password = RuleUtils.randomName()
+    private val secret = RuleUtils.randomName()
+    private val credentialId = RuleUtils.randomName()
+    private val defaultRegion = RuleUtils.randomName()
+    private val dbHost = "${RuleUtils.randomName()}.555555.us-west-2.rds.amazonaws.com"
+    private val port = 5432
+
+    private val mockCreds = AwsBasicCredentials.create("Access", "ItsASecret")
+
+    @Before
+    fun setUp() {
+        MockCredentialsManager.getInstance().addCredentials(credentialId, mockCreds)
+        MockRegionProvider.getInstance().addRegion(AwsRegion(defaultRegion, RuleUtils.randomName(), RuleUtils.randomName()))
+    }
+
+    @Test
+    fun `Intercept credentials succeeds`() {
+        resourceCache.get().addEntry(SecretsManagerResources.secrets, listOf(SecretListEntry.builder().name(secret).build()))
+        val client = clientManager.create<SecretsManagerClient>()
+        client.stub {
+            on { getSecretValue(any<GetSecretValueRequest>()) } doAnswer {
+                GetSecretValueResponse.builder().name(secret).secretString("""{"username":"$username","password":"$password"}""").build()
+            }
+        }
+        val connection = sAuth.intercept(buildConnection(), false)?.toCompletableFuture()?.get()
+        Assertions.assertThat(connection).isNotNull
+        Assertions.assertThat(connection!!.connectionProperties).containsKey("user")
+        Assertions.assertThat(connection.connectionProperties["user"]).isEqualTo(username)
+        Assertions.assertThat(connection.connectionProperties).containsKey("password")
+        Assertions.assertThat(connection.connectionProperties["password"]).isEqualTo(password)
+    }
+
+    @Test(expected = ExecutionException::class)
+    fun `Intercept credentials fails`() {
+        sAuth.intercept(buildConnection(hasSecret = false), false)!!.toCompletableFuture().get()
+    }
+
+    private fun buildConnection(
+        hasUrl: Boolean = true,
+        hasRegion: Boolean = true,
+        hasCredentials: Boolean = true,
+        hasHost: Boolean = true,
+        hasPort: Boolean = true,
+        hasSecret: Boolean = true
+    ): DatabaseConnectionInterceptor.ProtoConnection {
+        val mockConnection = mock<LocalDataSource> {
+            on { url } doReturn if (hasUrl) {
+                "jdbc:postgresql://${if (hasHost) dbHost else ""}${if (hasPort) ":$port" else ""}/dev"
+            } else {
+                null
+            }
+            on { databaseDriver } doReturn null
+            on { driverClass } doReturn "org.postgresql.Driver"
+        }
+        val dbConnectionPoint = mock<DatabaseConnectionPoint> {
+            on { additionalJdbcProperties } doAnswer {
+                val m = mutableMapOf<String, String>()
+                if (hasCredentials) {
+                    m[CREDENTIAL_ID_PROPERTY] = credentialId
+                }
+                if (hasRegion) {
+                    m[REGION_ID_PROPERTY] = defaultRegion
+                }
+                if (hasSecret) {
+                    m[SECRET_ID_PROPERTY] = secret
+                }
+                m
+            }
+            on { dataSource } doReturn mockConnection
+        }
+        return mock {
+            val m = mutableMapOf<String, String>()
+            on { connectionPoint } doReturn dbConnectionPoint
+            on { runConfiguration } doAnswer {
+                mock {
+                    on { project } doAnswer { projectRule.project }
+                }
+            }
+            on { connectionProperties } doReturn m
+        }
+    }
 }
+
