be236b54253743c705ea6dff7acd2ad266f239ef
Hunter Werlla <werlla@amazon.com>
2020-06-22 15:56:36 -0700

    will fix the ktlint issue tomorrow

--- jetbrains-ultimate/src-201/software/aws/toolkits/jetbrains/datagrip/actions/SecretsManagerDialogWrapper.kt
+++ jetbrains-ultimate/src-201/software/aws/toolkits/jetbrains/datagrip/actions/SecretsManagerDialogWrapper.kt
@@ -79,7 +79,7 @@ class SecretsManagerDialogWrapper(private val selected: AwsExplorerNode<*>) : Di
         val selectedSecret = secrets.selected()
         val response = DatabaseSecret.getSecret(selected.nodeProject, selectedSecret)
         if (response == null) {
-            runInEdt(ModalityState.any()) {
+            runInEdt {
                 super.doCancelAction()
                 notifyError(content = message("datagrip.secretsmanager.validation.failed_to_get", selectedSecret?.arn().toString()))
             }
@@ -90,10 +90,11 @@ class SecretsManagerDialogWrapper(private val selected: AwsExplorerNode<*>) : Di
         dbSecretArn = response.second
         // validate the content of the secret
         val validationInfo = DatabaseSecret.validateSecret(selected, response.first, response.second.arnToName())
-        runInEdt(ModalityState.any()) {
-            if (validationInfo == null) {
-                super.doOKAction()
-            } else {
+
+        if (validationInfo == null) {
+            runInEdt { super.doOKAction() }
+        } else {
+            runInEdt(ModalityState.any()) {
                 val result = Messages.showOkCancelDialog(
                     selected.nodeProject,
                     message("datagrip.secretsmanager.action.confirm_continue", validationInfo.message),
@@ -103,7 +104,7 @@ class SecretsManagerDialogWrapper(private val selected: AwsExplorerNode<*>) : Di
                     Messages.getWarningIcon()
                 )
                 if (result == Messages.OK) {
-                    super.doOKAction()
+                    runInEdt { super.doOKAction() }
                 }
             }
         }
