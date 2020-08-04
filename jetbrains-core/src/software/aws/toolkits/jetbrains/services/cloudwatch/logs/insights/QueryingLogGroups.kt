84a86c3ac4423d6ecd60d08507d9e9c0df2ccd55
Manodnya Bhoite <manodnyb@amazon.com>
2020-08-04 08:27:19 -0700

    String parsing done

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/insights/QueryingLogGroups.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/cloudwatch/logs/insights/QueryingLogGroups.kt
@@ -9,6 +9,7 @@ import kotlinx.coroutines.launch
 import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient
 import software.amazon.awssdk.services.cloudwatchlogs.model.DescribeQueriesRequest
 import software.amazon.awssdk.services.cloudwatchlogs.model.GetQueryResultsRequest
+import software.amazon.awssdk.services.cloudwatchlogs.model.ResultField
 import software.amazon.awssdk.services.cloudwatchlogs.model.StartQueryRequest
 import software.aws.toolkits.jetbrains.utils.ApplicationThreadPoolScope
 
@@ -23,16 +24,34 @@ class QueryingLogGroups(private val project: Project) : CoroutineScope by Applic
             .build()
         val response = client.startQuery(request)
         var queryId: String = response.queryId()
+        val fieldList= getFields(query)
+        println(fieldList)
         getQueryResults(queryId, client)
     }
 
+    private fun getFields(query: String) : List<String> {
+        return if(query.contains("fields", ignoreCase = true)){
+            var fields = query.substring(query.indexOf("fields", ignoreCase = true)+7)
+            if (fields.contains("|")) {
+                fields = fields.substring(0,fields.indexOf("|"))
+            }
+            fields.split(",").map{it.trim()}
+        }
+        else{
+            listOf<String>("@message","@timestamp")
+        }
+    }
     private fun getQueryResults(queryId: String, client: CloudWatchLogsClient) = launch {
         var status = ""
+        lateinit var a : MutableList<ResultField>
         while(status!="Complete"){
             val requestCheckQueryCompletion=GetQueryResultsRequest.builder().queryId(queryId).build()
             val responseCheckQueryCompletion=client.getQueryResults(requestCheckQueryCompletion)
             status= responseCheckQueryCompletion.statusAsString()
-            if(status=="Complete"){
+            println()
+            println(responseCheckQueryCompletion.results())
+
+            /*if(status=="Complete"){
                 var queryList = responseCheckQueryCompletion.results()
                 for (item in queryList){
                     for (item1 in item){
@@ -41,7 +60,7 @@ class QueryingLogGroups(private val project: Project) : CoroutineScope by Applic
 
                 }
                 //QueryResultsWindow.getInstance(project).showResults(queryList, queryId)
-            }
+            }*/
 
         }
         println("Done")
