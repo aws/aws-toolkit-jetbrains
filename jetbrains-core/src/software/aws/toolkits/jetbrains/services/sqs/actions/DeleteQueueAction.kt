275f78f258ef731a464c313806063a9bfa6a743e
ranyoo2367 <ranyoo@umich.edu>
2020-08-06 15:07:11 -0400

    Set up delete action

---
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/sqs/actions/DeleteQueueAction.kt
@@ -0,0 +1,28 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.sqs.actions
+
+import com.intellij.openapi.application.runInEdt
+import software.amazon.awssdk.services.sqs.SqsClient
+import software.aws.toolkits.jetbrains.core.awsClient
+import software.aws.toolkits.jetbrains.core.explorer.actions.DeleteResourceAction
+import software.aws.toolkits.jetbrains.core.explorer.refreshAwsTree
+import software.aws.toolkits.jetbrains.services.sqs.SqsQueueNode
+import software.aws.toolkits.jetbrains.services.sqs.resources.SqsResources
+import software.aws.toolkits.jetbrains.services.sqs.toolwindow.SqsWindow
+import software.aws.toolkits.jetbrains.utils.TaggingResourceType
+import software.aws.toolkits.resources.message
+
+class DeleteQueueAction : DeleteResourceAction<SqsQueueNode>(message("sqs.delete.queue.action"), TaggingResourceType.SQS_QUEUE) {
+    override fun performDelete(selected: SqsQueueNode) {
+        val client = selected.nodeProject.awsClient<SqsClient>()
+        runInEdt {
+            SqsWindow.getInstance(selected.nodeProject).closeQueue(selected.queueUrl)
+        }
+        client.deleteQueue { it.queueUrl(selected.queueUrl) }
+        selected.nodeProject.refreshAwsTree(SqsResources.LIST_QUEUE_URLS)
+
+        // TODO: Because telemetry not implemented, currently returns an error when deleting telemetry resource
+    }
+}
