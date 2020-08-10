437354b9bc433ef4b282649097f6ab89bb38101b
Hunter Werlla <werlla@amazon.com>
2020-08-10 11:51:21 -0700

    Refactor

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/filtering/ExplorerTagFilter.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/filtering/ExplorerTagFilter.kt
@@ -7,6 +7,8 @@ import com.intellij.ide.projectView.ViewSettings
 import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.Project
 import software.aws.toolkits.jetbrains.core.AwsResourceCache
+import software.aws.toolkits.jetbrains.core.credentials.activeCredentialProvider
+import software.aws.toolkits.jetbrains.core.credentials.activeRegion
 import software.aws.toolkits.jetbrains.core.explorer.AwsExplorerTreeStructureProvider
 import software.aws.toolkits.jetbrains.core.explorer.nodes.AwsExplorerNode
 import software.aws.toolkits.jetbrains.core.explorer.nodes.AwsExplorerResourceNode
@@ -17,37 +19,44 @@ class ExplorerTagFilter : AwsExplorerTreeStructureProvider {
         parent: AbstractTreeNode<*>,
         children: MutableCollection<AbstractTreeNode<*>>,
         settings: ViewSettings?
-    ): MutableCollection<AbstractTreeNode<*>> {
-        val awsParent = parent as? AwsExplorerNode<*> ?: return children
-        val filterManager = ResourceFilterManager.getInstance(awsParent.nodeProject)
-        if (!filterManager.tagFilterEnabled()) {
-            return children
-        }
-        val resourceNodes = children.filterIsInstance<AwsExplorerResourceNode<*>>()
-        val computedNodes = if (resourceNodes.isEmpty()) {
-            listOf()
-        } else {
-            filterByTag(awsParent.nodeProject, filterManager, resourceNodes)
-        }
-        val otherNodes = children.filter { it !is AwsExplorerResourceNode<*> }
-        return (otherNodes + computedNodes).toMutableList()
-    }
-
-    private fun filterByTag(
-        project: Project,
-        filterManager: ResourceFilterManager,
-        resourceNodes: List<AwsExplorerResourceNode<*>>
-    ): List<AwsExplorerResourceNode<*>> {
-        val resourceCache = AwsResourceCache.getInstance(project)
-        return resourceNodes.mapNotNull { node ->
-            val tags = resourceCache
-                .getResourceNow(ResourceGroupsTaggingApiResources.listResources(node.serviceId, node.resourceType(), filterManager.state.tags))
-                .resourceTagMappingList()
-            if (tags.any { node.resourceArn() == it.resourceARN() }) {
-                node
+    ): MutableCollection<AbstractTreeNode<*>> = when (parent) {
+        is AwsExplorerNode -> {
+            val filterManager = ResourceFilterManager.getInstance(parent.nodeProject)
+            if (!filterManager.tagFilterEnabled()) {
+                children
             } else {
-                null
+                val resourceNodes = children.filterIsInstance<AwsExplorerResourceNode<*>>()
+                val computedNodes = if (resourceNodes.isEmpty()) {
+                    listOf()
+                } else {
+                    filterByTag(parent.nodeProject, filterManager, resourceNodes)
+                }
+                val otherNodes = children.filter { it !is AwsExplorerResourceNode<*> }
+                (otherNodes + computedNodes).toMutableList()
             }
+        }
+        else -> children
+    }
+}
+
+private fun filterByTag(
+    project: Project,
+    filterManager: ResourceFilterManager,
+    resourceNodes: List<AwsExplorerResourceNode<*>>
+): List<AwsExplorerResourceNode<*>> {
+    val resourceCache = AwsResourceCache.getInstance(project)
+    return resourceNodes.mapNotNull { node ->
+        val tags = resourceCache
+            .getResourceNow(
+                resource = ResourceGroupsTaggingApiResources.listResources(node.serviceId, node.resourceType(), filterManager.state.tags),
+                region = node.nodeProject.activeRegion(),
+                credentialProvider = node.nodeProject.activeCredentialProvider()
+            )
+            .resourceTagMappingList()
+        if (tags.any { node.resourceArn() == it.resourceARN() }) {
+            node
+        } else {
+            null
         }
     }
 }
