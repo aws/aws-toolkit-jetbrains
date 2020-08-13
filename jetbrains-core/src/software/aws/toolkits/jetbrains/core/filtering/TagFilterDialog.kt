1b486a554897bd3626ebbb574b4d47f759abd959
Hunter Werlla <werlla@amazon.com>
2020-08-13 15:32:06 -0700

    values

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/filtering/TagFilterDialog.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/filtering/TagFilterDialog.kt
@@ -3,29 +3,35 @@
 package software.aws.toolkits.jetbrains.core.filtering
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.ui.ComboBox
-import software.amazon.awssdk.services.cloudformation.model.StackSummary
-import software.aws.toolkits.jetbrains.services.cloudformation.resources.CloudFormationResources
-import software.aws.toolkits.jetbrains.services.ecs.EcsUtils
-import software.aws.toolkits.jetbrains.ui.ResourceSelector
-import javax.swing.JComboBox
+import com.intellij.ui.components.JBTextField
 import javax.swing.JPanel
 import javax.swing.JTextField
 
-class TagFilterDialog(private val project: Project): FilterDialog {
+class TagFilterDialog(private val project: Project) : FilterDialog {
     override lateinit var component: JPanel
+    private lateinit var filterName: JTextField
+    private lateinit var keyBox: JTextField
+    private lateinit var valuesField: JBTextField
+
+    init {
+        valuesField.emptyText.text = "TODO all values"
+    }
+
     override fun validate() {
     }
 
     override fun save() {
+        val tags = if (valuesField.text.isBlank()) {
+            listOf()
+        } else {
+            valuesField.text.split(",").map { it.trim() }
+        }
         ResourceFilterManager.getInstance(project).state[filterName.text] = ResourceFilter(
             enabled = true,
-            tags = mapOf(keyBox.text to listOf())
+            tags = mapOf(keyBox.text to tags)
         )
     }
 
-    private lateinit var filterName: JTextField
-    private lateinit var keyBox: JTextField
 
     init {
         // TODO load autocomplete suggestions
