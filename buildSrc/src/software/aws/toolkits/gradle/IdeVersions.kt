9ca14dc274bcdcb78832000ff76b06afc9e637e8
Kyle Thomson <kylthoms@amazon.com>
2020-08-21 11:11:01 -0700

    Removed duplication from ide profile

--- buildSrc/src/software/aws/toolkits/gradle/IdeVersions.kt
+++ buildSrc/src/software/aws/toolkits/gradle/IdeVersions.kt
@@ -5,179 +5,101 @@ package software.aws.toolkits.gradle
 
 import org.gradle.api.Project
 
-enum class ProductCode {
-    IC,
-    IU,
-    RD
-}
-
 open class ProductProfile(
     val sdkVersion: String,
-    val plugins: List<String>
+    val plugins: Array<String>
 )
 
 class RiderProfile(
     sdkVersion: String,
-    plugins: List<String>,
+    plugins: Array<String>,
     val rdGenVersion: String,
     val nugetVersion: String
 ) : ProductProfile(sdkVersion, plugins)
 
-data class Profile(
-    val sinceVersion: String,
-    val untilVersion: String,
-    val products: Map<ProductCode, ProductProfile>
-)
-
-class IdeVersions(private val project: Project) {
-    private val ideProfiles = mapOf(
-        "2019.3" to Profile(
-            sinceVersion = "193",
-            untilVersion = "193.*",
-            products = mapOf(
-                ProductCode.IC to ProductProfile(
-                    sdkVersion = "IC-2019.3",
-                    plugins = listOf(
-                        "org.jetbrains.plugins.terminal",
-                        "org.jetbrains.plugins.yaml",
-                        "PythonCore:193.5233.139",
-                        "java",
-                        "com.intellij.gradle",
-                        "org.jetbrains.idea.maven",
-                        "Docker:193.5233.140"
-                    )
-                ),
-                ProductCode.IU to ProductProfile(
-                    sdkVersion = "IU-2019.3",
-                    plugins = listOf(
-                        "org.jetbrains.plugins.terminal",
-                        "Pythonid:193.5233.109",
-                        "org.jetbrains.plugins.yaml",
-                        "JavaScript",
-                        "JavaScriptDebugger"
-                    )
-                ),
-                ProductCode.RD to RiderProfile(
-                    sdkVersion = "RD-2019.3.4",
-                    rdGenVersion = "0.193.146",
-                    nugetVersion = "2019.3.4",
-                    plugins = listOf(
-                        "org.jetbrains.plugins.yaml"
-                    )
-                )
-            )
-        ),
-        "2020.1" to Profile(
-            sinceVersion = "201",
-            untilVersion = "201.*",
-            products = mapOf(
-                ProductCode.IC to ProductProfile(
-                    sdkVersion = "IC-2020.1",
-                    plugins = listOf(
-                        "org.jetbrains.plugins.terminal",
-                        "org.jetbrains.plugins.yaml",
-                        "PythonCore:201.6668.31",
-                        "java",
-                        "com.intellij.gradle",
-                        "org.jetbrains.idea.maven",
-                        "Docker:201.6668.30"
-                    )
-                ),
-                ProductCode.IU to ProductProfile(
-                    sdkVersion = "IU-2020.1",
-                    plugins = listOf(
-                        "org.jetbrains.plugins.terminal",
-                        "Pythonid:201.6668.31",
-                        "org.jetbrains.plugins.yaml",
-                        "JavaScript",
-                        "JavaScriptDebugger",
-                        "com.intellij.database"
-                    )
-                ),
-                ProductCode.RD to RiderProfile(
-                    sdkVersion = "RD-2020.1.0",
-                    rdGenVersion = "0.201.69",
-                    nugetVersion = "2020.1.0",
-                    plugins = listOf(
-                        "org.jetbrains.plugins.yaml"
-                    )
-                )
-            )
-        ),
-        "2020.2" to Profile(
-            sinceVersion = "202",
-            untilVersion = "202.*",
-            products = mapOf(
-                ProductCode.IC to ProductProfile(
-                    sdkVersion = "IC-2020.2",
-                    plugins = listOf(
-                        "org.jetbrains.plugins.terminal",
-                        "org.jetbrains.plugins.yaml",
-                        "PythonCore:202.6397.124",
-                        "java",
-                        "com.intellij.gradle",
-                        "org.jetbrains.idea.maven",
-                        "Docker:202.6397.93"
-                    )
-                ),
-                ProductCode.IU to ProductProfile(
-                    sdkVersion = "IU-2020.2",
-                    plugins = listOf(
-                        "org.jetbrains.plugins.terminal",
-                        "Pythonid:202.6397.98",
-                        "org.jetbrains.plugins.yaml",
-                        "JavaScript",
-                        "JavaScriptDebugger",
-                        "com.intellij.database"
-                    )
-                ),
-                ProductCode.RD to RiderProfile(
-                    sdkVersion = "RD-2020.2",
-                    rdGenVersion = "0.202.113",
-                    nugetVersion = "2020.2.0",
-                    plugins = listOf(
-                        "org.jetbrains.plugins.yaml"
-                    )
-                )
-            )
-        )
+class Profile(
+    val name: String,
+    val shortName: String = shortenedIdeProfileName(name),
+    val sinceVersion: String = shortName,
+    val untilVersion: String = "$sinceVersion.*",
+    pythonVersion: String,
+    communityPythonVersion: String = pythonVersion,
+    dockerVersion: String,
+    riderSdkOverride: String? = null,
+    rdGenVersion: String,
+    nugetVersion: String
+) {
+    private val commonPlugins = arrayOf(
+        "org.jetbrains.plugins.terminal",
+        "org.jetbrains.plugins.yaml"
     )
 
-    fun sinceVersion(): String = getProfile().sinceVersion
-    fun untilVersion() = getProfile().untilVersion
+    private val ultimatePlugins = commonPlugins + arrayOf(
+        "Pythonid:$pythonVersion",
+        "JavaScript",
+        "JavaScriptDebugger"
+    )
 
-    fun sdkVersion(code: ProductCode): String = getProductProfile(code).sdkVersion
-    fun plugins(code: ProductCode): List<String> = getProductProfile(code).plugins
+    private val communityPlugins = commonPlugins + arrayOf(
+        "PythonCore:$communityPythonVersion",
+        "java",
+        "com.intellij.gradle",
+        "org.jetbrains.idea.maven",
+        "Docker:$dockerVersion"
+    )
 
-    fun rdGenVersion(): String = getRiderProfile().rdGenVersion
-    fun nugetSdkVersion(): String = getRiderProfile().nugetVersion
+    val community: ProductProfile = ProductProfile(sdkVersion = "IC-$name", plugins = communityPlugins)
+    val ultimate: ProductProfile = ProductProfile(sdkVersion = "IU-$name", plugins = ultimatePlugins)
+    val rider: RiderProfile = RiderProfile(
+        sdkVersion = riderSdkOverride ?: "RD-$name",
+        plugins = arrayOf("org.jetbrains.plugins.yaml"),
+        rdGenVersion = rdGenVersion,
+        nugetVersion = nugetVersion
+    )
+}
 
-    // Convert (as an example) 2020.2 -> 202
-    fun resolveShortenedIdeProfileName(): String {
-        val profileName = resolveIdeProfileName().trim()
-        val parts = profileName.split(".")
-        return parts[0].substring(2) + parts[1]
+object IdeVersions {
+    fun ideProfile(project: Project): Profile {
+        val profileName = resolveIdeProfileName(project)
+        return ideProfiles[profileName] ?: throw IllegalStateException("Can't find profile for $profileName")
     }
 
-    fun ideSdkVersion(code: ProductCode): String = ideProfiles[resolveIdeProfileName()]
-        ?.products
-        ?.get(code)
-        ?.sdkVersion
-        ?: throw IllegalArgumentException("Product not in map of IDE versions: ${resolveIdeProfileName()}, $code")
+    private val ideProfiles = listOf(
+        Profile(
+            name = "2019.3",
+            pythonVersion = "193.5233.109",
+            communityPythonVersion = "193.5233.139",
+            dockerVersion = "193.5233.140",
+            riderSdkOverride = "RD-2019.3.4",
+            rdGenVersion = "0.193.146",
+            nugetVersion = "2019.3.4"
+        ),
+        Profile(
+            name = "2020.1",
+            pythonVersion = "201.6668.31",
+            dockerVersion = "201.6668.30",
+            riderSdkOverride = "RD-2020.1.0",
+            rdGenVersion = "0.201.69",
+            nugetVersion = "2020.1.0"
+        ),
+        Profile(
+            name = "2020.2",
+            pythonVersion = "202.6397.98",
+            communityPythonVersion = "202.6397.124",
+            dockerVersion = "202.6397.93",
+            rdGenVersion = "0.202.113",
+            nugetVersion = "2020.2.0"
+        )
+    ).map { it.name to it }.toMap()
 
-    fun resolveIdeProfileName(): String = if (System.getenv()["ALTERNATIVE_IDE_PROFILE_NAME"] != null) {
+    private fun resolveIdeProfileName(project: Project): String = if (System.getenv()["ALTERNATIVE_IDE_PROFILE_NAME"] != null) {
         System.getenv("ALTERNATIVE_IDE_PROFILE_NAME")
     } else {
         project.properties["ideProfileName"]?.toString() ?: throw IllegalStateException("No ideProfileName property set")
     }
+}
 
-    private fun getProfile(): Profile =
-        ideProfiles[resolveIdeProfileName()] ?: throw IllegalStateException("Unable to resolve profile ${resolveIdeProfileName()}")
-
-    private fun getProductProfile(code: ProductCode): ProductProfile =
-        ideProfiles[resolveIdeProfileName()]?.products?.get(code)
-            ?: throw IllegalStateException("Unable to get profile ${resolveIdeProfileName()} code $code")
-
-    private fun getRiderProfile(): RiderProfile = ideProfiles[resolveIdeProfileName()]?.products?.get(ProductCode.RD) as? RiderProfile
-        ?: throw IllegalStateException("Failed to get Rider profile for ${resolveIdeProfileName()}!")
+private fun shortenedIdeProfileName(sdkName: String): String {
+    val parts = sdkName.trim().split(".")
+    return parts[0].substring(2) + parts[1]
 }
