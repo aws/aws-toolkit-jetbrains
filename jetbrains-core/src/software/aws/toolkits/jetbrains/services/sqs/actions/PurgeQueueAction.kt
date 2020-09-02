2c27868f3fedb9783e61b66d81d8c031748d0140
Hunter Werlla <werlla@amazon.com>
2020-09-02 10:19:53 -0700

    ktlint and coroutines

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/sqs/actions/PurgeQueueAction.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/sqs/actions/PurgeQueueAction.kt
@@ -5,36 +5,44 @@ package software.aws.toolkits.jetbrains.services.sqs.actions
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import software.amazon.awssdk.services.sqs.SqsClient
 import software.amazon.awssdk.services.sqs.model.PurgeQueueInProgressException
 import software.aws.toolkits.jetbrains.core.awsClient
 import software.aws.toolkits.jetbrains.core.explorer.actions.SingleResourceNodeAction
 import software.aws.toolkits.jetbrains.services.sqs.SqsQueueNode
+import software.aws.toolkits.jetbrains.utils.ApplicationThreadPoolScope
 import software.aws.toolkits.jetbrains.utils.notifyError
 import software.aws.toolkits.jetbrains.utils.notifyInfo
 import software.aws.toolkits.resources.message
 
-class PurgeQueueAction : SingleResourceNodeAction<SqsQueueNode>(message("sqs.purge_queue")), DumbAware {
+class PurgeQueueAction :
+    SingleResourceNodeAction<SqsQueueNode>(message("sqs.purge_queue")),
+    CoroutineScope by ApplicationThreadPoolScope("PurgeQueueAction "),
+    DumbAware {
     override fun actionPerformed(selected: SqsQueueNode, e: AnActionEvent) {
         val project = selected.nodeProject
         val client: SqsClient = project.awsClient()
-        try {
-            client.purgeQueue { it.queueUrl(selected.queueUrl) }
-            notifyInfo(
-                project = project,
-                title = message("aws.notification.title"),
-                content = message("sqs.purge_queue.succeeded", selected.queueUrl)
-            )
-        } catch (e: PurgeQueueInProgressException) {
-            notifyError(
-                project = project,
-                content = message("sqs.purge_queue.failed.60_seconds", selected.queueUrl)
-            )
-        } catch (e: Exception) {
-            notifyError(
-                project = project,
-                content = message("sqs.purge_queue.failed", selected.queueUrl)
-            )
+        launch {
+            try {
+                client.purgeQueue { it.queueUrl(selected.queueUrl) }
+                notifyInfo(
+                    project = project,
+                    title = message("aws.notification.title"),
+                    content = message("sqs.purge_queue.succeeded", selected.queueUrl)
+                )
+            } catch (e: PurgeQueueInProgressException) {
+                notifyError(
+                    project = project,
+                    content = message("sqs.purge_queue.failed.60_seconds", selected.queueUrl)
+                )
+            } catch (e: Exception) {
+                notifyError(
+                    project = project,
+                    content = message("sqs.purge_queue.failed", selected.queueUrl)
+                )
+            }
         }
     }
 }
