0c01f22cba8de584573040f78f98d3e6e2e0280a
Austin Brooks <alfredbr@amazon.com>
2020-10-20 10:57:22 -0700

    Fix being unable to use a SSO profile in a source_profile chain
    * Added test around InteractiveCredentials

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/SsoSupport.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/SsoSupport.kt
@@ -56,5 +56,7 @@ interface SsoRequiredInteractiveCredentials : InteractiveCredential {
 
     override val userAction: AnAction get() = RefreshConnectionAction(message("credentials.sso.action"))
 
-    override suspend fun userActionRequired(): Boolean = ssoCache.loadAccessToken(ssoUrl) == null
+    override suspend fun userActionRequired(): Boolean {
+        return ssoCache.loadAccessToken(ssoUrl) == null
+    }
 }
