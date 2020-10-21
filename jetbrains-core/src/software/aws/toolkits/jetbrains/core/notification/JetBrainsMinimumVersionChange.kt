a6197e05a5efa009b27298f4f3c01449cc88bf2f
Hunter Werlla <werlla@amazon.com>
2020-10-21 10:04:18 -0700

    Start updating min to 201

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/notification/JetBrainsMinimumVersionChange.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/notification/JetBrainsMinimumVersionChange.kt
@@ -8,14 +8,14 @@ import com.intellij.openapi.application.ApplicationNamesInfo
 import software.aws.toolkits.resources.message
 
 class JetBrainsMinimumVersionChange : NoticeType {
-    override val id: String = "JetBrainsMinimumVersion_193"
+    override val id: String = "JetBrainsMinimumVersion_201"
     private val noticeContents = NoticeContents(
         message("notice.title.jetbrains.minimum.version"),
         message(
             "notice.message.jetbrains.minimum.version",
             ApplicationInfo.getInstance().fullVersion,
             ApplicationNamesInfo.getInstance().fullProductName,
-            "2019.3"
+            "2020.1"
         )
     )
 
@@ -31,12 +31,9 @@ class JetBrainsMinimumVersionChange : NoticeType {
     override fun isNotificationRequired(): Boolean {
         val appInfo = ApplicationInfo.getInstance()
         val majorVersion = appInfo.majorVersion.toIntOrNull()
-        val minorVersion = appInfo.minorVersion.toFloatOrNull()
 
         majorVersion?.let {
-            minorVersion?.let {
-                return majorVersion < 2019 || (majorVersion == 2019 && minorVersion < 3)
-            }
+            return majorVersion < 2020
         }
 
         return true
