9539d905cdc25b31af7f953192d5f8fcc7ad81d5
Austin Brooks <alfredbr@amazon.com>
2020-11-04 11:53:06 -0800

    Fix test compilation

--- jetbrains-core/tst/software/aws/toolkits/jetbrains/services/lambda/upload/UpdateLambdaConfigurationTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/services/lambda/upload/UpdateLambdaConfigurationTest.kt
@@ -1,7 +1,7 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package software.aws.toolkits.jetbrains.services.lambda.upload.steps
+package software.aws.toolkits.jetbrains.services.lambda.upload
 
 import com.intellij.testFramework.ProjectRule
 import com.nhaarman.mockitokotlin2.any
@@ -19,7 +19,6 @@ import software.amazon.awssdk.services.lambda.model.UpdateFunctionConfigurationR
 import software.aws.toolkits.core.utils.test.aString
 import software.aws.toolkits.jetbrains.core.MockClientManagerRule
 import software.aws.toolkits.jetbrains.services.iam.IamRole
-import software.aws.toolkits.jetbrains.services.lambda.upload.FunctionDetails
 
 class UpdateLambdaConfigurationTest {
     @Rule
