59e331d7794cea06e6ec1b163387517c4c786b41
Hunter Werlla <werlla@amazon.com>
2021-02-15 13:02:40 -0800

    UI test works

--- ui-tests/tst/software/aws/toolkits/jetbrains/uitests/tests/SamRunConfigTest.kt
+++ ui-tests/tst/software/aws/toolkits/jetbrains/uitests/tests/SamRunConfigTest.kt
@@ -3,10 +3,14 @@
 
 package software.aws.toolkits.jetbrains.uitests.tests
 
+import com.intellij.remoterobot.fixtures.ComboBoxFixture
+import com.intellij.remoterobot.fixtures.ContainerFixture
+import com.intellij.remoterobot.fixtures.JListFixture
 import com.intellij.remoterobot.search.locators.byXpath
 import com.intellij.remoterobot.stepsProcessing.step
 import com.intellij.remoterobot.utils.keyboard
 import org.apache.commons.io.FileUtils
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -22,10 +26,14 @@ import software.aws.toolkits.jetbrains.uitests.fixtures.welcomeFrame
 import software.aws.toolkits.jetbrains.uitests.utils.setupSamCli
 import java.nio.file.Path
 import java.nio.file.Paths
+import java.time.Duration
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SamRunConfigTest {
     private val dataPath = Paths.get(System.getProperty("testDataPath")).resolve("samProjects/zip/java11")
+    private val input = """
+        {"data": "${dataPath.hashCode()}"}
+    """.trimIndent()
 
     @TempDir
     lateinit var tempDir: Path
@@ -47,21 +55,48 @@ class SamRunConfigTest {
 
             idea {
                 waitForBackgroundTasks()
-                step("Add a run configuration") {
-                    findAndClick("//div[@accessiblename='Add Configuration...']")
-                    val dialog = find<DialogFixture>(DialogFixture.byTitleContains("Run"))
+                findAndClick("//div[@accessiblename='Add Configuration...']")
+                val dialog = step("Add a run configuration") {
+                    val dialog = find<DialogFixture>(DialogFixture.byTitleContains("Run"), Duration.ofSeconds(5))
                     dialog.findAndClick("//div[@accessiblename='Add New Configuration']")
                     find<JTreeFixture>(byXpath("//div[@accessiblename='WizardTree' and @class='MyTree']")).clickPath("AWS Lambda", "Local")
+                    dialog
                 }
                 step("Populate run configuration") {
-                    findAndClick("//div[@text='From template']")
-                    findAndClick("//div[@class='Wrapper']//div[@class='TextFieldWithBrowseButton']")
-                    keyboard { enterText(tempDir.resolve("template.yml").toAbsolutePath().toString()) }
-                    findAndClick("//div[@class='MyEditorTextField']")
-                    keyboard { enterText("{}") }
+                    step("Set up function from template") {
+                        findAndClick("//div[@text='From template']")
+                        findAndClick("//div[@class='Wrapper']//div[@class='TextFieldWithBrowseButton']")
+                        keyboard { enterText(tempDir.resolve("template.yaml").toAbsolutePath().toString()) }
+                    }
+                    step("Assert validation works by checking the error") {
+                        assertThat(dialog.findAllText()).anySatisfy { assertThat(it.text).contains("Must specify an input") }
+                    }
+                    step("Enter text") {
+                        findAndClick("//div[@class='MyEditorTextField']")
+                        keyboard { enterText(input) }
+                    }
                     pressOk()
+                }
+                step("Validate run configuration was saved and loads properly") {
+                    step("Reopen the run configuration") {
+                        findAndClick("//div[@accessiblename='[Local] SomeFunction']")
+                        find<JListFixture>(byXpath("//div[@class='MyList']")).selectItem("Edit Configurations...")
+                    }
+                    step("Assert the same function is selected") {
+                        assertThat(functionModel().selectedText()).isEqualTo("SomeFunction")
+                    }
+                    step("Assert the run configuration has no errors") {
+                        assertThat(dialog.findAllText()).noneSatisfy { assertThat(it.text).contains("Error") }
+                    }
+                    // TODO the field is a JTextComponent so findAllText does not work and we don't have a JComponent
+                    // step("Assert the input is the same") {
+                    //    assertThat(dialog.findAllText()).anySatisfy { assertThat(it.text).isEqualTo(input) }
+                    // }
                 }
             }
         }
     }
+
+    private fun ContainerFixture.functionModel(): ComboBoxFixture =
+        find(byXpath("//div[@class='TextFieldWithBrowseButton']/following-sibling::div[@class='ComboBox']"))
 }
