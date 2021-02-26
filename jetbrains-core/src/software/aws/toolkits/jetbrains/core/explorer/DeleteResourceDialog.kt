530f22c2a06a9ff6f24711ecb267397db2fa06cc
Manodnya Jaydeep Bhoite <manodnyb@amazon.com>
2021-02-26 13:20:39 -0800

    Ok button disabled until delete me entered

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/DeleteResourceDialog.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/DeleteResourceDialog.kt
@@ -6,7 +6,6 @@ package software.aws.toolkits.jetbrains.core.explorer
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.Messages
-import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.ui.DocumentAdapter
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextField
@@ -49,20 +48,10 @@ class DeleteResourceDialog(
 
     override fun createCenterPanel(): JComponent = component
 
-    override fun doValidate(): ValidationInfo? = validateDeleteConfirmation(deleteResourceConfirmation)
-
     override fun doOKAction() {
-        if (doValidateAll().isNotEmpty()) return
         if (!okAction.isEnabled) {
             return
         }
         close(OK_EXIT_CODE)
-    }
-
-    private fun validateDeleteConfirmation(deleteResourceConfirmation: JBTextField): ValidationInfo? {
-        if (deleteResourceConfirmation.text != message("delete_resource.confirmation_text")) {
-            return ValidationInfo(message("delete_resource.valid_entry_check"), deleteResourceConfirmation)
-        }
-        return null
     }
 }
