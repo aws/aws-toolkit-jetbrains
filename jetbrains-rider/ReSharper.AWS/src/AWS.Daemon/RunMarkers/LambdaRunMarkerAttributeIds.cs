3d52aaf071c082477ff9c03d10c3ac5327a5f22a
Hunter Werlla <werlla@amazon.com>
2021-03-15 14:35:28 -0700

    Deleted a !PROFILE_2020_1 on accident

--- jetbrains-rider/ReSharper.AWS/src/AWS.Daemon/RunMarkers/LambdaRunMarkerAttributeIds.cs
+++ jetbrains-rider/ReSharper.AWS/src/AWS.Daemon/RunMarkers/LambdaRunMarkerAttributeIds.cs
@@ -3,6 +3,12 @@ using JetBrains.TextControl.DocumentMarkup;
 
 namespace AWS.Daemon.RunMarkers
 {
+    [RegisterHighlighter(
+        LAMBDA_RUN_METHOD_MARKER_ID,
+        GutterMarkType = typeof(LambdaMethodRunMarkerGutterMark),
+        EffectType = EffectType.GUTTER_MARK,
+        Layer = HighlighterLayer.SYNTAX + 1
+    )]
     public static class LambdaRunMarkerAttributeIds
     {
         public const string LAMBDA_RUN_METHOD_MARKER_ID = "AWS Lambda Run Method Gutter Mark";
