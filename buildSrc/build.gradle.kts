4a0c6fa90bfe56c105c4ee668b9117899698126c
Austin Brooks <alfredbr@amazon.com>
2021-03-29 11:25:42 -0700

    Move versions to properties

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,16 +1,6 @@
 // Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-val jacksonVersion: String by project
-val kotlinVersion: String by project
-val awsSdkVersion: String by project
-
-val assertjVersion: String by project
-val junitVersion: String by project
-val mockitoVersion: String by project
-val mockitoKotlinVersion: String by project
-val ideaPluginVersion: String by project
-
 buildscript {
     // This has to be here otherwise properties are not loaded and nothing works
     val props = java.util.Properties()
@@ -27,21 +17,21 @@ plugins {
 dependencies {
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-    implementation("org.eclipse.jgit:org.eclipse.jgit:5.0.2.201807311906-r")
-    implementation("org.commonmark:commonmark:0.17.1")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:$jgitVersion")
+    implementation("org.commonmark:commonmark:$commonsMarkVersion")
 
     implementation("software.amazon.awssdk:codegen:$awsSdkVersion")
 
     implementation("org.jetbrains.intellij.plugins:gradle-intellij-plugin:$ideaPluginVersion")
 
-    implementation("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
+    implementation("org.jlleitschuh.gradle:ktlint-gradle:$ktintPluginVersion")
     compileOnly("com.pinterest.ktlint:ktlint-core:$ktlintVersion")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     testImplementation("com.pinterest.ktlint:ktlint-core:$ktlintVersion")
     testImplementation("com.pinterest.ktlint:ktlint-test:$ktlintVersion")
 
-    implementation("org.gradle:test-retry-gradle-plugin:1.2.1")
-    implementation("com.adarshr:gradle-test-logger-plugin:2.1.1")
+    implementation("org.gradle:test-retry-gradle-plugin:$gradleRetryPluginVersion")
+    implementation("com.adarshr:gradle-test-logger-plugin:$gradleTestLoggerPlugin")
 
     testImplementation("org.assertj:assertj-core:$assertjVersion")
     testImplementation("junit:junit:$junitVersion")
