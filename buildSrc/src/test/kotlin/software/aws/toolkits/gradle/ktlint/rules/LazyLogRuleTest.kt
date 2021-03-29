44070ecaf151568f264a440dde1d997f0f416d20
Austin Brooks <alfredbr@amazon.com>
2021-03-29 11:55:17 -0700

    Add back ui test hack in ktlint

--- buildSrc/src/test/kotlin/software/aws/toolkits/gradle/ktlint/rules/LazyLogRuleTest.kt
+++ buildSrc/src/test/kotlin/software/aws/toolkits/gradle/ktlint/rules/LazyLogRuleTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
 package software.aws.toolkits.gradle.ktlint.rules
@@ -54,6 +54,24 @@ val LOG = LoggerFactory.getLogger(T::class.java)
 fun foo() {
     val e = RuntimeException()
     LOG.debug(e) {"Hi" }
+}
+                """.trimIndent()
+            )
+        ).isEmpty()
+    }
+
+    @Test
+    fun methodCallIsUsedToLogInUiTests() {
+        assertThat(
+            rule.lint(
+                """
+package software.aws.toolkits.jetbrains.uitests.really.cool.test
+
+import org.slf4j.LoggerFactory
+
+val LOG = LoggerFactory.getLogger(T::class.java)
+fun foo() {
+    LOG.debug("Hi")
 }
                 """.trimIndent()
             )
