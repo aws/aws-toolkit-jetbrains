b80c13a2cd9c651d7a7b79eac82d067dc831d077
Austin Brooks <alfredbr@amazon.com>
2021-03-30 11:35:40 -0700

    Triage more

--- buildSrc/src/main/kotlin/toolkit-intellij-subplugin.gradle.kts
+++ buildSrc/src/main/kotlin/toolkit-intellij-subplugin.gradle.kts
@@ -3,7 +3,6 @@
 
 import org.jetbrains.intellij.tasks.DownloadRobotServerPluginTask
 import org.jetbrains.intellij.tasks.RunIdeForUiTestTask
-import org.jetbrains.intellij.tasks.RunIdeTask
 import software.aws.toolkits.gradle.IdeVersions
 import software.aws.toolkits.gradle.ciOnly
 import software.aws.toolkits.gradle.findFolders
@@ -23,6 +22,38 @@ plugins {
     id("toolkit-testing")
     id("org.jetbrains.intellij")
 }
+
+// Add our source sets per IDE profile version (i.e. src-211)
+sourceSets {
+    main {
+        java.srcDirs(findFolders(project, "src", ideProfile))
+        resources.srcDirs(findFolders(project, "resources", ideProfile))
+    }
+    test {
+        java.srcDirs(findFolders(project, "tst", ideProfile))
+        resources.srcDirs(findFolders(project, "tst-resources", ideProfile))
+    }
+
+    plugins.withType<ToolkitIntegrationTestingPlugin> {
+        maybeCreate("integrationTest").apply {
+            java.srcDirs(findFolders(project, "it", ideProfile))
+            resources.srcDirs(findFolders(project, "it-resources", ideProfile))
+        }
+    }
+}
+
+configurations {
+    runtimeClasspath {
+        // Exclude dependencies that ship with iDE
+        exclude(group = "org.slf4j")
+        exclude(group = "org.jetbrains.kotlin")
+        exclude(group = "org.jetbrains.kotlinx")
+
+        // Exclude dependencies we don't use to make plugin smaller
+        exclude(group = "software.amazon.awssdk", module = "netty-nio-client")
+    }
+}
+
 
 // Run after the project has been evaluated so that the extension (intellijToolkit) has been configured
 afterEvaluate {
@@ -56,91 +87,58 @@ afterEvaluate {
     tasks.buildSearchableOptions {
         enabled = false
     }
-}
 
-// Add our source sets per IDE profile version (i.e. src-211)
-sourceSets {
-    main {
-        java.srcDirs(findFolders(project, "src", ideProfile))
-        resources.srcDirs(findFolders(project, "resources", ideProfile))
-    }
-    test {
-        java.srcDirs(findFolders(project, "tst", ideProfile))
-        resources.srcDirs(findFolders(project, "tst-resources", ideProfile))
+    tasks.withType<Test>().all {
+        systemProperty("log.dir", "${intellij.sandboxDirectory}-test/logs")
+        systemProperty("testDataPath", project.rootDir.resolve("testdata").absolutePath)
     }
 
-    plugins.withType<ToolkitIntegrationTestingPlugin> {
-        maybeCreate("integrationTest").apply {
-            java.srcDirs(findFolders(project, "it", ideProfile))
-            resources.srcDirs(findFolders(project, "it-resources", ideProfile))
-        }
+    tasks.withType<JavaExec> {
+        systemProperty("aws.toolkits.enableTelemetry", false)
     }
-}
 
-tasks.withType<Test>().all {
-    systemProperty("log.dir", "${intellij.sandboxDirectory}-test/logs")
-    systemProperty("testDataPath", project.rootDir.resolve("testdata").absolutePath)
-}
-
-tasks.withType<JavaExec> {
-    systemProperty("aws.toolkits.enableTelemetry", false)
-}
-
-tasks.withType<RunIdeTask> {
-    val alternativeIde = System.getenv("ALTERNATIVE_IDE")
-    if (alternativeIde != null) {
-        // remove the trailing slash if there is one or else it will not work
-        val path = alternativeIde.trimEnd('/')
-        if (File(path).exists()) {
-            setIdeDirectory(path)
-        } else {
-            throw GradleException("ALTERNATIVE_IDE path not found $alternativeIde")
+    tasks.runIde {
+        val alternativeIde = System.getenv("ALTERNATIVE_IDE")
+        if (alternativeIde != null) {
+            // remove the trailing slash if there is one or else it will not work
+            val path = alternativeIde.trimEnd('/')
+            if (File(path).exists()) {
+                setIdeDirectory(path)
+            } else {
+                throw GradleException("ALTERNATIVE_IDE path not found $alternativeIde")
+            }
         }
     }
 
-    maxHeapSize = "1G"
-}
-
-tasks.withType<DownloadRobotServerPluginTask>() {
-    version = remoteRobotVersion
-}
-
-// Enable coverage for the UI test target IDE
-extensions.getByType<JacocoPluginExtension>().applyTo(tasks.withType<RunIdeForUiTestTask>())
-tasks.withType<RunIdeForUiTestTask>().all {
-    systemProperty("robot-server.port", remoteRobotPort)
-    systemProperty("ide.mac.file.chooser.native", "false")
-    systemProperty("jb.consents.confirmation.enabled", "false")
-    // This does some magic in EndUserAgreement.java to make it not show the privacy policy
-    systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-    // This only works on 2020.3+ FIX_WHEN_MIN_IS_203 remove this explanation
-    systemProperty("ide.show.tips.on.startup.default.value", false)
-
-    systemProperty("aws.telemetry.skip_prompt", "true")
-    systemProperty("aws.suppress_deprecation_prompt", true)
-    ciOnly {
-        systemProperty("aws.sharedCredentialsFile", "/tmp/.aws/credentials")
+    tasks.withType<DownloadRobotServerPluginTask>() {
+        version = remoteRobotVersion
     }
 
-    debugOptions {
-        enabled.set(true)
-        suspend.set(false)
-    }
+    // Enable coverage for the UI test target IDE
+    extensions.getByType<JacocoPluginExtension>().applyTo(tasks.withType<RunIdeForUiTestTask>())
+    tasks.withType<RunIdeForUiTestTask>().all {
+        systemProperty("robot-server.port", remoteRobotPort)
+        systemProperty("ide.mac.file.chooser.native", "false")
+        systemProperty("jb.consents.confirmation.enabled", "false")
+        // This does some magic in EndUserAgreement.java to make it not show the privacy policy
+        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
+        // This only works on 2020.3+ FIX_WHEN_MIN_IS_203 remove this explanation
+        systemProperty("ide.show.tips.on.startup.default.value", false)
 
-    configure<JacocoTaskExtension> {
-        includes = listOf("software.aws.toolkits.*")
-        setDestinationFile(file("$buildDir/jacoco/${Instant.now()}-jacocoUiTests.exec"))
-    }
-}
+        systemProperty("aws.telemetry.skip_prompt", "true")
+        systemProperty("aws.suppress_deprecation_prompt", true)
+        ciOnly {
+            systemProperty("aws.sharedCredentialsFile", "/tmp/.aws/credentials")
+        }
 
-configurations {
-    runtimeClasspath {
-        // Exclude dependencies that ship with iDE
-        exclude(group = "org.slf4j")
-        exclude(group = "org.jetbrains.kotlin")
-        exclude(group = "org.jetbrains.kotlinx")
+        debugOptions {
+            enabled.set(true)
+            suspend.set(false)
+        }
 
-        // Exclude dependencies we don't use to make plugin smaller
-        exclude(group = "software.amazon.awssdk", module = "netty-nio-client")
+        configure<JacocoTaskExtension> {
+            includes = listOf("software.aws.toolkits.*")
+            setDestinationFile(file("$buildDir/jacoco/${Instant.now()}-jacocoUiTests.exec"))
+        }
     }
 }
