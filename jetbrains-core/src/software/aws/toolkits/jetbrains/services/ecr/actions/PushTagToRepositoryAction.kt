4f9ebdb896cd81421c58f34eb707f95ad09e5c05
Richard Li <richali@amazon.com>
2021-04-05 17:44:18 -0700

    polish/fix validation

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/ecr/actions/PushTagToRepositoryAction.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/ecr/actions/PushTagToRepositoryAction.kt
@@ -20,17 +20,24 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import software.amazon.awssdk.core.exception.SdkException
 import software.amazon.awssdk.services.ecr.EcrClient
+import software.aws.toolkits.core.utils.error
+import software.aws.toolkits.core.utils.getLogger
 import software.aws.toolkits.jetbrains.core.awsClient
+import software.aws.toolkits.jetbrains.core.explorer.ExplorerDataKeys
 import software.aws.toolkits.jetbrains.services.ecr.EcrPushRequest
+import software.aws.toolkits.jetbrains.services.ecr.EcrRepositoryNode
 import software.aws.toolkits.jetbrains.services.ecr.getDockerLogin
 import software.aws.toolkits.jetbrains.services.ecr.getDockerServerRuntimeInstance
 import software.aws.toolkits.jetbrains.services.ecr.pushImage
 import software.aws.toolkits.jetbrains.services.ecr.resources.EcrResources
+import software.aws.toolkits.jetbrains.services.ecr.resources.Repository
 import software.aws.toolkits.jetbrains.services.ecr.toLocalImageList
 import software.aws.toolkits.jetbrains.services.ecs.execution.DockerUtil
 import software.aws.toolkits.jetbrains.ui.ResourceSelector
 import software.aws.toolkits.jetbrains.utils.ApplicationThreadPoolScope
+import software.aws.toolkits.jetbrains.utils.notifyError
 import software.aws.toolkits.jetbrains.utils.ui.blankAsNull
 import software.aws.toolkits.jetbrains.utils.ui.selected
 import software.aws.toolkits.resources.message
@@ -49,7 +56,13 @@ class PushTagToRepositoryAction :
         val project = e.getRequiredData(LangDataKeys.PROJECT)
         val client: EcrClient = project.awsClient()
 
-        val dialog = PushToEcrDialog(project, dockerServerRuntime)
+        val selectedRepository = e.getData(ExplorerDataKeys.SELECTED_NODES)
+            ?.mapNotNull { it as? EcrRepositoryNode }
+            ?.takeIf { it.size == 1 }
+            ?.first()
+            ?.repository
+
+        val dialog = PushToEcrDialog(project, selectedRepository, dockerServerRuntime)
         val result = dialog.showAndGet()
 
         if (!result) {
@@ -59,13 +72,29 @@ class PushTagToRepositoryAction :
         val pushRequest = dialog.getPushRequest()
 
         launch {
-            val authData = withContext(Dispatchers.IO) {
-                client.authorizationToken.authorizationData().first()
-            }
+            try {
+                val authData = withContext(Dispatchers.IO) {
+                    client.authorizationToken.authorizationData().first()
+                }
 
-            val ecrLogin = authData.getDockerLogin()
-            pushImage(project, dockerServerRuntime.await(), ecrLogin, pushRequest)
+                val ecrLogin = authData.getDockerLogin()
+                pushImage(project, dockerServerRuntime.await(), ecrLogin, pushRequest)
+            } catch (e: SdkException) {
+                val message = message("ecr.push.credential_fetch_failed")
+
+                LOG.error(e) { message }
+                notifyError(message("ecr.push.title"), message)
+            } catch (e: Exception) {
+                val message = message("ecr.push.unknown_exception")
+
+                LOG.error(e) { message }
+                notifyError(message("ecr.push.title"), message)
+            }
         }
+    }
+
+    companion object {
+        private val LOG = getLogger<PushTagToRepositoryAction>()
     }
 }
 
@@ -76,6 +105,7 @@ internal data class LocalImage(
 
 internal class PushToEcrDialog(
     project: Project,
+    selectedRepository: Repository?,
     private val dockerServerRuntime: Deferred<DockerServerRuntimeInstance>
 ) : DialogWrapper(project, null, false, IdeModalityType.PROJECT),
     CoroutineScope by ApplicationThreadPoolScope("PushRepositoryDialog") {
@@ -90,6 +120,8 @@ internal class PushToEcrDialog(
         .build()
 
     init {
+        selectedRepository?.let { repo -> remoteRepos.selectedItem { it == repo } }
+
         title = message("ecr.push.title")
         setOKButtonText(message("ecr.push.confirm"))
 
@@ -104,8 +136,13 @@ internal class PushToEcrDialog(
     }
 
     override fun createCenterPanel() = panel {
+        // attach a group so combo boxes grow/shrink together
+        val selectorGroup = "selectors"
+
         row(message("ecr.repo.label")) {
             component(remoteRepos)
+                .sizeGroup(selectorGroup)
+                .withErrorOnApplyIf(message("ecr.repo.not_selected")) { it.selected() == null }
         }
 
         row(message("ecr.push.source")) {
@@ -117,11 +154,14 @@ internal class PushToEcrDialog(
                 listCellRenderer { value, _, _ ->
                     text = value.tag ?: value.imageId.take(15)
                 }
-            ).withErrorOnApplyIf(message("ecr.repo.not_selected")) { it.selected() == null }
+            )
+                .sizeGroup(selectorGroup)
+                .withErrorOnApplyIf(message("ecr.image.not_selected")) { it.selected() == null }
         }
 
         row(message("ecr.push.remoteTag")) {
             textField(::remoteTag)
+                .sizeGroup(selectorGroup)
                 .withErrorOnApplyIf(message("ecr.tag.not_provided")) { it.blankAsNull() == null }
         }
     }
