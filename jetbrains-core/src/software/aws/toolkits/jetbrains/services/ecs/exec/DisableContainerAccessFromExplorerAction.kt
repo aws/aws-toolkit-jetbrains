d759bf32485f780818204ff14a800662dc5e5071
Manodnya Jaydeep Bhoite <manodnyb@amazon.com>
2021-04-19 08:34:32 -0700

    Initial Changes

---
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/ecs/exec/DisableContainerAccessFromExplorerAction.kt
@@ -0,0 +1,15 @@
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.ecs.exec
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import software.aws.toolkits.jetbrains.core.explorer.actions.SingleResourceNodeAction
+import software.aws.toolkits.jetbrains.services.ecs.EcsServiceNode
+
+class DisableContainerAccessFromExplorerAction:
+    SingleResourceNodeAction<EcsServiceNode>("Disable Container Access", null) {
+    override fun actionPerformed(selected: EcsServiceNode, e: AnActionEvent) {
+        TODO("Not yet implemented")
+    }
+}
