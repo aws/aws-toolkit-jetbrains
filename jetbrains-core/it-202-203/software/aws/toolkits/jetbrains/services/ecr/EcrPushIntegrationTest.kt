7b384319e0d19e91bef2fb827e89fa71bb3ed8a1
Richard Li <742829+rli@users.noreply.github.com>
2021-04-29 10:46:09 -0700

    Add support for build and push to ECR from local Dockerfile (#2553)

--- jetbrains-core/it-202-203/software/aws/toolkits/jetbrains/services/ecr/EcrPushIntegrationTest.kt
+++ jetbrains-core/it-202-203/software/aws/toolkits/jetbrains/services/ecr/EcrPushIntegrationTest.kt
@@ -3,6 +3,7 @@
 
 package software.aws.toolkits.jetbrains.services.ecr
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.intellij.docker.DockerCloudConfiguration
 import com.intellij.docker.remote.run.runtime.DockerAgentBuildImageConfig
 import com.intellij.docker.remote.run.runtime.RemoteDockerApplicationRuntime
@@ -10,12 +11,16 @@ import com.intellij.docker.remote.run.runtime.RemoteDockerRuntime
 import com.intellij.testFramework.ProjectRule
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.ecr.EcrClient
+import software.amazon.awssdk.services.ecr.model.Image
+import software.amazon.awssdk.services.ecr.model.ImageIdentifier
 import software.aws.toolkits.core.rules.EcrTemporaryRepositoryRule
+import software.aws.toolkits.jetbrains.services.ecr.resources.Repository
 import java.util.UUID
 
 class EcrPushIntegrationTest {
@@ -35,9 +40,15 @@ class EcrPushIntegrationTest {
     @JvmField
     val ecrRule = EcrTemporaryRepositoryRule(ecrClient)
 
+    private lateinit var remoteRepo: Repository
+
+    @Before
+    fun setUp() {
+        remoteRepo = ecrRule.createRepository().toToolkitEcrRepository()!!
+    }
+
     @Test
-    fun testPush() {
-        val remoteRepo = ecrRule.createRepository().toToolkitEcrRepository()!!
+    fun testPushFromImage() {
         val remoteTag = UUID.randomUUID().toString()
 
         val dockerfile = folder.newFile()
@@ -51,7 +62,7 @@ class EcrPushIntegrationTest {
 
         val project = projectRule.project
         runBlocking {
-            val serverInstance = getDockerServerRuntimeInstance()
+            val serverInstance = EcrUtils.getDockerServerRuntimeInstance().runtimeInstance
             val ecrLogin = ecrClient.authorizationToken.authorizationData().first().getDockerLogin()
             val runtime = RemoteDockerApplicationRuntime.createWithPullImage(
                 RemoteDockerRuntime.create(DockerCloudConfiguration.createDefault(), project),
@@ -59,22 +70,76 @@ class EcrPushIntegrationTest {
             )
             // gross transform because we only have the short SHA right now
             val localImageId = runtime.agent.getImages(null).first { it.imageId.startsWith("sha256:${runtime.agentApplication.imageId}") }.imageId
-            val pushRequest = EcrPushRequest(
+            val pushRequest = ImageEcrPushRequest(
+                dockerServerRuntime = serverInstance,
                 localImageId = localImageId,
                 remoteRepo = remoteRepo,
                 remoteTag = remoteTag
             )
 
-            pushImage(projectRule.project, serverInstance, ecrLogin, pushRequest)
-        }
+            EcrUtils.pushImage(projectRule.project, ecrLogin, pushRequest)
 
-        assertThat(
-            ecrClient.describeImages {
-                it.repositoryName(remoteRepo.repositoryName)
-            }.imageDetails()
-        ).anySatisfy {
-            assertThat(it.imageTags()).contains(remoteTag)
-            // would check the digest matches what we've uploaded, but it appears to change upon upload to ECR
+            assertThat(
+                ecrClient.batchGetImage {
+                    it.repositoryName(remoteRepo.repositoryName)
+                    it.imageIds(ImageIdentifier.builder().imageTag(remoteTag).build())
+                }.images()
+            )
+                .hasSize(1)
+                .allSatisfy { image ->
+                    assertDigestFromDockerManifest(image, localImageId)
+                }
         }
+    }
+
+    @Test
+    fun testPushFromDockerfile() {
+        val remoteTag = UUID.randomUUID().toString()
+
+        val dockerfile = folder.newFile()
+        dockerfile.writeText(
+            """
+                # arbitrary base image with a shell
+                FROM public.ecr.aws/lambda/provided:latest
+                RUN touch $(date +%s)
+            """.trimIndent()
+        )
+
+        val ecrLogin = ecrClient.authorizationToken.authorizationData().first().getDockerLogin()
+        val config = EcrUtils.dockerRunConfigurationFromPath(projectRule.project, remoteTag, dockerfile.absolutePath)
+        val pushRequest = DockerfileEcrPushRequest(
+            config.configuration as DockerRunConfiguration,
+            remoteRepo,
+            remoteTag
+        )
+        runBlocking {
+            EcrUtils.pushImage(projectRule.project, ecrLogin, pushRequest)
+
+            // find our local image id
+            val serverInstance = EcrUtils.getDockerServerRuntimeInstance().runtimeInstance
+            val localImageId = serverInstance.agent.getImages(null).first { it.imageRepoTags.contains("${remoteRepo.repositoryUri}:$remoteTag") }.imageId
+
+            assertThat(
+                ecrClient.batchGetImage {
+                    it.repositoryName(remoteRepo.repositoryName)
+                    it.imageIds(ImageIdentifier.builder().imageTag(remoteTag).build())
+                }.images()
+            )
+                .hasSize(1)
+                .allSatisfy { image ->
+                    assertDigestFromDockerManifest(image, localImageId)
+                }
+        }
+    }
+
+    private fun assertDigestFromDockerManifest(image: Image, imageId: String) {
+        // inspect the manifest because the registry digest is not the same as the image id
+        // https://github.com/docker/hub-feedback/issues/1925
+        val node = objectMapper.readTree(image.imageManifest())
+        assertThat(node.get("config").get("digest").asText()).isEqualTo(imageId)
+    }
+
+    companion object {
+        val objectMapper = jacksonObjectMapper()
     }
 }
