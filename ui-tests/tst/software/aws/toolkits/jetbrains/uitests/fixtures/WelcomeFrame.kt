36ef3dcf3cd9221d06420e8ebd9e3659db2480b8
Austin Brooks <alfredbr@amazon.com>
2021-05-14 12:01:26 -0700

    Missed some action links

--- ui-tests/tst/software/aws/toolkits/jetbrains/uitests/fixtures/WelcomeFrame.kt
+++ ui-tests/tst/software/aws/toolkits/jetbrains/uitests/fixtures/WelcomeFrame.kt
@@ -5,7 +5,6 @@ package software.aws.toolkits.jetbrains.uitests.fixtures
 
 import com.intellij.remoterobot.RemoteRobot
 import com.intellij.remoterobot.data.RemoteComponent
-import com.intellij.remoterobot.fixtures.ActionLinkFixture
 import com.intellij.remoterobot.fixtures.CommonContainerFixture
 import com.intellij.remoterobot.fixtures.ComponentFixture
 import com.intellij.remoterobot.fixtures.DefaultXpath
@@ -38,7 +37,7 @@ class WelcomeFrame(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) :
     fun openPreferences() = step("Opening preferences dialog") {
         // TODO: Remove FIX_WHEN_MIN_IS_203
         if (remoteRobot.ideMajorVersion() <= 202) {
-            actionLink("Configure").click()
+            actionLink(ActionLinkFixtureExt.byText("Configure")).click()
 
             // MyList finds both the list of actions and the most recently used file list, so get all candidates
             val found = findAll(ComponentFixture::class.java, byXpath("//div[@class='MyList']"))
@@ -71,7 +70,7 @@ class WelcomeFrame(remoteRobot: RemoteRobot, remoteComponent: RemoteComponent) :
     fun openFolder(path: Path) {
         // TODO: Remove FIX_WHEN_MIN_IS_203
         if (remoteRobot.ideMajorVersion() <= 202) {
-            actionLink(ActionLinkFixture.byTextContains("Open")).click()
+            actionLink(ActionLinkFixtureExt.byTextContains("Open")).click()
         } else {
             selectTab("Projects")
             findAll<ComponentFixture>(byXpath("//div[contains(@accessiblename, 'Open') and (@class='MainButton' or @class='JButton')]")).first().click()
