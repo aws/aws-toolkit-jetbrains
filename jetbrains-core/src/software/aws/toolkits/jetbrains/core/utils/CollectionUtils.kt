eb1ec3ed30643388a8c7d783282fb42aa2b38468
Austin Brooks <alfredbr@amazon.com>
2021-05-20 15:59:28 -0700

    Remove inlines that were crashing the kotlin compileri
    The intellij javac2 instumentation was producng bytecode that crashes the compiler, so remove them

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/utils/CollectionUtils.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/utils/CollectionUtils.kt
@@ -6,10 +6,10 @@ package software.aws.toolkits.jetbrains.core.utils
 /*
  * Replace with [kotlin.collections.buildList] when experimental is removed
  */
-inline fun <E> buildList(builderAction: MutableList<E>.() -> Unit): List<E> = ArrayList<E>().apply(builderAction)
-inline fun <E> buildList(capacity: Int, builderAction: MutableList<E>.() -> Unit): List<E> = ArrayList<E>(capacity).apply(builderAction)
+fun <E> buildList(builderAction: MutableList<E>.() -> Unit): List<E> = ArrayList<E>().apply(builderAction)
+fun <E> buildList(capacity: Int, builderAction: MutableList<E>.() -> Unit): List<E> = ArrayList<E>(capacity).apply(builderAction)
 
 /*
  * Replace with [kotlin.collections.buildMap] when experimental is removed
  */
-inline fun <T, U> buildMap(builderAction: MutableMap<T, U>.() -> Unit): Map<T, U> = mutableMapOf<T, U>().apply(builderAction)
+fun <T, U> buildMap(builderAction: MutableMap<T, U>.() -> Unit): Map<T, U> = mutableMapOf<T, U>().apply(builderAction)
