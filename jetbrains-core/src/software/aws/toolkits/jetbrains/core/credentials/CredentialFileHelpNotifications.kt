a6413c63553b164f0a26dc526585fb587c4344be
Austin Brooks <alfredbr@amazon.com>
2021-06-02 14:07:15 -0700

    Add notification to config/credential files pointing to help page

---
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/credentials/CredentialFileHelpNotifications.kt
@@ -0,0 +1,73 @@
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.core.credentials
+
+import com.intellij.ide.util.PropertiesComponent
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.help.HelpManager
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.ui.EditorNotificationPanel
+import com.intellij.ui.EditorNotifications
+import software.amazon.awssdk.profiles.ProfileFileLocation
+import software.aws.toolkits.jetbrains.core.credentials.CredentialFileHelpNotifications.CredentialFileNotificationPanel
+import software.aws.toolkits.jetbrains.core.help.HelpIds
+import software.aws.toolkits.resources.message
+
+class CredentialFileHelpNotifications : EditorNotifications.Provider<CredentialFileNotificationPanel>(), DumbAware {
+    override fun getKey(): Key<CredentialFileNotificationPanel> = KEY
+
+    override fun createNotificationPanel(file: VirtualFile, fileEditor: FileEditor, project: Project): CredentialFileNotificationPanel? {
+        if (PropertiesComponent.getInstance().isTrueValue(DISABLE_KEY)) return null
+        if (fileEditor.getUserData(HIDE_KEY) != null) return null
+        if (!isCredentialsFile(file)) return null
+
+        return CredentialFileNotificationPanel(file, project, fileEditor)
+    }
+
+    private fun isCredentialsFile(file: VirtualFile): Boolean = try {
+        val filePath = file.toNioPath().toAbsolutePath()
+        ProfileFileLocation.configurationFilePath().toAbsolutePath() == filePath || ProfileFileLocation.credentialsFilePath().toAbsolutePath() == filePath
+    } catch (e: Exception) {
+        false
+    }
+
+    class CredentialFileNotificationPanel(file: VirtualFile, project: Project, fileEditor: FileEditor) : EditorNotificationPanel() {
+        init {
+            createActionLabel(message("general.help")) {
+                HelpManager.getInstance().invokeHelp(HelpIds.SETUP_CREDENTIALS.id)
+            }
+
+            createActionLabel(message("general.notification.action.hide_once")) {
+                dismiss(file, project, fileEditor)
+            }
+
+            createActionLabel(message("general.notification.action.hide_forever")) {
+                hideForever(file, project)
+            }
+
+            text(message("credentials.file.notification"))
+        }
+
+        fun dismiss(file: VirtualFile, project: Project, fileEditor: FileEditor) {
+            fileEditor.putUserData(HIDE_KEY, true)
+            update(file, project)
+        }
+
+        fun hideForever(file: VirtualFile, project: Project) {
+            PropertiesComponent.getInstance().setValue(DISABLE_KEY, true)
+            update(file, project)
+        }
+
+        private fun update(file: VirtualFile, project: Project) = EditorNotifications.getInstance(project).updateNotifications(file)
+    }
+
+    companion object {
+        val KEY = Key.create<CredentialFileNotificationPanel>("software.aws.toolkits.jetbrains.core.credentials.editor.notification.provider")
+        private val HIDE_KEY = Key.create<Boolean>("software.aws.toolkits.jetbrains.core.credentials.editor.notification.hidden")
+        const val DISABLE_KEY = "software.aws.toolkits.jetbrains.core.credentials.editor.notification.disabled"
+    }
+}
