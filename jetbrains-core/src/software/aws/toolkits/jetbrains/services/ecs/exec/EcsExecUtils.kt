c00675dac98bfb38345758eed61a92cceec81ba3
manodnyab <66754471+manodnyab@users.noreply.github.com>
2021-06-08 15:21:55 -0700

    Progress indicator for enabling and disabling commands (#2631)
    * progress indicator added
    
    * progress indicator
    
    * progress indicator
    
    * progress indicator messages
    
    * ssm changes and refreshingtree with connection settings
    
    * refresh after enable/disable
    
    * refresh after enable/disable
    
    * getConnectionSettings moved to AwsConnectionManager

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/ecs/exec/EcsExecUtils.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/ecs/exec/EcsExecUtils.kt
@@ -4,9 +4,11 @@
 package software.aws.toolkits.jetbrains.services.ecs.exec
 
 import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import software.amazon.awssdk.services.ecs.EcsClient
 import software.amazon.awssdk.services.ecs.model.DescribeServicesRequest
@@ -14,6 +16,7 @@ import software.amazon.awssdk.services.ecs.model.InvalidParameterException
 import software.amazon.awssdk.services.ecs.model.Service
 import software.amazon.awssdk.services.ecs.model.UpdateServiceRequest
 import software.aws.toolkits.jetbrains.core.awsClient
+import software.aws.toolkits.jetbrains.core.credentials.AwsConnectionManager.Companion.getConnectionSettings
 import software.aws.toolkits.jetbrains.core.explorer.refreshAwsTree
 import software.aws.toolkits.jetbrains.services.ecs.resources.EcsResources
 import software.aws.toolkits.jetbrains.utils.ApplicationThreadPoolScope
@@ -33,7 +36,7 @@ object EcsExecUtils : CoroutineScope by ApplicationThreadPoolScope("EcsExec") {
                     .enableExecuteCommand(enabled)
                     .forceNewDeployment(true).build()
                 project.awsClient<EcsClient>().updateService(request)
-                checkServiceState(project, service, enable = enabled)
+                checkServiceState(project, service, enabled)
             } catch (e: InvalidParameterException) {
                 runInEdt {
                     TaskRoleNotFoundWarningDialog(project).show()
@@ -43,33 +46,45 @@ object EcsExecUtils : CoroutineScope by ApplicationThreadPoolScope("EcsExec") {
         }
     }
 
-    suspend fun checkServiceState(project: Project, service: Service, enable: Boolean) {
-        /* There appears to be a lag between the time the UpdateService call is made and
-         the time the changes are surfaced via the DescribeServices call.
-         Hence a delay of 5000 milliseconds is added before polling for the completion state of the service
-         */
-        delay(5000)
-        val request = DescribeServicesRequest.builder().cluster(service.clusterArn()).services(service.serviceArn()).build()
-        val client = project.awsClient<EcsClient>()
-        val waiter = client.waiter()
-        try {
-            waiter.waitUntilServicesStable(request)
-            project.refreshAwsTree(EcsResources.describeService(service.clusterArn(), service.serviceArn()))
-            if (enable) {
-                notifyInfo(message("ecs.execute_command_enable"), message("ecs.execute_command_enable_success", service.serviceName()))
-                EcsTelemetry.enableExecuteCommand(project, Result.Succeeded)
-            } else {
-                notifyInfo(message("ecs.execute_command_disable"), message("ecs.execute_command_disable_success", service.serviceName()))
-                EcsTelemetry.disableExecuteCommand(project, Result.Succeeded)
-            }
-        } catch (e: Exception) {
-            if (enable) {
-                notifyError(message("ecs.execute_command_enable"), message("ecs.execute_command_enable_failed", service.serviceName()))
-                EcsTelemetry.enableExecuteCommand(project, Result.Failed)
-            } else {
-                notifyError(message("ecs.execute_command_disable"), message("ecs.execute_command_disable_failed", service.serviceName()))
-                EcsTelemetry.disableExecuteCommand(project, Result.Failed)
-            }
+    private fun checkServiceState(project: Project, service: Service, enable: Boolean) {
+        val title = if (enable) {
+            message("ecs.execute_command_enable_progress_indicator_message")
+        } else {
+            message("ecs.execute_command_disable_progress_indicator_message")
         }
+
+        ProgressManager.getInstance().run(
+            object : Task.Backgroundable(project, title, false) {
+                override fun run(indicator: ProgressIndicator) {
+                    val request = DescribeServicesRequest.builder().cluster(service.clusterArn()).services(service.serviceArn()).build()
+                    val client = project.awsClient<EcsClient>()
+                    val waiter = client.waiter()
+                    waiter.waitUntilServicesStable(request)
+                }
+
+                override fun onSuccess() {
+                    val currentConnectionSettings = project.getConnectionSettings()
+                    project.refreshAwsTree(EcsResources.describeService(service.clusterArn(), service.serviceArn()), currentConnectionSettings)
+
+                    if (enable) {
+                        notifyInfo(message("ecs.execute_command_enable"), message("ecs.execute_command_enable_success", service.serviceName()))
+                        EcsTelemetry.enableExecuteCommand(project, Result.Succeeded)
+                    } else {
+                        notifyInfo(message("ecs.execute_command_disable"), message("ecs.execute_command_disable_success", service.serviceName()))
+                        EcsTelemetry.disableExecuteCommand(project, Result.Succeeded)
+                    }
+                }
+
+                override fun onThrowable(error: Throwable) {
+                    if (enable) {
+                        notifyError(message("ecs.execute_command_enable"), message("ecs.execute_command_enable_failed", service.serviceName()))
+                        EcsTelemetry.enableExecuteCommand(project, Result.Failed)
+                    } else {
+                        notifyError(message("ecs.execute_command_disable"), message("ecs.execute_command_disable_failed", service.serviceName()))
+                        EcsTelemetry.disableExecuteCommand(project, Result.Failed)
+                    }
+                }
+            }
+        )
     }
 }
