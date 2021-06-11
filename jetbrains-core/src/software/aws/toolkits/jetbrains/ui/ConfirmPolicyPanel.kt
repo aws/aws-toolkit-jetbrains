76590c5d7943a05a388bc1cea6d66bb774ebe934
Austin Brooks <alfredbr@amazon.com>
2021-06-11 16:20:57 -0700

    Fix compile and detekt

--- jetbrains-core/src/software/aws/toolkits/jetbrains/ui/ConfirmPolicyPanel.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/ui/ConfirmPolicyPanel.kt
@@ -1,5 +1,6 @@
-// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
 package software.aws.toolkits.jetbrains.ui
 
 import com.intellij.json.JsonLanguage
