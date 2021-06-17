f2d336ddb2086ca7a556d57b33f941b6fc3f6f12
Austin Brooks <alfredbr@amazon.com>
2021-06-17 16:31:46 -0700

    Remove extra file

--- jetbrains-core/src/software/aws/toolkits/jetbrains/ui/resources/PrefixBasedCompletionProvider.kt
+++
@@ -1,8 +0,0 @@
-// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-package software.aws.toolkits.jetbrains.ui.resources
-
-import com.intellij.util.TextFieldCompletionProviderDumbAware
-
-abstract class PrefixBasedCompletionProvider(caseInsensitivity: Boolean) : TextFieldCompletionProviderDumbAware(caseInsensitivity)
