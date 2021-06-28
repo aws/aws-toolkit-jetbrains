57c4e75c4665f4468f54bef3166282f475596480
Richard Li <richali@amazon.com>
2021-06-28 14:28:16 -0700

    refactor and integ test

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/ecr/actions/PullFromRepositoryAction.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/ecr/actions/PullFromRepositoryAction.kt
@@ -3,8 +3,8 @@
 
 package software.aws.toolkits.jetbrains.services.ecr.actions
 
-import com.intellij.docker.agent.DockerAgentProgressCallback
-import com.intellij.docker.registry.DockerAgentRepositoryConfigImpl
+import com.intellij.docker.DockerServerRuntimeInstance
+import com.intellij.docker.agent.OngoingProcess
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
@@ -13,12 +13,12 @@ import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.ui.SimpleListCellRenderer
 import com.intellij.ui.layout.panel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.apache.commons.io.FileUtils
 import software.amazon.awssdk.services.ecr.EcrClient
 import software.aws.toolkits.core.utils.getLogger
 import software.aws.toolkits.jetbrains.core.awsClient
+import software.aws.toolkits.jetbrains.services.ecr.EcrLogin
 import software.aws.toolkits.jetbrains.services.ecr.EcrRepositoryNode
 import software.aws.toolkits.jetbrains.services.ecr.EcrUtils
 import software.aws.toolkits.jetbrains.services.ecr.getDockerLogin
@@ -26,8 +26,6 @@ import software.aws.toolkits.jetbrains.services.ecr.resources.EcrResources
 import software.aws.toolkits.jetbrains.services.ecr.resources.Repository
 import software.aws.toolkits.jetbrains.ui.ResourceSelector
 import software.aws.toolkits.jetbrains.utils.ApplicationThreadPoolScope
-import software.aws.toolkits.jetbrains.utils.notifyError
-import software.aws.toolkits.jetbrains.utils.notifyInfo
 import software.aws.toolkits.resources.message
 
 class PullFromRepositoryAction : EcrDockerAction() {
@@ -43,52 +41,13 @@ class PullFromRepositoryAction : EcrDockerAction() {
 
         val (repo, image) = dialog.getPullRequest()
         val client: EcrClient = project.awsClient()
-        object : Task.Backgroundable(project, message("ecr.pull.progress", repo.repositoryUri, image)) {
-            override fun run(indicator: ProgressIndicator) {
-                indicator.isIndeterminate = true
-
-                runBlocking(coroutineScope.coroutineContext) {
-                    val runtime = dockerServerRuntime.await()
-                    val authData = withContext(Dispatchers.IO) {
-                        client.authorizationToken.authorizationData().first()
-                    }
-                    val ecrLogin = authData.getDockerLogin()
-                    val config = DockerAgentRepositoryConfigImpl(EcrUtils.buildDockerRepositoryModel(ecrLogin, repo, image))
-
-                    runtime.agent.pullImage(
-                        config,
-                        object : DockerAgentProgressCallback {
-                            // based on RegistryRuntimeTask's impl
-                            override fun step(status: String, current: Long, total: Long) {
-                                val indeterminate = total == 0L
-                                indicator.text2 = "$status " +
-                                    (if (indeterminate) "" else "${FileUtils.byteCountToDisplaySize(current)} of ${FileUtils.byteCountToDisplaySize(total)}")
-                                indicator.isIndeterminate = indeterminate
-                                if (!indeterminate) {
-                                    indicator.fraction = current.toDouble() / total.toDouble()
-                                }
-                            }
-
-                            override fun succeeded(message: String) {
-                                notifyInfo(
-                                    project = project,
-                                    title = message("ecr.pull.title"),
-                                    content = message
-                                )
-                            }
-
-                            override fun failed(message: String) {
-                                notifyError(
-                                    project = project,
-                                    title = message("ecr.pull.title"),
-                                    content = message
-                                )
-                            }
-                        }
-                    ).await()
-                }
+        coroutineScope.launch {
+            val runtime = dockerServerRuntime.await()
+            val authData = withContext(Dispatchers.IO) {
+                client.authorizationToken.authorizationData().first()
             }
-        }.queue()
+            PullFromEcrTask(project, authData.getDockerLogin(), repo, image, runtime).queue()
+        }
     }
 
     private companion object {
@@ -141,4 +100,28 @@ private class PullFromRepositoryDialog(selectedRepository: Repository, project: 
     }
 
     fun getPullRequest() = repoSelector.selected()!! to imageSelector.selected()!!
+}
+
+private class PullFromEcrTask(
+    project: Project,
+    private val ecrLogin: EcrLogin,
+    private val repository: Repository,
+    private val image: String,
+    private val runtime: DockerServerRuntimeInstance
+) : Task.Backgroundable(project, message("ecr.pull.progress", repository.repositoryUri, image)) {
+    private var task: OngoingProcess? = null
+
+    override fun onCancel() {
+        super.onCancel()
+        task?.cancel()
+    }
+
+    override fun run(indicator: ProgressIndicator) {
+        indicator.isIndeterminate = true
+        val config = EcrUtils.buildDockerRepositoryModel(ecrLogin, repository, image)
+        task = EcrUtils.pullImage(project, runtime, config, indicator).also {
+            // don't return until docker process exits
+            it.await()
+        }
+    }
 }
