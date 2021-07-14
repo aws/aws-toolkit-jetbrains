8101be0f068d42b689c3a0372c23558f56698b30
Richard Li <742829+rli@users.noreply.github.com>
2021-07-14 10:57:07 -0700

    Update ToolkitDockerAdapter.kt

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/docker/ToolkitDockerAdapter.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/docker/ToolkitDockerAdapter.kt
@@ -11,6 +11,7 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
 import org.apache.commons.io.FileUtils
 import org.jetbrains.annotations.TestOnly
+import software.aws.toolkits.core.utils.debug
 import software.aws.toolkits.core.utils.getLogger
 import software.aws.toolkits.jetbrains.utils.notifyError
 import software.aws.toolkits.jetbrains.utils.notifyInfo
@@ -23,6 +24,7 @@ abstract class AbstractToolkitDockerAdapter(protected val project: Project, prot
     abstract suspend fun pushImage(localTag: String, config: DockerRepositoryModel)
 
     fun getLocalImages(): List<LocalImage> =
+        // potentially null pre-212
         agent.getImages(null)?.flatMap { image ->
             image.imageRepoTags?.map { localTag ->
                 val tag = localTag.takeUnless { it == NO_TAG_TAG }
@@ -38,7 +40,7 @@ abstract class AbstractToolkitDockerAdapter(protected val project: Project, prot
         object : DockerAgentProgressCallback {
             // based on RegistryRuntimeTask's impl
             override fun step(status: String, current: Long, total: Long) {
-                LOG.debug("step: status: $status, current: $current, total: $total")
+                LOG.debug { "step: status: $status, current: $current, total: $total" }
                 progressIndicator?.let { indicator ->
                     val indeterminate = total == 0L
                     indicator.text2 = "$status " +
@@ -51,7 +53,7 @@ abstract class AbstractToolkitDockerAdapter(protected val project: Project, prot
             }
 
             override fun succeeded(message: String) {
-                LOG.debug("Pull from ECR succeeded: $message")
+                LOG.debug { "Pull from ECR succeeded: $message" }
                 notifyInfo(
                     project = project,
                     title = software.aws.toolkits.resources.message("ecr.pull.title"),
@@ -60,7 +62,7 @@ abstract class AbstractToolkitDockerAdapter(protected val project: Project, prot
             }
 
             override fun failed(message: String) {
-                LOG.debug("Pull from ECR failed: $message")
+                LOG.debug { "Pull from ECR failed: $message" }
                 notifyError(
                     project = project,
                     title = software.aws.toolkits.resources.message("ecr.pull.title"),
