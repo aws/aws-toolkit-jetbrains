c0363e710bc3332d77f825bf51889ebe372c23fa
Austin Brooks <alfredbr@amazon.com>
2021-08-17 15:35:44 -0700

    Back out SAM init changes

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/wizard/SamInitSelectionPanel.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/wizard/SamInitSelectionPanel.kt
@@ -8,23 +8,27 @@ import com.intellij.openapi.ui.ValidationInfo
 import com.intellij.ui.CollectionComboBoxModel
 import com.intellij.ui.IdeBorderFactory
 import com.intellij.ui.SimpleListCellRenderer
+import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.panels.Wrapper
 import com.intellij.ui.layout.panel
+import com.intellij.util.text.SemVer
 import software.amazon.awssdk.services.lambda.model.PackageType
 import software.aws.toolkits.core.lambda.LambdaRuntime
-import software.aws.toolkits.jetbrains.core.tools.SemanticVersion
-import software.aws.toolkits.jetbrains.core.tools.ToolManager
-import software.aws.toolkits.jetbrains.core.tools.ToolPathSelector
-import software.aws.toolkits.jetbrains.core.tools.toValidationInfo
+import software.aws.toolkits.jetbrains.core.executables.ExecutableInstance.BadExecutable
+import software.aws.toolkits.jetbrains.core.executables.ExecutableManager
+import software.aws.toolkits.jetbrains.core.executables.ExecutableType.Companion.getExecutable
 import software.aws.toolkits.jetbrains.services.lambda.minSamInitVersion
 import software.aws.toolkits.jetbrains.services.lambda.runtimeGroup
-import software.aws.toolkits.jetbrains.services.lambda.sam.SamCli
+import software.aws.toolkits.jetbrains.services.lambda.sam.SamCommon
+import software.aws.toolkits.jetbrains.services.lambda.sam.SamExecutable
 import software.aws.toolkits.jetbrains.utils.ui.validationInfo
 import software.aws.toolkits.resources.message
 import java.awt.BorderLayout
+import javax.swing.JButton
 import javax.swing.JComponent
 import javax.swing.JPanel
 import javax.swing.JRadioButton
+import javax.swing.JTextField
 
 class SamInitSelectionPanel(
     wizardFragmentList: List<WizardFragment>,
@@ -35,16 +39,19 @@ class SamInitSelectionPanel(
     lateinit var mainPanel: JPanel
 
     private lateinit var runtimeComboBox: ComboBox<LambdaRuntime>
+    private lateinit var samExecutableField: JTextField
+    private lateinit var editSamExecutableButton: JButton
+    private lateinit var samLabel: JBLabel
     private lateinit var packageZip: JRadioButton
     private lateinit var templateComboBox: ComboBox<SamProjectTemplate>
     private lateinit var fragments: Wrapper
-    private lateinit var executableSelector: ToolPathSelector<SamCli>
-    private lateinit var executableResult: Wrapper
 
     private val wizardFragments: Map<WizardFragment, JComponent>
     private val runtimes = CollectionComboBoxModel(supportedRuntimes())
 
     init {
+        setupSamSelectionElements(samExecutableField, editSamExecutableButton, samLabel)
+
         runtimeComboBox.model = runtimes
         runtimeComboBox.addActionListener {
             runtimeUpdate()
@@ -84,11 +91,6 @@ class SamInitSelectionPanel(
         // this will also fire wizardUpdate since templateComboBox will change
         // otherwise we make 2 of them
         runtimeUpdate()
-    }
-
-    private fun createUIComponents() {
-        executableResult = Wrapper()
-        executableSelector = ToolPathSelector(SamCli, executableResult).apply { reset() }
     }
 
     // Source all templates, find all the runtimes they support, then filter those by what the IDE supports
@@ -156,28 +158,23 @@ class SamInitSelectionPanel(
     }
 
     fun validate(): ValidationInfo? {
+        val samExecutable = ExecutableManager.getInstance().getExecutableIfPresent(getExecutable(SamExecutable::class.java))
+        if (samExecutable is BadExecutable) {
+            return ValidationInfo(samExecutable.validationError, samExecutableField)
+        }
+
+        val samVersion = SemVer.parseFromText(samExecutable.version)
+            ?: throw IllegalStateException("SemVer is invalid even with valid SAM executable")
+
+        if (packageType() == PackageType.IMAGE && samVersion < SamCommon.minImageVersion) {
+            return ValidationInfo(message("lambda.image.sam_version_too_low", samVersion, SamCommon.minImageVersion))
+        }
+
         val selectedRuntime = runtimes.selected ?: return templateComboBox.validationInfo(message("sam.init.error.no.runtime.selected"))
 
-        executableSelector.apply()
-
-        val toolManager = ToolManager.getInstance()
-        val samExecutable2 = toolManager.getTool(SamCli)
-
-        val stricterMinVersion = listOfNotNull(
-            if (packageType() == PackageType.IMAGE) SamCli.MIN_IMAGE_VERSION else null,
-            selectedRuntime.minSamInitVersion().run {
-                SemanticVersion(major, minor, patch)
-            }
-        ).max()
-
-        val validity = toolManager.validateCompatability(
-            project = null,
-            tool = samExecutable2,
-            stricterMinVersion = stricterMinVersion
-        )
-
-        validity.toValidationInfo(SamCli)?.let {
-            return it
+        val minSamVersion = selectedRuntime.minSamInitVersion()
+        if (samVersion < minSamVersion) {
+            return ValidationInfo(message("sam.executable.minimum_too_low_runtime", selectedRuntime, minSamVersion), runtimeComboBox)
         }
 
         val samProjectTemplate = templateComboBox.selectedItem as? SamProjectTemplate
