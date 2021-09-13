22777407a0f70dfbbedd03e7a0a68629f7dcb1a6
Richard Li <richali@amazon.com>
2021-09-13 12:25:04 -0700

    move mutation under indicator

--- jetbrains-core/src/software/aws/toolkits/jetbrains/ui/AsyncComboBox.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/ui/AsyncComboBox.kt
@@ -4,26 +4,30 @@
 package software.aws.toolkits.jetbrains.ui
 
 import com.intellij.openapi.Disposable
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.progress.EmptyProgressIndicator
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.ui.AnimatedIcon
 import com.intellij.ui.SimpleListCellRenderer
-import kotlinx.coroutines.runBlocking
+import com.intellij.util.Alarm
+import com.intellij.util.AlarmFactory
+import kotlinx.coroutines.launch
 import org.jetbrains.annotations.TestOnly
+import org.jetbrains.concurrency.AsyncPromise
 import software.aws.toolkits.jetbrains.utils.ApplicationThreadPoolScope
-import software.aws.toolkits.jetbrains.utils.spinUntil
 import software.aws.toolkits.jetbrains.utils.ui.selected
 import java.awt.Component
-import java.time.Duration
-import java.util.Timer
-import java.util.TimerTask
+import java.util.concurrent.Future
 import java.util.concurrent.atomic.AtomicBoolean
 import javax.swing.DefaultComboBoxModel
 import javax.swing.JList
-import javax.swing.event.ListDataEvent
-import kotlin.concurrent.timerTask
+import javax.swing.MutableComboBoxModel
+import javax.swing.event.ListDataListener
 
 class AsyncComboBox<T>(
-    private val comboBoxModel: DefaultComboBoxModel<T> = DefaultComboBoxModel(),
+    private val comboBoxModel: MutableComboBoxModel<T> = DefaultComboBoxModel(),
     customizer: SimpleListCellRenderer.Customizer<in T>? = null
 ) : ComboBox<T>(comboBoxModel), Disposable {
     private val loading = AtomicBoolean(false)
@@ -53,29 +57,34 @@ class AsyncComboBox<T>(
         }
     }
 
-    private val reloadTimer = Timer()
-    private var reloadTimerTask: TimerTask? = null
+    private val reloadAlarm = AlarmFactory.getInstance().create(Alarm.ThreadToUse.SWING_THREAD, this)
+    private var currentIndicator: ProgressIndicator? = null
 
     @Synchronized
-    fun proposeModelUpdate(modelConsumer: suspend (DefaultComboBoxModel<T>) -> Unit) {
+    fun proposeModelUpdate(newModel: suspend (MutableComboBoxModel<T>) -> Unit) {
+        reloadAlarm.cancelAllRequests()
+        currentIndicator?.cancel()
         loading.set(true)
         removeAllItems()
-        reloadTimerTask?.cancel()
-        // debounce
-        reloadTimerTask = timerTask {
-            val event = ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, -1, -1)
-            // force redraw of renderer without actually touching the underlying model
-            comboBoxModel.listDataListeners?.forEach { it.contentsChanged(event) }
-            runBlocking(scope.coroutineContext) {
-                try {
-                    modelConsumer.invoke(comboBoxModel)
-                } finally {
-                    loading.set(false)
-                }
-            }
-            comboBoxModel.listDataListeners?.forEach { it.contentsChanged(event) }
+        val indicator = EmptyProgressIndicator(ModalityState.NON_MODAL).also {
+            currentIndicator = it
         }
-        reloadTimer.schedule(reloadTimerTask, 350)
+        // delay with magic number to debounce
+        reloadAlarm.addRequest(
+            {
+                ProgressManager.getInstance().runProcess(
+                    {
+                        scope.launch {
+                            newModel.invoke(delegatedComboBoxModel(indicator))
+                            loading.set(false)
+                            repaint()
+                        }
+                    },
+                    indicator
+                )
+            },
+            350
+        )
     }
 
     override fun dispose() {
@@ -90,14 +99,14 @@ class AsyncComboBox<T>(
 
     @TestOnly
     @Synchronized
-    internal fun blockingGet(duration: Duration): T? {
-        try {
-            spinUntil(duration) { !loading.get() }
-        } catch (e: Exception) {
-            return null
+    internal fun waitForSelection(): Future<T?> {
+        val future = AsyncPromise<T>()
+        while (loading.get()) {
+            Thread.onSpinWait()
         }
+        future.setResult(selected())
 
-        return selected()
+        return future
     }
 
     override fun setSelectedItem(anObject: Any?) {
@@ -106,4 +115,44 @@ class AsyncComboBox<T>(
         }
         super.setSelectedItem(anObject)
     }
+
+    private fun delegatedComboBoxModel(indicator: ProgressIndicator) =
+        object : MutableComboBoxModel<T> {
+            override fun getSize() = comboBoxModel.size
+            override fun getElementAt(index: Int): T = comboBoxModel.getElementAt(index)
+
+            override fun addListDataListener(l: ListDataListener?) {
+                TODO("Not supported")
+            }
+
+            override fun removeListDataListener(l: ListDataListener?) {
+                TODO("Not supported")
+            }
+
+            override fun setSelectedItem(anItem: Any?) {
+                comboBoxModel.selectedItem = anItem
+            }
+
+            override fun getSelectedItem(): Any = comboBoxModel.selectedItem
+
+            override fun addElement(item: T?) {
+                indicator.checkCanceled()
+                comboBoxModel.addElement(item)
+            }
+
+            override fun removeElement(obj: Any?) {
+                indicator.checkCanceled()
+                comboBoxModel.removeElement(item)
+            }
+
+            override fun insertElementAt(item: T?, index: Int) {
+                indicator.checkCanceled()
+                comboBoxModel.insertElementAt(item, index)
+            }
+
+            override fun removeElementAt(index: Int) {
+                indicator.checkCanceled()
+                comboBoxModel.removeElementAt(index)
+            }
+        }
 }
--- jetbrains-core/tst/software/aws/toolkits/jetbrains/ui/AsyncComboBoxTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/ui/AsyncComboBoxTest.kt
@@ -3,21 +3,27 @@
 
 package software.aws.toolkits.jetbrains.ui
 
+import com.intellij.testFramework.ApplicationRule
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
 import org.junit.Test
 import org.mockito.kotlin.spy
+import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
-import java.time.Duration
 import java.util.concurrent.CountDownLatch
 import javax.swing.DefaultComboBoxModel
 
 class AsyncComboBoxTest {
+    @Rule
+    @JvmField
+    val applicationRule = ApplicationRule()
+
     @Test
     fun `can populate combobox`() {
         val spy = spy<DefaultComboBoxModel<String>>()
         val sut = AsyncComboBox(comboBoxModel = spy)
         sut.proposeModelUpdate { it.addElement("1") }
-        assertThat(sut.blockingGet(Duration.ofSeconds(5))).isEqualTo("1")
+        sut.waitForSelection().get()
 
         verify(spy).addElement("1")
     }
@@ -45,8 +51,34 @@ class AsyncComboBoxTest {
         sut.proposeModelUpdate { it.addElement("2") }
         sut.proposeModelUpdate { it.addElement("3") }
 
-        assertThat(sut.blockingGet(Duration.ofSeconds(5))).isEqualTo("3")
+        sut.waitForSelection().get()
 
         verify(spy).addElement("3")
+    }
+
+    @Test
+    fun `long running update proposals preempted by newer ones`() {
+        val spy = spy<DefaultComboBoxModel<String>>()
+        val sut = AsyncComboBox(comboBoxModel = spy)
+        val latch = CountDownLatch(1)
+        val latch2 = CountDownLatch(1)
+        sut.proposeModelUpdate {
+            it.addElement("1")
+            latch2.countDown()
+            latch.await()
+            it.addElement("2")
+        }
+        // wait for first update to start running
+        latch2.await()
+        sut.proposeModelUpdate {
+            it.addElement("3")
+        }
+
+        sut.waitForSelection().get()
+        latch.countDown()
+
+        verify(spy).addElement("1")
+        verify(spy).addElement("3")
+        verify(spy, times(0)).addElement("2")
     }
 }
