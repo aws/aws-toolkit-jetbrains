bfcd01962c0db70aae3fce678e70948ef66234be
Austin Brooks <alfredbr@amazon.com>
2021-10-07 11:49:27 -0700

    revert test change

--- jetbrains-core/it/software/aws/toolkits/jetbrains/services/lambda/python/PythonLocalLambdaRunConfigurationIntegrationTest.kt
+++ jetbrains-core/it/software/aws/toolkits/jetbrains/services/lambda/python/PythonLocalLambdaRunConfigurationIntegrationTest.kt
@@ -39,10 +39,10 @@ class PythonLocalLambdaRunConfigurationIntegrationTest(private val runtime: Runt
         @JvmStatic
         @Parameterized.Parameters(name = "{0}")
         fun data(): Collection<Array<Runtime>> = listOf(
-//            arrayOf(Runtime.PYTHON3_6),
+            arrayOf(Runtime.PYTHON3_6),
             arrayOf(Runtime.PYTHON3_7),
-//            arrayOf(Runtime.PYTHON3_8),
-//            arrayOf(Runtime.PYTHON3_9)
+            arrayOf(Runtime.PYTHON3_8),
+            arrayOf(Runtime.PYTHON3_9)
         )
     }
 
