4f1881bbd45198178f3c95f64ba2cd0cc531beae
Manodnya Jaydeep Bhoite <manodnyb@amazon.com>
2021-11-05 16:44:55 -0700

    improve UI for View Bucket

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/ViewResourceDialog.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/ViewResourceDialog.kt
@@ -5,6 +5,7 @@ package software.aws.toolkits.jetbrains.core.explorer
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.ui.layout.GrowPolicy
 import com.intellij.ui.layout.panel
 import javax.swing.JComponent
 
@@ -14,9 +15,9 @@ class ViewResourceDialog(project: Project, val resourceType: String, actionTitle
     private val component by lazy {
         panel {
             row("$resourceType:") {
-                textField(::resourceName).constraints(grow).withErrorOnApplyIf("$resourceType must be entered") {
+                textField(::resourceName).withErrorOnApplyIf("$resourceType must be entered") {
                     it.text.isNullOrBlank() || checkResourceNameValidity(it.text)
-                }
+                }.constraints(grow).growPolicy(GrowPolicy.SHORT_TEXT)
             }
         }
     }
