21433a326fd21c6ef06925739da28a86f3ad780c
Kyle Thomson <kylthoms@amazon.com>
2021-12-09 12:10:43 -0800

    Changed from removed to deprecated

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/ecs/EcsExperiments.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/ecs/EcsExperiments.kt
@@ -5,6 +5,7 @@ package software.aws.toolkits.jetbrains.services.ecs
 
 import software.aws.toolkits.jetbrains.core.experiments.ToolkitExperiment
 import software.aws.toolkits.resources.message
+import java.util.Date
 
 object EcsExecExperiment : ToolkitExperiment(
     "ecsExec",
@@ -17,6 +18,5 @@ object EcsExecExperiment : ToolkitExperiment(
 object EcsCloudDebugExperiment : ToolkitExperiment(
     "ecsCloudDebug",
     { message("cloud_debug.experiment.title") },
-    { message("cloud_debug.experiment.description") },
-    hidden = true
+    { message("cloud_debug.experiment.description", Date()) }
 )
