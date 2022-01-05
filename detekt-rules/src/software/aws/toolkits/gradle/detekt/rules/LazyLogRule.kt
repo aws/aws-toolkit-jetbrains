573a4b3b4aae606b651da62e27717c8a5b2d9617
Richard Li <richali@amazon.com>
2022-01-05 15:12:36 -0800

    Enforce LazyLog rule on Logger type instead of variable name

--- detekt-rules/src/software/aws/toolkits/gradle/detekt/rules/LazyLogRule.kt
+++ detekt-rules/src/software/aws/toolkits/gradle/detekt/rules/LazyLogRule.kt
@@ -9,17 +9,19 @@ import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
-import org.jetbrains.kotlin.name.Name
+import io.gitlab.arturbosch.detekt.api.internal.RequiresTypeResolution
+import io.gitlab.arturbosch.detekt.rules.fqNameOrNull
 import org.jetbrains.kotlin.psi.KtCallExpression
 import org.jetbrains.kotlin.psi.psiUtil.getCallNameExpression
-import org.jetbrains.kotlin.psi.psiUtil.getReceiverExpression
-import org.jetbrains.kotlin.psi.psiUtil.referenceExpression
+import org.jetbrains.kotlin.resolve.BindingContext
+import org.jetbrains.kotlin.resolve.calls.callUtil.getResolvedCall
 
+@RequiresTypeResolution
 class LazyLogRule : Rule() {
     override val issue = Issue("LazyLog", Severity.Style, "Use lazy logging synatax (e.g. warning {\"abc\"} ) instead of warning(\"abc\")", Debt.FIVE_MINS)
 
     private val logMethods = setOf("error", "warn", "info", "debug", "trace")
-    private val logNames = setOf("log", "logger")
+    private val loggerFQDN = "org.slf4j.Logger"
 
     // UI tests have issues with this TODO see if we want multiple detekt.yml files or disable for certain modules in this rule
     private val optOut = setOf("software.aws.toolkits.jetbrains.uitests")
@@ -31,13 +33,15 @@ class LazyLogRule : Rule() {
                 return
             }
 
-            if (optOut.any { name -> element.containingKtFile.packageFqName.startsWith(Name.identifier(name)) }) {
+            if (optOut.any { name -> element.containingKtFile.packageFqName.asString().startsWith(name) }) {
                 return
             }
 
-            val referenceExpression = it.getReceiverExpression()?.referenceExpression() ?: return
+            if (bindingContext == BindingContext.EMPTY) return
+            val type = it.getResolvedCall(bindingContext)?.extensionReceiver?.type?.fqNameOrNull()?.asString()
+                ?: it.getResolvedCall(bindingContext)?.dispatchReceiver?.type?.fqNameOrNull()?.asString()
 
-            if (!logNames.contains(referenceExpression.text.toLowerCase())) {
+            if (type != loggerFQDN) {
                 return
             }
 
@@ -46,7 +50,7 @@ class LazyLogRule : Rule() {
                     CodeSmell(
                         issue,
                         Entity.from(element),
-                        message = "Use the Lambda version of ${referenceExpression.text}.${it.text} instead"
+                        message = "Use the lambda version of $type.${it.text} instead"
                     )
                 )
             }
