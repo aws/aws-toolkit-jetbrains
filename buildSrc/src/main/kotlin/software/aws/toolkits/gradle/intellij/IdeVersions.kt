94650b74abeecfb4089f1003f6c72aae3e89be27
Manodnya Jaydeep Bhoite <manodnyb@amazon.com>
2022-06-09 12:09:20 -0700

    Add 2022.2 support

--- buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
+++ buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
@@ -163,7 +163,45 @@ object IdeVersions {
                 rdGenVersion = "2022.1.3",
                 nugetVersion = "2022.1.0"
             )
+        ),
+        Profile(
+            name = "2022.2",
+            community = ProductProfile(
+                sdkFlavor = IdeFlavor.IC,
+                sdkVersion = "222-EAP-SNAPSHOT",
+                plugins = commonPlugins + listOf(
+                    "java",
+                    "com.intellij.gradle",
+                    "org.jetbrains.idea.maven",
+                    "PythonCore:222.2889.12",
+                    "Docker:222.2889.12"
+                )
+            ),
+            ultimate = ProductProfile(
+                sdkFlavor = IdeFlavor.IU,
+                sdkVersion = "222-EAP-SNAPSHOT",
+                plugins = commonPlugins + listOf(
+                    "JavaScript",
+                    // Transitive dependency needed for javascript
+                    // Can remove when https://github.com/JetBrains/gradle-intellij-plugin/issues/608 is fixed
+                    "com.intellij.css",
+                    "JavaScriptDebugger",
+                    "com.intellij.database",
+                    "Pythonid:222.2889.14",
+                    "org.jetbrains.plugins.go:222.2889.14"
+                )
+            ),
+            rider = RiderProfile(
+                sdkVersion = "2022.2-EAP3-SNAPSHOT",
+                plugins = commonPlugins + listOf(
+                    "rider-plugins-appender" // Workaround for https://youtrack.jetbrains.com/issue/IDEA-179607
+                ),
+                netFrameworkTarget = "net472",
+                rdGenVersion = "2022.2.1",
+                nugetVersion = "2022.2.0-eap03"
+            )
         )
+
     ).associateBy { it.name }
 
     fun ideProfile(project: Project): Profile = ideProfile(project.providers).get()
