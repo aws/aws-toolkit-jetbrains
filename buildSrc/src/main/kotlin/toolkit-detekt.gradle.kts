102c4a35f0f3ead293c3865c5dc6a9f52177bbe3
Richard Li <richali@amazon.com>
2022-06-27 17:40:34 -0700

    Bump some libs and cleanup some Gradle warnings

--- buildSrc/src/main/kotlin/toolkit-detekt.gradle.kts
+++ buildSrc/src/main/kotlin/toolkit-detekt.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 // Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
@@ -9,7 +11,7 @@ plugins {
 // TODO: https://github.com/gradle/gradle/issues/15383
 val versionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
 dependencies {
-    detektPlugins(versionCatalog.findDependency("detekt-formattingRules").get())
+    detektPlugins(versionCatalog.findLibrary("detekt-formattingRules").get())
     detektPlugins(project(":detekt-rules"))
 }
 
@@ -22,8 +24,11 @@ detekt {
     config = files("$rulesProject/detekt.yml")
     autoCorrect = true
 
+}
+
+tasks.withType<Detekt> {
     reports {
-        html.enabled = true // Human readable report
-        xml.enabled = true // Checkstyle like format for CI tool integrations
+        html.required.set(true) // Human readable report
+        xml.required.set(true) // Checkstyle like format for CI tool integrations
     }
 }
