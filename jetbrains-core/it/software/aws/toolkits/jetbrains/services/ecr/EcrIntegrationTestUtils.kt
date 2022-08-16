585778949a8359c7100f77c5397ae25c551160f6
Richard Li <richali@amazon.com>
2022-08-16 14:18:19 -0700

    fix

--- jetbrains-core/it/software/aws/toolkits/jetbrains/services/ecr/EcrIntegrationTestUtils.kt
+++ jetbrains-core/it/software/aws/toolkits/jetbrains/services/ecr/EcrIntegrationTestUtils.kt
@@ -1,5 +1,7 @@
-package software.aws.toolkits.jetbrains.services.ecr// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.ecr
 
 object EcrIntegrationTestUtils {
     fun getImagePrefix(imageId: String): String = imageId
