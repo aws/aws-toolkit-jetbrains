85f913968b02c9d98653270b0b954347e6cce9b5
Richard Li <richali@amazon.com>
2022-09-29 15:38:11 -0700

    revert

--- jetbrains-core/tst/software/aws/toolkits/jetbrains/core/credentials/CredentialsFileHelpNotificationProviderTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/core/credentials/CredentialsFileHelpNotificationProviderTest.kt
@@ -72,6 +72,6 @@ class CredentialsFileHelpNotificationProviderTest {
     private fun openEditor(file: VirtualFile): FileEditor = FileEditorManagerEx.getInstanceEx(projectRule.project).openFile(file, true).single()
 
     private fun getEditorNotifications(editor: FileEditor): CredentialFileNotificationPanel? =
-        getEditorNotifications(projectRule.project, editor, CredentialsFileHelpNotificationProvider::class.java)
+        getEditorNotifications(projectRule.project, editor, CredentialsFileHelpNotificationProvider::class.java, CredentialsFileHelpNotificationProvider.KEY)
             as CredentialFileNotificationPanel?
 }
