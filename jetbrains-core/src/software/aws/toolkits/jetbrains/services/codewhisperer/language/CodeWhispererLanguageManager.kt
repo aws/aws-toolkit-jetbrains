3498018cd75229753a02f583cda82891439dbc2c
Shao-Hua Lo <xshaohua@amazon.com>
2022-10-04 14:00:07 -0700

    fix for NPE caused by cwsprLanguageManager

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/language/CodeWhispererLanguageManager.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/language/CodeWhispererLanguageManager.kt
@@ -26,7 +26,13 @@ class CodeWhispererLanguageManager {
         }
     }
 
-    fun getLanguage(psiFile: PsiFile): CodeWhispererProgrammingLanguage = getLanguage(psiFile.virtualFile)
+    fun getLanguage(psiFile: PsiFile): CodeWhispererProgrammingLanguage {
+        return psiFile.virtualFile?.let {
+            getLanguage(psiFile.virtualFile)
+        } ?: run {
+            CodeWhispererUnknownLanguage.INSTANCE
+        }
+    }
 
     companion object {
         fun getInstance(): CodeWhispererLanguageManager = service()
