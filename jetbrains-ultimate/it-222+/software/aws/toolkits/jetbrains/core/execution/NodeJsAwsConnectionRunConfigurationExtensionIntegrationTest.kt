222e8d72b39511fee5e3d3b29cd156c213b6f917
Kyle Thomson <kylthoms@amazon.com>
2022-10-21 11:14:24 -0700

    rebasing onto disposing-of-swing-timers

--- jetbrains-ultimate/it-222+/software/aws/toolkits/jetbrains/core/execution/NodeJsAwsConnectionRunConfigurationExtensionIntegrationTest.kt
+++ jetbrains-ultimate/it-222+/software/aws/toolkits/jetbrains/core/execution/NodeJsAwsConnectionRunConfigurationExtensionIntegrationTest.kt
@@ -8,11 +8,9 @@ import com.intellij.execution.RunManager
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.openapi.module.ModuleType
 import com.intellij.testFramework.PsiTestUtil
-import com.intellij.util.ReflectionUtil
 import com.jetbrains.nodejs.run.NodeJsRunConfiguration
 import com.jetbrains.nodejs.run.NodeJsRunConfigurationType
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 import software.aws.toolkits.jetbrains.core.credentials.MockCredentialManagerRule
@@ -20,9 +18,6 @@ import software.aws.toolkits.jetbrains.core.region.MockRegionProviderRule
 import software.aws.toolkits.jetbrains.utils.executeRunConfigurationAndWait
 import software.aws.toolkits.jetbrains.utils.rules.ExperimentRule
 import software.aws.toolkits.jetbrains.utils.rules.HeavyNodeJsCodeInsightTestFixtureRule
-import java.util.concurrent.DelayQueue
-import java.util.concurrent.TimeUnit
-import javax.swing.Timer
 import kotlin.test.assertNotNull
 
 class NodeJsAwsConnectionRunConfigurationExtensionIntegrationTest {
@@ -42,11 +37,6 @@ class NodeJsAwsConnectionRunConfigurationExtensionIntegrationTest {
     @Rule
     @JvmField
     val experiment = ExperimentRule(NodeJsAwsConnectionExperiment)
-
-    @After
-    fun tearDown() {
-        checkJavaSwingTimersAreDisposed()
-    }
 
     @Test
     fun environmentVariablesAreInjected() {
@@ -81,24 +71,5 @@ class NodeJsAwsConnectionRunConfigurationExtensionIntegrationTest {
         assertNotNull(executor)
 
         assertThat(executeRunConfigurationAndWait(runConfiguration).stdout).isEqualToIgnoringWhitespace(mockRegion.id)
-    }
-
-    // Adapted from https://github.com/JetBrains/intellij-community/blob/2926d8e27e2e0d8120a63089d1173b7e461e3dd1/platform/testFramework/src/com/intellij/testFramework/TestApplicationManager.kt#L338
-    // See https://intellij-support.jetbrains.com/hc/en-us/community/posts/360006918780-Tests-Fail-due-to-Java-Swing-Timers-Not-Disposed
-    // Seems to have been removed in 223: https://github.com/JetBrains/intellij-community/blob/223.7126/platform/testFramework/src/com/intellij/testFramework/TestApplicationManager.kt
-    private fun checkJavaSwingTimersAreDisposed() {
-        val timerQueueClass = Class.forName("javax.swing.TimerQueue")
-        val sharedInstance = timerQueueClass.getMethod("sharedInstance")
-        sharedInstance.isAccessible = true
-        val timerQueue = sharedInstance.invoke(null)
-        val delayQueue = ReflectionUtil.getField(timerQueueClass, timerQueue, DelayQueue::class.java, "queue")
-        while (true) {
-            val timer = delayQueue.peek() ?: return
-            val delay = timer.getDelay(TimeUnit.MILLISECONDS)
-            val getTimer = ReflectionUtil.getDeclaredMethod(timer.javaClass, "getTimer") ?: throw NullPointerException()
-            val swingTimer = getTimer.invoke(timer) as Timer
-            println("Not disposed javax.swing.Timer: (listeners: ${listOf(*swingTimer.actionListeners)}) (delayed for ${delay}ms)")
-            swingTimer.stop()
-        }
     }
 }
