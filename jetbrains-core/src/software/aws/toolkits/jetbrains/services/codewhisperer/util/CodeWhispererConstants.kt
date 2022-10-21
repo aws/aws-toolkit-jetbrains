f45e69d425787f24aefe15ee583101b79a9e008a
Will Lo <96078566+Will-ShaoHua@users.noreply.github.com>
2022-10-21 14:34:49 -0700

    Update jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/util/CodeWhispererConstants.kt
    Co-authored-by: Richard Li <742829+rli@users.noreply.github.com>

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/util/CodeWhispererConstants.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/util/CodeWhispererConstants.kt
@@ -49,6 +49,6 @@ object CodeWhispererConstants {
     object Config {
         const val CODEWHISPERER_ENDPOINT = "https://codewhisperer.us-east-1.amazonaws.com/"
         const val CODEWHISPERER_IDPOOL_ID = "us-east-1:70717e99-906f-4add-908c-bd9074a2f5b9"
-        val REGION: Region = Region.US_EAST_1
+        val REGION = Region.US_EAST_1
     }
 }
