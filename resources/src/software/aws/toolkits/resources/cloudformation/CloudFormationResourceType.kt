951d338bce04f5965372d7dd906e2236571967cf
Kyle Thomson <kylthoms@amazon.com>
2023-02-09 15:20:25 -0800

    Adding CloudFormationResourceType value type, and generating all known CFN Resource Types from the CFN registry as part of the build.

---
+++ resources/src/software/aws/toolkits/resources/cloudformation/CloudFormationResourceType.kt
@@ -0,0 +1,7 @@
+// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.resources.cloudformation
+
+@JvmInline
+value class CloudFormationResourceType(val fullName: String)
