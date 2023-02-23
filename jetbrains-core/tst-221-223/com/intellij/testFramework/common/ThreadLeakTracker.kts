cd6fe4badb5b35e3f58fa1f864b825a9fbbf00f1
Richard Li <richali@amazon.com>
2023-02-23 13:33:39 -0800

    thread tracjer

---
+++ jetbrains-core/tst-221-223/com/intellij/testFramework/common/ThreadLeakTracker.kts
@@ -0,0 +1,4 @@
+// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+typealias ThreadLeakTracker = com.intellij.testFramework.ThreadTracker
