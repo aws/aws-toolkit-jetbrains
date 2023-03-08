1bba889a2f8c5bd9733633bddddfc98aab7033e5
vineeth chikoti <vchikoti@amazon.com>
2023-03-08 09:50:46 -0800

    Provided displaying spaceName/projectName on statusBar of MDE

---
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/caws/CawsSpaceProjectInfo.kt
@@ -0,0 +1,50 @@
+// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+package software.aws.toolkits.jetbrains.services.caws
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.popup.ListPopup
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.wm.StatusBar
+import com.intellij.openapi.wm.StatusBarWidget
+import com.intellij.openapi.wm.StatusBarWidgetFactory
+import com.intellij.openapi.wm.impl.status.EditorBasedWidget
+import com.intellij.util.Consumer
+import java.awt.event.MouseEvent
+
+private const val WIDGET_ID = "CawsSpaceProjectInfo"
+
+class CawsStatusBarInstaller : StatusBarWidgetFactory {
+    override fun getId(): String = WIDGET_ID
+
+    override fun getDisplayName(): String = "Displays"
+
+    override fun isAvailable(project: Project): Boolean = true
+
+    override fun createWidget(project: Project): StatusBarWidget = CawsSpaceProjectInfo(project)
+
+    override fun disposeWidget(widget: StatusBarWidget) {
+        Disposer.dispose(widget)
+    }
+
+    override fun canBeEnabledOn(statusBar: StatusBar): Boolean = true
+}
+
+private class CawsSpaceProjectInfo(project: Project) :
+    StatusBarWidget,
+    StatusBarWidget.MultipleTextValuesPresentation, EditorBasedWidget(project) {
+
+    override fun ID(): String = WIDGET_ID
+    override fun getPresentation(): StatusBarWidget.WidgetPresentation = this
+    override fun getTooltipText(): String = "displays spaceName/projectName"
+    override fun getClickConsumer(): Consumer<MouseEvent>? = null
+    override fun getPopupStep(): ListPopup? = null
+    override fun getSelectedValue(): String {
+        val spaceName = System.getenv(CawsConstants.CAWS_ENV_ORG_NAME_VAR)
+        val projectName = System.getenv(CawsConstants.CAWS_ENV_PROJECT_NAME_VAR)
+        if (spaceName != null && projectName != null) {
+            return "$spaceName/$projectName"
+        }
+        return ""
+    }
+}
