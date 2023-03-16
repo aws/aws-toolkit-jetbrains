2757acf6b61892dd9727e0f3b1061a0e584405ff
Richard Li <richali@amazon.com>
2023-03-15 22:30:07 -0700

    test-impl coroutines-core

--- jetbrains-core/build.gradle.kts
+++ jetbrains-core/build.gradle.kts
@@ -156,6 +156,7 @@ dependencies {
 
     testImplementation(project(path = ":core", configuration = "testArtifacts"))
     testImplementation(libs.wiremock)
+    testImplementation(libs.kotlin.coroutines)
     testImplementation(libs.kotlin.coroutinesTest)
     testImplementation(libs.kotlin.coroutinesDebug)
 }
