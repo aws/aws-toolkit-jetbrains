befd7d3f0ecf01d80ee02fb42e39eb750574a258
Richard Li <richali@amazon.com>
2023-03-23 17:59:57 -0700

    Fix regression with availability of OpenWorkspaceInGateway action

--- jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/devToolsTab/nodes/actions/OpenWorkspaceInGateway.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/devToolsTab/nodes/actions/OpenWorkspaceInGateway.kt
@@ -10,10 +10,6 @@ import com.intellij.openapi.project.DumbAwareAction
 import software.aws.toolkits.jetbrains.utils.isRunningOnRemoteBackend
 
 class OpenWorkspaceInGateway : DumbAwareAction(ProductIcons.getInstance().productIcon) {
-    override fun update(e: AnActionEvent) {
-        e.presentation.isEnabledAndVisible = !isRunningOnRemoteBackend()
-    }
-
     override fun actionPerformed(e: AnActionEvent) {
         ActionManager.getInstance().getAction("OpenRemoteDevelopment").actionPerformed(e)
     }
