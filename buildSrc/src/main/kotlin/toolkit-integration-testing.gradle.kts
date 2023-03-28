6798dd14d61d4145452e913f67e777a066831ed5
Richard Li <richali@amazon.com>
2023-03-28 15:03:50 -0700

    convention

--- buildSrc/src/main/kotlin/toolkit-integration-testing.gradle.kts
+++ buildSrc/src/main/kotlin/toolkit-integration-testing.gradle.kts
@@ -19,11 +19,11 @@ sourceSets {
 }
 
 configurations.getByName("integrationTestCompileClasspath") {
-    extendsFrom(configurations.getByName(JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME))
+    extendsFrom(configurations.getByName(JavaPlugin.TEST_COMPILE_CLASSPATH_CONFIGURATION_NAME))
     isCanBeResolved = true
 }
 configurations.getByName("integrationTestRuntimeClasspath") {
-    extendsFrom(configurations.getByName(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME))
+    extendsFrom(configurations.getByName(JavaPlugin.TEST_RUNTIME_CLASSPATH_CONFIGURATION_NAME))
     isCanBeResolved = true
 }
 
