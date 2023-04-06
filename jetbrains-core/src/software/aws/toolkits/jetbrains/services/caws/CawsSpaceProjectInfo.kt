ae66ddf8b84121606d6caaa1e88ac59bc819922c
vineeth chikoti <vchikoti@amazon.com>
2023-04-06 14:19:03 -0700

    PR comments

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/caws/CawsSpaceProjectInfo.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/caws/CawsSpaceProjectInfo.kt
@@ -8,6 +8,7 @@ import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidgetFactory
 import com.intellij.util.Consumer
+import java.awt.Component
 import java.awt.event.MouseEvent
 
 private const val WIDGET_ID = "CawsSpaceProjectInfo"
@@ -20,7 +21,7 @@ class CawsStatusBarInstaller : StatusBarWidgetFactory {
 
     override fun getDisplayName(): String = "spaceName/projectName"
 
-    override fun isAvailable(project: Project): Boolean = System.getenv("REMOTE_DEV_LAUNCHER_NAME_FOR_USAGE") == null
+    override fun isAvailable(project: Project): Boolean = spaceName != null && projectName != null
 
     override fun createWidget(project: Project): StatusBarWidget = CawsSpaceProjectInfo(spaceName, projectName)
 
@@ -40,16 +41,13 @@ private class CawsSpaceProjectInfo(val spaceName: String?, val projectName: Stri
     override fun getPresentation(): StatusBarWidget.WidgetPresentation = this
 
     override fun getTooltipText(): String = "$spaceName/$projectName"
-
     override fun getClickConsumer(): Consumer<MouseEvent>? = null
 
     override fun getText(): String = "$spaceName/$projectName"
 
+    override fun getAlignment(): Float = Component.CENTER_ALIGNMENT
+
     override fun dispose() {}
 
     override fun install(statusBar: StatusBar) {}
-
-    override fun getAlignment(): Float {
-        TODO("Not to be implemented")
-    }
 }
