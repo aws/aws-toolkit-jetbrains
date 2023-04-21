8d19935950a5e8cbec18215dc8cf83af63729207
vineeth chikoti <vchikoti@amazon.com>
2023-04-21 14:39:02 -0700

    Addressed PR comment

--- jetbrains-core/tst/software/aws/toolkits/jetbrains/services/codewhisperer/CodeWhispererConstantsTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/services/codewhisperer/CodeWhispererConstantsTest.kt
@@ -3,14 +3,22 @@
 
 package software.aws.toolkits.jetbrains.services.codewhisperer
 
+import com.intellij.help.impl.HelpManagerImpl
+import com.intellij.openapi.help.HelpManager
+import com.intellij.testFramework.ApplicationRule
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
 import org.junit.Test
-import software.aws.toolkits.jetbrains.services.codewhisperer.util.CodeWhispererConstants
-
+import software.aws.toolkits.jetbrains.core.help.HelpIds
 class CodeWhispererConstantsTest {
+
+    @Rule
+    @JvmField
+    val application = ApplicationRule()
+
     @Test
     fun `is codewishperer help uri has correct doc link`() {
-        assertThat(CodeWhispererConstants.CODEWHISPERER_LOGIN_HELP_URI)
+        assertThat((HelpManager.getInstance() as HelpManagerImpl).getHelpUrl(HelpIds.CODEWHISPERER_LOGIN_YES_NO.id))
             .isEqualTo("https://docs.aws.amazon.com/toolkit-for-jetbrains/latest/userguide/setup-credentials.html")
     }
 }
