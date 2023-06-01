e6e85bfe7559d4c2f88c917e67df2b878b22197d
Richard Li <richali@amazon.com>
2023-06-01 01:25:58 +0000

    go

--- jetbrains-ultimate/tst/software/aws/toolkits/jetbrains/go/VgoCompatShims.kt
+++ jetbrains-ultimate/tst/software/aws/toolkits/jetbrains/go/VgoCompatShims.kt
@@ -5,10 +5,11 @@ package software.aws.toolkits.jetbrains.go
 
 import com.goide.vgo.project.VgoDependency
 import com.goide.vgo.project.VgoStandaloneModule
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
 object VgoCompatShims {
     @JvmStatic
-    fun newVgoModule(root: VirtualFile, importPath: String, goVersion: String?, dependencies: Map<String, VgoDependency>) =
-        VgoStandaloneModule(root, importPath, goVersion, dependencies.values)
+    fun newVgoModule(project: Project, root: VirtualFile, importPath: String, goVersion: String?, dependencies: Map<String, VgoDependency>) =
+        VgoStandaloneModule(project, root, importPath, goVersion, dependencies.values)
 }
