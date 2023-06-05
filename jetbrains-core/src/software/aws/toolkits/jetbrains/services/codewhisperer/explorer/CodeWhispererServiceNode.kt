0232cb3b883d101a393f3c79ad1fdd61cd50f1c4
Richard Li <richali@amazon.com>
2023-06-05 21:48:13 +0000

    Patch CodeWhisperer preview actions

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/explorer/CodeWhispererServiceNode.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/explorer/CodeWhispererServiceNode.kt
@@ -4,21 +4,16 @@
 package software.aws.toolkits.jetbrains.services.codewhisperer.explorer
 
 import com.intellij.icons.AllIcons
+import com.intellij.ide.BrowserUtil
 import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.Project
 import software.aws.toolkits.jetbrains.core.explorer.devToolsTab.nodes.AbstractActionTreeNode
 import software.aws.toolkits.jetbrains.core.explorer.devToolsTab.nodes.ActionGroupOnRightClick
 import software.aws.toolkits.jetbrains.core.explorer.nodes.AwsExplorerNode
-import software.aws.toolkits.jetbrains.services.codewhisperer.codescan.CodeWhispererCodeScanManager
-import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager.Companion.ACTION_ENABLE_CODEWHISPERER
-import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager.Companion.ACTION_ENTER_ACCESSTOKEN
-import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager.Companion.ACTION_OPEN_CODE_REFERENCE_PANEL
-import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager.Companion.ACTION_PAUSE_CODEWHISPERER
-import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager.Companion.ACTION_REQUEST_ACCESSTOKEN
-import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager.Companion.ACTION_RESUME_CODEWHISPERER
-import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager.Companion.ACTION_RUN_SECURITY_SCAN
+import software.aws.toolkits.jetbrains.core.utils.buildList
 import software.aws.toolkits.jetbrains.services.codewhisperer.explorer.CodeWhispererExplorerActionManager.Companion.ACTION_WHAT_IS_CODEWHISPERER
 import software.aws.toolkits.resources.message
+import software.aws.toolkits.telemetry.UiTelemetry
 import javax.swing.Icon
 
 class CodeWhispererServiceNode(
@@ -27,24 +22,7 @@ class CodeWhispererServiceNode(
 ) : AbstractActionTreeNode(project, value, null), ActionGroupOnRightClick {
     private val nodeProject
         get() = myProject
-    private val pauseCodeWhispererNode by lazy {
-        CodeWhispererActionNode(
-            nodeProject,
-            message("codewhisperer.explorer.pause_auto"),
-            ACTION_PAUSE_CODEWHISPERER,
-            1,
-            AllIcons.Actions.Pause
-        )
-    }
-    private val resumeCodeWhispererNode by lazy {
-        CodeWhispererActionNode(
-            nodeProject,
-            message("codewhisperer.explorer.resume_auto"),
-            ACTION_RESUME_CODEWHISPERER,
-            1,
-            AllIcons.Actions.Resume
-        )
-    }
+
     private val whatIsCodeWhispererNode by lazy {
         CodeWhispererActionNode(
             nodeProject,
@@ -54,75 +32,23 @@ class CodeWhispererServiceNode(
             AllIcons.Actions.Help
         )
     }
-    private val enableCodeWhispererNode by lazy {
-        CodeWhispererActionNode(
-            nodeProject,
-            message("codewhisperer.explorer.enable"),
-            ACTION_ENABLE_CODEWHISPERER,
-            1,
-            AllIcons.Actions.Execute
-        )
-    }
-    private val openCodeReferenceNode by lazy {
-        CodeWhispererActionNode(
-            nodeProject,
-            message("codewhisperer.explorer.code_reference.open"),
-            ACTION_OPEN_CODE_REFERENCE_PANEL,
-            3,
-            AllIcons.Actions.Preview
-        )
-    }
-    private val runCodeScanNode by lazy {
-        CodeWhispererActionNode(
-            nodeProject, message("codewhisperer.codescan.run_scan"),
-            ACTION_RUN_SECURITY_SCAN,
-            2,
-            CodeWhispererCodeScanManager.getInstance(nodeProject).getActionButtonIcon()
-        )
-    }
-    private val requestAccessTokenNode by lazy {
-        CodeWhispererActionNode(
-            nodeProject,
-            message("codewhisperer.explorer.token.request"),
-            ACTION_REQUEST_ACCESSTOKEN,
-            4,
-            AllIcons.General.User
-        )
-    }
-    private val enterAccessTokenNode by lazy {
-        CodeWhispererActionNode(
-            nodeProject,
-            message("codewhisperer.explorer.token.dialog_title"),
-            ACTION_ENTER_ACCESSTOKEN,
-            5,
-            AllIcons.Actions.Edit
-        )
-    }
 
     override fun onDoubleClick() {}
 
-    override fun getChildren(): Collection<AbstractTreeNode<*>> {
-        val codewhispererActions = mutableListOf<CodeWhispererActionNode>()
-        val manager = CodeWhispererExplorerActionManager.getInstance()
-        if (!manager.isAuthorized()) {
-            codewhispererActions.add(whatIsCodeWhispererNode)
-            codewhispererActions.add(requestAccessTokenNode)
-            codewhispererActions.add(enterAccessTokenNode)
-        } else {
-            if (!manager.hasAcceptedTermsOfService()) {
-                codewhispererActions.add(whatIsCodeWhispererNode)
-                codewhispererActions.add(enableCodeWhispererNode)
-            } else {
-                if (manager.isAutoEnabled()) {
-                    codewhispererActions.add(pauseCodeWhispererNode)
-                } else {
-                    codewhispererActions.add(resumeCodeWhispererNode)
+    override fun getChildren(): Collection<AbstractTreeNode<*>> = buildList {
+        add(whatIsCodeWhispererNode)
+        add(
+            object : AbstractActionTreeNode(
+                nodeProject,
+                message("codewhisperer.upgrade_ide"),
+                AllIcons.General.User
+            ) {
+                override fun onDoubleClick() {
+                    BrowserUtil.browse("https://github.com/aws/aws-toolkit-jetbrains/issues/3684")
+                    UiTelemetry.click(project, "cw_upgradeIde_Cta")
                 }
-                codewhispererActions.add(runCodeScanNode)
-                codewhispererActions.add(openCodeReferenceNode)
             }
-        }
-        return codewhispererActions
+        )
     }
 
     override fun actionGroupName(): String = "aws.toolkit.explorer.codewhisperer"
