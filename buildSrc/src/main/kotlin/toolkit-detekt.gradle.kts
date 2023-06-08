025f870cd5d7b31aa293675d506614d2c4a078bf
JadenSimon <sijaden@amazon.com>
2023-06-08 13:35:48 -0700

    Remove year from copyright header

--- buildSrc/src/main/kotlin/toolkit-detekt.gradle.kts
+++ buildSrc/src/main/kotlin/toolkit-detekt.gradle.kts
@@ -1,4 +1,4 @@
-// Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
