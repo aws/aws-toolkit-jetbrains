e945af5ad6d3aa8b6e127ca8223d1610c18c50c2
vineeth chikoti <vchikoti@amazon.com>
2023-07-20 03:20:57 -0700

    Updrade 232 ide version and fixed UiConstraints

--- buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
+++ buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
@@ -156,23 +156,23 @@ object IdeVersions {
             name = "2023.2",
             gateway = ProductProfile(
                 sdkFlavor = IdeFlavor.GW,
-                sdkVersion = "232.6734-EAP-CANDIDATE-SNAPSHOT",
+                sdkVersion = "232.8660-EAP-CANDIDATE-SNAPSHOT",
                 plugins = arrayOf("org.jetbrains.plugins.terminal")
             ),
             community = ProductProfile(
                 sdkFlavor = IdeFlavor.IC,
-                sdkVersion = "232.6734-EAP-CANDIDATE-SNAPSHOT",
+                sdkVersion = "232.8660-EAP-CANDIDATE-SNAPSHOT",
                 plugins = commonPlugins223 + listOf(
                     "java",
                     "com.intellij.gradle",
                     "org.jetbrains.idea.maven",
-                    "PythonCore:232.6734.9",
-                    "Docker:232.6734.4"
+                    "PythonCore:232.8660.48",
+                    "Docker:232.8660.88"
                 )
             ),
             ultimate = ProductProfile(
                 sdkFlavor = IdeFlavor.IU,
-                sdkVersion = "232.6734-EAP-CANDIDATE-SNAPSHOT",
+                sdkVersion = "232.8660-EAP-CANDIDATE-SNAPSHOT",
                 plugins = commonPlugins223 + listOf(
                     "JavaScript",
                     // Transitive dependency needed for javascript
@@ -181,8 +181,8 @@ object IdeVersions {
                     "JavaScriptDebugger",
                     "com.intellij.database",
                     "com.jetbrains.codeWithMe",
-                    "Pythonid:232.6734.9",
-                    "org.jetbrains.plugins.go:232.6734.9",
+                    "Pythonid:232.8660.48",
+                    "org.jetbrains.plugins.go:232.8660.48",
                     // https://github.com/JetBrains/gradle-intellij-plugin/issues/1056
                     "org.intellij.intelliLang"
                 )
