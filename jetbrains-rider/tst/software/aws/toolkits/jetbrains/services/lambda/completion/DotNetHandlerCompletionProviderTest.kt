39462b9e0b12ecc3c0ea79e268a0dda452e09dc6
vineeth chikoti <vchikoti@amazon.com>
2023-07-21 06:14:09 -0700

    ignore tests

--- jetbrains-rider/tst/software/aws/toolkits/jetbrains/services/lambda/completion/DotNetHandlerCompletionProviderTest.kt
+++ jetbrains-rider/tst/software/aws/toolkits/jetbrains/services/lambda/completion/DotNetHandlerCompletionProviderTest.kt
@@ -6,6 +6,7 @@ package software.aws.toolkits.jetbrains.services.lambda.completion
 import base.AwsReuseSolutionTestBase
 import org.assertj.core.api.Assertions.assertThat
 import org.testng.annotations.DataProvider
+import org.testng.annotations.Ignore
 import org.testng.annotations.Test
 import software.aws.toolkits.core.lambda.LambdaRuntime
 import software.aws.toolkits.jetbrains.utils.OPEN_SOLUTION_DIR_NAME
@@ -21,11 +22,12 @@ class DotNetHandlerCompletionProviderTest : AwsReuseSolutionTestBase() {
         arrayOf("DotNet60", LambdaRuntime.DOTNET6_0)
     )
 
+    @Ignore("test for 232")
     @Test(
         dataProvider = "handlerCompletionSupportedData",
         description = "Check completion in run configuration feature is enabled for DOTNET runtime."
     )
-    fun testCompletion_IsSupportedForDotNetRuntime(name: String, runtime: LambdaRuntime) {
+    fun `testCompletion is SupportedForDotNetRuntime`(name: String, runtime: LambdaRuntime) {
         val provider = HandlerCompletionProvider(project, runtime)
         assertThat(provider.isCompletionSupported).isTrue()
     }
