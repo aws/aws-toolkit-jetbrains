1641468b8bc548dca9925559ec3b172b0285bea9
Shaoqing Pan <panshao@amazon.com>
2023-09-15 11:16:25 -0700

    change completionType for empty suggestions from Unknown to Line

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/service/CodeWhispererService.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/service/CodeWhispererService.kt
@@ -381,7 +381,7 @@ class CodeWhispererService {
                     false,
                     false,
                     "",
-                    CodewhispererCompletionType.Unknown
+                    CodewhispererCompletionType.Line
                 ),
                 -1,
                 CodewhispererSuggestionState.Empty,
