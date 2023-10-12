6896cd1ae8d3d6e275c84405b6fa98cc1b4c95ca
Manodnya Jaydeep Bhoite <manodnyb@amazon.com>
2023-10-12 16:58:12 -0700

    Hardcoded version number

--- jetbrains-core/src/software/aws/toolkits/jetbrains/services/caws/DevFileSchemaProviderFactory.kt
+++ jetbrains-core/src/software/aws/toolkits/jetbrains/services/caws/DevFileSchemaProviderFactory.kt
@@ -3,7 +3,6 @@
 
 package software.aws.toolkits.jetbrains.services.caws
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
@@ -12,7 +11,6 @@ import com.jetbrains.jsonSchema.extension.SchemaType
 import com.jetbrains.jsonSchema.impl.JsonSchemaVersion
 import com.jetbrains.jsonSchema.remote.JsonFileResolver
 import software.aws.toolkits.resources.message
-import java.net.URL
 
 class DevFileSchemaProviderFactory : JsonSchemaProviderFactory {
     override fun getProviders(project: Project): List<JsonSchemaFileProvider> = listOf(
@@ -21,14 +19,14 @@ class DevFileSchemaProviderFactory : JsonSchemaProviderFactory {
 
             override fun isAvailable(file: VirtualFile): Boolean = file.name.matches(Regex("devfile\\.y[a]?ml", RegexOption.IGNORE_CASE))
 
-            override fun getSchemaFile(): VirtualFile? {
-                val latestTag = jacksonObjectMapper().readTree(URL("https://api.github.com/repos/devfile/api/releases/latest")).get("tag_name").textValue()
-                val schemaUrl: String = "https://raw.githubusercontent.com/devfile/api/$latestTag/schemas/latest/devfile.json"
-                return JsonFileResolver.urlToFile(schemaUrl) }
+            override fun getSchemaFile(): VirtualFile? = JsonFileResolver.urlToFile(schemaUrl)
 
             override fun getSchemaType(): SchemaType = SchemaType.remoteSchema
 
             override fun getSchemaVersion(): JsonSchemaVersion = JsonSchemaVersion.SCHEMA_7
         }
     )
+    private companion object {
+        const val schemaUrl: String = "https://raw.githubusercontent.com/devfile/api/v2.2.1/schemas/latest/devfile.json"
+    }
 }
