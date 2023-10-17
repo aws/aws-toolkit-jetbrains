817f06d767da0bc4c30b52ee8a7d5d7b9f3ee615
Richard Li <richali@amazon.com>
2023-10-17 10:05:40 -0700

    Only show getting started popup on first install

---
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/core/gettingstarted/GettingStartedOnStartupTest.kt
@@ -0,0 +1,99 @@
+// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.core.gettingstarted
+
+import com.intellij.configurationStore.getPersistentStateComponentStorageLocation
+import com.intellij.testFramework.ProjectExtension
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.mockkObject
+import io.mockk.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.extension.RegisterExtension
+import software.aws.toolkits.core.utils.deleteIfExists
+import software.aws.toolkits.core.utils.touch
+import software.aws.toolkits.jetbrains.core.credentials.MockCredentialManagerExtension
+import software.aws.toolkits.jetbrains.core.gettingstarted.editor.GettingStartedPanel
+import software.aws.toolkits.jetbrains.settings.GettingStartedSettings
+
+@ExperimentalCoroutinesApi
+@ExtendWith(MockKExtension::class)
+class GettingStartedOnStartupTest {
+    companion object {
+        @JvmField
+        @RegisterExtension
+        val projectExtension = ProjectExtension()
+    }
+
+    @JvmField
+    @RegisterExtension
+    val credManagerExtension = MockCredentialManagerExtension()
+
+    private val sut = GettingStartedOnStartup()
+
+    @Test
+    fun `does not show screen if aws settings exist and has credentials`() {
+        mockkObject(GettingStartedPanel.Companion)
+        every { GettingStartedPanel.openPanel(any()) } returns null
+        GettingStartedSettings.getInstance().shouldDisplayPage = true
+        val fp = getPersistentStateComponentStorageLocation(GettingStartedSettings::class.java) ?: error(
+            "could not determine persistent storage for GettingStartedSettings"
+        )
+        try {
+            fp.touch()
+            sut.runActivity(projectExtension.project)
+        } finally {
+            fp.deleteIfExists()
+        }
+
+        verify(exactly = 0) {
+            GettingStartedPanel.openPanel(projectExtension.project)
+        }
+    }
+
+    @Test
+    fun `does not show screen if has previously shown screen`() {
+        mockkObject(GettingStartedPanel.Companion)
+        every { GettingStartedPanel.openPanel(any()) } returns null
+        GettingStartedSettings.getInstance().shouldDisplayPage = false
+        sut.runActivity(projectExtension.project)
+
+        verify(exactly = 0) {
+            GettingStartedPanel.openPanel(projectExtension.project)
+        }
+    }
+
+    @Test
+    fun `shows screen if aws settings exist and no credentials`() {
+        mockkObject(GettingStartedPanel.Companion)
+        every { GettingStartedPanel.openPanel(any()) } returns null
+        GettingStartedSettings.getInstance().shouldDisplayPage = true
+        val fp = getPersistentStateComponentStorageLocation(GettingStartedSettings::class.java) ?: error(
+            "could not determine persistent storage for GettingStartedSettings"
+        )
+        try {
+            fp.touch()
+            sut.runActivity(projectExtension.project)
+        } finally {
+            fp.deleteIfExists()
+        }
+
+        verify(exactly = 0) {
+            GettingStartedPanel.openPanel(projectExtension.project)
+        }
+    }
+
+    @Test
+    fun `shows screen on first install`() {
+        mockkObject(GettingStartedPanel.Companion)
+        every { GettingStartedPanel.openPanel(any()) } returns null
+        sut.runActivity(projectExtension.project)
+
+        verify {
+            GettingStartedPanel.openPanel(projectExtension.project)
+        }
+    }
+}
