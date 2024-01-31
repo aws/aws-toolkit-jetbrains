d2b0f51d7a365a8b97f63a8d461f40a2850eb68f
Richard Li <richali@amazon.com>
2024-01-31 13:16:02 -0800

    downgrade rider

--- buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
+++ buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
@@ -179,13 +179,13 @@ object IdeVersions {
                 )
             ),
             rider = RiderProfile(
-                sdkVersion = "2024.1-EAP1-SNAPSHOT",
+                sdkVersion = "2023.3",
                 plugins = commonPlugins + listOf(
                     "rider-plugins-appender" // Workaround for https://youtrack.jetbrains.com/issue/IDEA-179607
                 ),
                 netFrameworkTarget = "net472",
                 rdGenVersion = "2023.3.2",
-                nugetVersion = "2024.1.0-eap01"
+                nugetVersion = "2023.3.0"
             )
         ),
 
