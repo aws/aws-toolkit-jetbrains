7005747e6811c546827afa540b2d9e32ae6a5514
David Hasani <davhasan@amazon.com>
2024-02-13 12:27:16 -0800

    add back test

--- jetbrains-core/tst/software/aws/toolkits/jetbrains/services/codewhisperer/codemodernizer/ui/components/PreCodeTransformUserDialogTest.kt
+++ jetbrains-core/tst/software/aws/toolkits/jetbrains/services/codewhisperer/codemodernizer/ui/components/PreCodeTransformUserDialogTest.kt
@@ -6,21 +6,20 @@ package software.aws.toolkits.jetbrains.services.codewhisperer.codemodernizer.ui
 import com.intellij.openapi.projectRoots.JavaSdkVersion
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import software.aws.toolkits.jetbrains.services.codemodernizer.getJdkVersionText
 
 class PreCodeTransformUserDialogTest {
     @Test
     fun `getJdkVersionText returns proper text when Java version is supported`() {
-        assertEquals(JavaSdkVersion.JDK_1_8, JavaSdkVersion.JDK_1_8)
-//        val jdkVersionText = getJdkVersionText(JavaSdkVersion.JDK_1_8)
-//        val expectedText = "We found Java version: JDK_1_8. Select a different version if incorrect."
-//        assertEquals(jdkVersionText, expectedText)
+        val jdkVersionText = getJdkVersionText(JavaSdkVersion.JDK_1_8)
+        val expectedText = "We found Java version: JDK_1_8. Select a different version if incorrect."
+        assertEquals(jdkVersionText, expectedText)
     }
 
     @Test
     fun `getJdkVersionText returns proper text when Java version is unsupported`() {
-        assertEquals(JavaSdkVersion.JDK_1_8, JavaSdkVersion.JDK_1_8)
-//        val jdkVersionText = getJdkVersionText(JavaSdkVersion.JDK_1_5)
-//        val expectedText = "We found an unsupported Java version (JDK_1_5). Select your version here if incorrect."
-//        assertEquals(jdkVersionText, expectedText)
+        val jdkVersionText = getJdkVersionText(JavaSdkVersion.JDK_1_5)
+        val expectedText = "We found an unsupported Java version (JDK_1_5). Select your version here if incorrect."
+        assertEquals(jdkVersionText, expectedText)
     }
 }
