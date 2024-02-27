591e8d044b2f26e539c9fbeb25a33f1d395ffc8e
Richard Li <richali@amazon.com>
2024-02-27 13:23:49 -0800

    Add build skeleton for Q subplugin

---
+++ plugins/amazonq/codewhisperer/jetbrains-ultimate/build.gradle.kts
@@ -0,0 +1,17 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import software.aws.toolkits.gradle.intellij.IdeFlavor
+import software.aws.toolkits.gradle.intellij.IdeVersions
+
+plugins {
+    id("toolkit-intellij-subplugin")
+}
+
+intellijToolkit {
+    ideFlavor.set(IdeFlavor.IU)
+}
+
+dependencies {
+    compileOnly(project(":plugin-amazonq:codewhisperer:jetbrains-community"))
+}
---
+++ plugins/toolkit/jetbrains-core/src/software/aws/toolkits/resources/AwsToolkitBundle.kt
@@ -0,0 +1,23 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.resources
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+import java.util.function.Supplier
+
+object AwsToolkitBundle {
+    private const val BUNDLE_FQN: @NonNls String = "software.aws.toolkits.resources.MessagesBundle"
+    private val BUNDLE = DynamicBundle(AwsToolkitBundle::class.java, BUNDLE_FQN)
+
+    fun message(key: @PropertyKey(resourceBundle = BUNDLE_FQN) String, vararg params: Any): @Nls String {
+        return BUNDLE.getMessage(key, *params)
+    }
+
+    fun messagePointer(key: @PropertyKey(resourceBundle = BUNDLE_FQN) String, vararg params: Any): Supplier<String> {
+        return BUNDLE.getLazyMessage(key, *params)
+    }
+}
