0534f0814ac85386390980d188b46dafefcd392f
Richard Li <742829+rli@users.noreply.github.com>
2024-03-18 15:05:39 -0700

    Update PythonRuntimeGroupTest.kt

--- plugins/toolkit/jetbrains-core/tst/software/aws/toolkits/jetbrains/services/lambda/python/PythonRuntimeGroupTest.kt
+++ plugins/toolkit/jetbrains-core/tst/software/aws/toolkits/jetbrains/services/lambda/python/PythonRuntimeGroupTest.kt
@@ -52,7 +52,8 @@ class PythonRuntimeGroupTest {
     @Test
     fun testRuntimeDetection312() {
         val module = projectRule.module
-        projectRule.setModuleSdk(module, PyTestSdk.create("3.12.0"))
+        // PythonCodeInsightTestFixtureRule already sets SDK to 3.12
+        // projectRule.setModuleSdk(module, PyTestSdk.create("3.12.0"))
 
         assertThat(sut.determineRuntime(module)).isEqualTo(LambdaRuntime.PYTHON3_12)
     }
