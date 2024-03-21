16f8d968f63ab9ccf88432dee29fd28b7567b540
Andrew Yu <yuxqiang@amazon.com>
2024-03-21 12:34:57 -0700

    Remove QServiceNode files since it got migrated, and the not supported usecases

--- plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/cwqTab/CwQTreeRootNode.kt
+++ plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/cwqTab/CwQTreeRootNode.kt
@@ -8,17 +8,19 @@ import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.actionSystem.ActionGroup
 import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.project.Project
+import com.intellij.ui.SimpleTextAttributes
 import software.aws.toolkits.jetbrains.ToolkitPlaces
 import software.aws.toolkits.jetbrains.core.credentials.AwsBearerTokenConnection
 import software.aws.toolkits.jetbrains.core.credentials.ToolkitAuthManager
 import software.aws.toolkits.jetbrains.core.explorer.actions.AnActionTreeNode
-import software.aws.toolkits.jetbrains.core.explorer.cwqTab.nodes.QRootNode
 import software.aws.toolkits.jetbrains.core.gettingstarted.editor.ActiveConnection
-import software.aws.toolkits.jetbrains.core.gettingstarted.editor.ActiveConnectionType
 import software.aws.toolkits.jetbrains.core.gettingstarted.editor.BearerTokenFeatureSet
 import software.aws.toolkits.jetbrains.core.gettingstarted.editor.checkBearerConnectionValidity
+import software.aws.toolkits.jetbrains.services.amazonq.isQSupportedInThisVersion
 import software.aws.toolkits.jetbrains.services.codemodernizer.explorer.nodes.CodeModernizerRunModernizeNode
 import software.aws.toolkits.jetbrains.services.codemodernizer.isCodeModernizerAvailable
+import software.aws.toolkits.jetbrains.utils.isRunningOnRemoteBackend
+import software.aws.toolkits.resources.message
 
 class CwQTreeRootNode(private val nodeProject: Project) : AbstractTreeNode<Any>(nodeProject, Object()) {
     private val runCodeModernizerNode by lazy { CodeModernizerRunModernizeNode(nodeProject) }
@@ -36,14 +38,24 @@ class CwQTreeRootNode(private val nodeProject: Project) : AbstractTreeNode<Any>(
 
         val actions = ActionManager.getInstance().getAction(groupId) as ActionGroup
         val childNodes = actions.getChildren(null).mapNotNull {
-            AnActionTreeNode(project, ToolkitPlaces.CWQ_TOOL_WINDOW, it)
+            object : AnActionTreeNode(project, ToolkitPlaces.CWQ_TOOL_WINDOW, it) {
+                override fun update(presentation: PresentationData) {
+                    super.update(presentation)
+                    if (presentation.coloredText[0].text != message("action.q.openchat.text")) return
+                    if (groupId == Q_SIGNED_IN_ACTION_GROUP) {
+                        if (isRunningOnRemoteBackend()) {
+                            presentation.addText(message("codewhisperer.explorer.root_node.unavailable"), SimpleTextAttributes.GRAY_ATTRIBUTES)
+                        } else if (!isQSupportedInThisVersion()) {
+                            presentation.addText(message("q.unavailable"), SimpleTextAttributes.GRAY_ATTRIBUTES)
+                        }
+                    }
+                }
+            }
         }
 
-        // Temporary put the transform node in the panel, to be removed soon.
-        if (groupId == QRootNode.Q_SIGNED_IN_ACTION_GROUP) {
-            if (connection.connectionType == ActiveConnectionType.IAM_IDC && isCodeModernizerAvailable(project)) {
-                return childNodes + runCodeModernizerNode
-            }
+        // Temporary put the transform node in the panel(as the second node before the migration node), to be removed soon.
+        if (groupId == Q_SIGNED_IN_ACTION_GROUP && isCodeModernizerAvailable(project)) {
+            return listOf(childNodes[0], runCodeModernizerNode, childNodes[1])
         }
 
         return childNodes
