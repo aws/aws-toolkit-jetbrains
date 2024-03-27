165734f65dd42beb939a4bbaa4630bbd60c562a1
Richard Li <richali@amazon.com>
2024-03-27 12:49:16 -0700

    lint

--- plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/CodeTransformUtils2.kt
+++ plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/CodeTransformUtils2.kt
@@ -4,20 +4,9 @@
 package software.aws.toolkits.jetbrains.services.codemodernizer
 
 import com.intellij.openapi.application.ApplicationInfo
-import com.intellij.openapi.project.Project
-import software.aws.toolkits.jetbrains.core.gettingstarted.editor.ActiveConnection
-import software.aws.toolkits.jetbrains.core.gettingstarted.editor.ActiveConnectionType
-import software.aws.toolkits.jetbrains.core.gettingstarted.editor.BearerTokenFeatureSet
-import software.aws.toolkits.jetbrains.core.gettingstarted.editor.checkBearerConnectionValidity
 import java.time.Instant
 
 fun calculateTotalLatency(startTime: Instant, endTime: Instant) = (endTime.toEpochMilli() - startTime.toEpochMilli()).toInt()
-
-fun isCodeModernizerAvailable(project: Project): Boolean {
-    if (!isIntellij()) return false
-    val connection = checkBearerConnectionValidity(project, BearerTokenFeatureSet.Q)
-    return connection.connectionType == ActiveConnectionType.IAM_IDC && connection is ActiveConnection.ValidBearer
-}
 
 fun isIntellij(): Boolean {
     val productCode = ApplicationInfo.getInstance().build.productCode
