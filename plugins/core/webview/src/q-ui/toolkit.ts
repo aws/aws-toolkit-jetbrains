7629420201ec7bcdfbdc2d681ff2863f4579f8a0
Shao-Hua Lo <xshaohua@amazon.com>
2024-04-10 13:50:18 -0700

    patch

---
+++ plugins/core/webview/src/q-ui/toolkit.ts
@@ -0,0 +1,74 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// eslint-disable-next-line header/header
+import {createApp} from 'vue'
+import {createStore, Store} from 'vuex'
+import root from './components/root.vue'
+import {IdcInfo, Region, Stage, State} from "../model";
+import {IdeClient} from "../ideClient";
+import './assets/common.scss'
+
+const app = createApp(root, { app: 'TOOLKIT' })
+
+// const app = createApp({
+//     data() {
+//         return {
+//             message: 'Hello World!'
+//         }
+//     },
+//     template: '<h1>{{ message }}</h1>'
+//
+// })
+
+const store = createStore<State>({
+    state: {
+        stage: 'START' as Stage,
+        ssoRegions: [] as Region[],
+        authorizationCode: '',
+        lastLoginIdcInfo: {
+            profileName: '',
+            directoryId: '',
+            region: '',
+        }
+    },
+    getters: {},
+    mutations: {
+        setStage(state: State, stage: Stage) {
+            state.stage = stage
+        },
+        setSsoRegions(state: State, regions: Region[]) {
+            state.ssoRegions = regions
+        },
+        setAuthorizationCode(state: State, code: string) {
+            state.authorizationCode = code
+        },
+        setLastLoginIdcInfo(state: State, idcInfo: IdcInfo) {
+            console.log('state idc info is updated')
+            state.lastLoginIdcInfo.profileName = idcInfo.profileName
+            state.lastLoginIdcInfo.directoryId = idcInfo.directoryId
+            state.lastLoginIdcInfo.region = idcInfo.region
+        },
+        reset(state: State) {
+            state.stage = 'START'
+            state.ssoRegions = []
+            state.authorizationCode = ''
+            state.lastLoginIdcInfo = {
+                profileName: '',
+                directoryId: '',
+                region: ''
+            }
+        }
+    },
+    actions: {},
+    modules: {},
+})
+
+window.ideClient = new IdeClient(store)
+app.directive('autofocus', {
+    // When the bound element is inserted into the DOM...
+    mounted: function (el) {
+        el.focus();
+    }
+});
+app.use(store).mount('#app')
