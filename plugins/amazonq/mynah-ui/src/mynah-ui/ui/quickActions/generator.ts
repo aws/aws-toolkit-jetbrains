e1fc75b09d483bad8fde3780b5338167a32cd9c4
Dogus Atasoy <dogusata@amazon.com>
2024-04-11 14:45:07 +0200

    fixed autofocus to prompt input after code sent to Q (MynahUI: 4.5.4) fixed quick action commands inconsistency between Q tabs added no tabs description with open new tab button when no tab is open

--- plugins/amazonq/mynah-ui/src/mynah-ui/ui/quickActions/generator.ts
+++ plugins/amazonq/mynah-ui/src/mynah-ui/ui/quickActions/generator.ts
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { QuickActionCommandGroup } from '@aws/mynah-ui-chat/dist/static'
+import { QuickActionCommand, QuickActionCommandGroup } from '@aws/mynah-ui-chat/dist/static'
 import { TabType } from '../storages/tabsStorage'
 
 export interface QuickActionGeneratorProps {
@@ -21,51 +21,89 @@ export class QuickActionGenerator {
     }
 
     public generateForTab(tabType: TabType): QuickActionCommandGroup[] {
-        switch (tabType) {
-            case 'featuredev':
-                return []
-            default:
-                return [
-                    ...(this.isFeatureDevEnabled
-                        ? [
-                              {
-                                  groupName: 'Application Development',
-                                  commands: [
-                                      {
-                                          command: '/dev',
-                                          placeholder: 'Briefly describe a task or issue',
-                                          description:
-                                              'Use all project files as context for code suggestions (increases latency).',
-                                      },
-                                  ],
-                              },
-                          ]
-                        : []),
-                    ...(this.isCodeTransformEnabled
-                        ? [
-                              {
-                                  commands: [
-                                      {
-                                          command: '/transform',
-                                          description: 'Transform your Java 8 or 11 Maven project to Java 17',
-                                      },
-                                  ],
-                              },
-                          ]
-                        : []),
+        const quickActionCommands = [
+            ...(this.isFeatureDevEnabled
+                ? [
                     {
+                        groupName: 'Application Development',
                         commands: [
                             {
-                                command: '/help',
-                                description: 'Learn more about Amazon Q',
-                            },
-                            {
-                                command: '/clear',
-                                description: 'Clear this session',
+                                command: '/dev',
+                                placeholder: 'Briefly describe a task or issue',
+                                description:
+                                    'Use all project files as context for code suggestions (increases latency).',
                             },
                         ],
                     },
                 ]
+                : []),
+            ...(this.isCodeTransformEnabled
+                ? [
+                    {
+                        commands: [
+                            {
+                                command: '/transform',
+                                description: 'Transform your Java 8 or 11 Maven project to Java 17',
+                            },
+                        ],
+                    },
+                ]
+                : []),
+            {
+                commands: [
+                    {
+                        command: '/help',
+                        description: 'Learn more about Amazon Q',
+                    },
+                    {
+                        command: '/clear',
+                        description: 'Clear this session',
+                    },
+                ],
+            },
+        ]
+
+        const commandUnavailability: Record<
+            TabType,
+            {
+                description: string
+                unavailableItems: string[]
+            }
+        > = {
+            cwc: {
+                description: '',
+                unavailableItems: [],
+            },
+            featuredev: {
+                description: "This command isn't available in /dev",
+                unavailableItems: ['/dev', '/transform', '/help', '/clear'],
+            },
+            codetransform: {
+                description: "This command isn't available in /transform",
+                unavailableItems: ['/dev', '/transform'],
+            },
+            unknown: {
+                description: "This command isn't available",
+                unavailableItems: ['/dev', '/transform', '/help', '/clear'],
+            },
         }
+
+        return quickActionCommands.map(commandGroup => {
+            return {
+                groupName: commandGroup.groupName,
+                commands: commandGroup.commands.map((commandItem: QuickActionCommand) => {
+                    const commandNotAvailable = commandUnavailability[tabType].unavailableItems.includes(
+                        commandItem.command
+                    )
+                    return {
+                        ...commandItem,
+                        disabled: commandNotAvailable,
+                        description: commandNotAvailable
+                            ? commandUnavailability[tabType].description
+                            : commandItem.description,
+                    }
+                }) as QuickActionCommand[],
+            }
+        }) as QuickActionCommandGroup[]
     }
 }
