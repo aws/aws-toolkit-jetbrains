64351d33a70129b2ca5cc207b824125339b80029
Shao-Hua Lo <xshaohua@amazon.com>
2024-04-17 15:56:30 -0700

    q conn not pinned if it's builder id conn

--- plugins/core/jetbrains-community/src/software/aws/toolkits/jetbrains/core/credentials/pinning/QConnection.kt
+++ plugins/core/jetbrains-community/src/software/aws/toolkits/jetbrains/core/credentials/pinning/QConnection.kt
@@ -6,6 +6,8 @@ package software.aws.toolkits.jetbrains.core.credentials.pinning
 import software.aws.toolkits.jetbrains.core.credentials.AwsBearerTokenConnection
 import software.aws.toolkits.jetbrains.core.credentials.ToolkitConnection
 import software.aws.toolkits.jetbrains.core.credentials.sono.Q_SCOPES
+import software.aws.toolkits.jetbrains.core.credentials.sono.Q_SCOPES_UNAVAILABLE_BUILDER_ID
+import software.aws.toolkits.jetbrains.core.credentials.sono.isSono
 
 class QConnection : FeatureWithPinnedConnection {
     override val featureId = "aws.q"
@@ -13,6 +15,10 @@ class QConnection : FeatureWithPinnedConnection {
 
     override fun supportsConnectionType(connection: ToolkitConnection): Boolean {
         if (connection is AwsBearerTokenConnection) {
+            if (connection.isSono()) {
+                (Q_SCOPES - Q_SCOPES_UNAVAILABLE_BUILDER_ID.toSet()).all { it in connection.scopes }
+            }
+
             return Q_SCOPES.all { it in connection.scopes }
         }
 
