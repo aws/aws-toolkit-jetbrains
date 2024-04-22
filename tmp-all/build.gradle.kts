de491fdc6325ea0f349f5e330360e08b1c69ee87
Richard Li <richali@amazon.com>
2024-04-22 16:40:19 -0700

    tst

---
+++ tmp-all/build.gradle.kts
@@ -0,0 +1,27 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import software.aws.toolkits.gradle.intellij.IdeFlavor
+import software.aws.toolkits.gradle.intellij.IdeVersions
+
+plugins {
+    id("org.jetbrains.intellij")
+    id("toolkit-kotlin-conventions")
+    id("toolkit-testing")
+}
+
+intellij {
+    val ideProfile = IdeVersions.ideProfile(project)
+    version.set(ideProfile.community.version())
+    localPath.set(ideProfile.community.localPath())
+    plugins.set(
+        listOf(
+            project(":plugin-core"),
+            project(":plugin-amazonq"),
+            "aws.toolkit:2.19-233"
+        )
+    )
+
+    updateSinceUntilBuild.set(false)
+    instrumentCode.set(false)
+}
