ee4ae2693a1bb22563e5fd22755c5d975c14be0a
Richard Li <742829+rli@users.noreply.github.com>
2024-04-23 10:18:45 -0700

    Update build.gradle.kts

--- tmp-all/build.gradle.kts
+++ tmp-all/build.gradle.kts
@@ -1,7 +1,6 @@
 // Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import software.aws.toolkits.gradle.intellij.IdeFlavor
 import software.aws.toolkits.gradle.intellij.IdeVersions
 
 plugins {
