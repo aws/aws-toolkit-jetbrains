c3b533b91fb600a426f13246b2d907b76346ecf2
Galib Sarayev <sarayev@amazon.com>
2024-04-24 22:01:45 +0200

    update the welcome message

--- plugins/amazonq/mynah-ui/src/mynah-ui/ui/tabs/generator.ts
+++ plugins/amazonq/mynah-ui/src/mynah-ui/ui/tabs/generator.ts
@@ -47,9 +47,7 @@ export class TabDataGenerator {
             'featuredev',
             `Welcome to feature development.
 
-I can generate code to implement new functionality across your workspace. 
-
-We'll start by discussing an implementation plan, and then we can review and regenerate code multiple times until you find a solution that works for you. 
+I can generate code to implement new functionality across your workspace. We'll start by discussing an implementation plan, and then we can review and regenerate code based on your feedback. 
 
 To get started, describe the task you are trying to accomplish.
 `,
