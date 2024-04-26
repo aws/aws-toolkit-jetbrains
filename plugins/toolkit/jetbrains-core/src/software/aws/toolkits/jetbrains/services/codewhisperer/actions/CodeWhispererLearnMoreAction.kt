a779827bee72303d080f7abca7c23e3027fe21e5
Andrew Yu <yuxqiang@amazon.com>
2024-04-26 13:33:32 -0700

    detekt

--- plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/actions/CodeWhispererLearnMoreAction.kt
+++ plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/services/codewhisperer/actions/CodeWhispererLearnMoreAction.kt
@@ -8,9 +8,9 @@ import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAware
-import software.aws.toolkits.jetbrains.services.codewhisperer.util.CodeWhispererConstants.Q_MARKETPLACE_URI
 import software.aws.toolkits.jetbrains.services.codewhisperer.util.CodeWhispererConstants.CODEWHISPERER_LOGIN_LEARN_MORE_URI
 import software.aws.toolkits.jetbrains.services.codewhisperer.util.CodeWhispererConstants.CODEWHISPERER_SSO_LEARN_MORE_URI
+import software.aws.toolkits.jetbrains.services.codewhisperer.util.CodeWhispererConstants.Q_MARKETPLACE_URI
 import software.aws.toolkits.resources.message
 import java.net.URI
 
