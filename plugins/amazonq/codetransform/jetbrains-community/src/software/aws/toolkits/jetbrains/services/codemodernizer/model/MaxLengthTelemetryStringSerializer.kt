d9d51b107e20451e982ba1311c4f216c44ac45bb
Nick Ardecky <nardeck@amazon.com>
2024-05-31 13:34:55 -0700

    Chore - fix detekt issues

--- plugins/amazonq/codetransform/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/model/MaxLengthTelemetryStringSerializer.kt
+++ plugins/amazonq/codetransform/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/model/MaxLengthTelemetryStringSerializer.kt
@@ -1,10 +1,12 @@
+// Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
 package software.aws.toolkits.jetbrains.services.codemodernizer.model
 
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.SerializerProvider
 
-val MAX_SERIALIZABLE_STRING_LENGTH  = 1000
+const val MAX_SERIALIZABLE_STRING_LENGTH = 1000
 class MaxLengthTelemetryStringSerializer : JsonSerializer<String>() {
     override fun serialize(value: String, gen: JsonGenerator, provider: SerializerProvider) {
         val truncatedValue = if (value.length > MAX_SERIALIZABLE_STRING_LENGTH) {
