2a06d8f421fe9935dec7b5e146c3e2827f6f0691
vineeth chikoti <vchikoti@amazon.com>
2024-06-13 15:54:23 -0700

    tst debug

--- plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/steps/BuildLambda.kt
+++ plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/services/lambda/steps/BuildLambda.kt
@@ -13,6 +13,7 @@ import software.aws.toolkits.jetbrains.utils.execution.steps.StepEmitter
 import software.aws.toolkits.resources.message
 import java.nio.file.Files
 import java.nio.file.Path
+import java.util.UUID
 
 data class BuildLambdaRequest(
     val templatePath: Path,
@@ -25,7 +26,7 @@ data class BuildLambdaRequest(
 
 class BuildLambda(private val request: BuildLambdaRequest) : SamCliStep() {
     override val stepName: String = message("lambda.create.step.build")
-    private val gradleUserHome = Files.createTempDirectory("test-gradle-user-home").toAbsolutePath().toString()
+    private val gradleUserHome = Files.createTempDirectory("test-gradle-user-home-${UUID.randomUUID()}").toAbsolutePath().toString()
     private val buildEnvVars = mapOf("GRADLE_USER_HOME" to gradleUserHome)
 
     override fun constructCommandLine(context: Context): GeneralCommandLine = getCli().samBuildCommand(
