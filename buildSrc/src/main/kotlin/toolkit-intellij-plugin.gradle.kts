5a136dc9aa0d0d06c503e6b0c54806660c113b52
Richard Li <richali@amazon.com>
2024-06-26 17:20:21 -0700

    noop?

--- buildSrc/src/main/kotlin/toolkit-intellij-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/toolkit-intellij-plugin.gradle.kts
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.tasks.aware.RunnableIdeAware
+import software.aws.toolkits.gradle.ciOnly
 import software.aws.toolkits.gradle.intellij.ToolkitIntelliJExtension
 
 private val toolkitIntelliJ = project.extensions.create<ToolkitIntelliJExtension>("intellijToolkit")
@@ -30,4 +32,16 @@ dependencies {
 
 tasks.verifyPlugin {
     isEnabled = false
+}
+
+// CI keeps running out of RAM, so limit IDE instance count to 4
+ciOnly {
+    abstract class NoopBuildService : BuildService<BuildServiceParameters.None> {}
+    val noopService = gradle.sharedServices.registerIfAbsent("noopService", NoopBuildService::class.java) {
+        maxParallelUsages = 4
+    }
+
+    tasks.matching { it is RunnableIdeAware }.all {
+        usesService(noopService)
+    }
 }
