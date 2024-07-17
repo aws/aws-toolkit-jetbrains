bcfc7042f26df9d4f8ff1fc23ec8f7784790bc8e
Zoe Lin <zoelin@amazon.com>
2024-07-17 14:16:34 -0700

    enabled workspace context once for selected users

--- plugins/amazonq/chat/jetbrains-community/tst/software/aws/toolkits/jetbrains/services/amazonq/TelemetryHelperTest.kt
+++ plugins/amazonq/chat/jetbrains-community/tst/software/aws/toolkits/jetbrains/services/amazonq/TelemetryHelperTest.kt
@@ -140,7 +140,8 @@ class TelemetryHelperTest {
             userIntent = UserIntent.IMPROVE_CODE,
             triggerType = TriggerType.Hotkeys,
             customization = mockCustomization,
-            relevantTextDocuments = listOf()
+            relevantTextDocuments = listOf(),
+            useRelevantDocuments = true,
         )
         private val response = ChatMessage(
             tabId = tabId,
