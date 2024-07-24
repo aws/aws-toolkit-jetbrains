b9bba27680e42be94112eb227ec528fd61022b86
Richard Li <richali@amazon.com>
2024-07-24 19:46:56 +0000

    Use JetBrains CloudFront distribution directly to avoid gateway 502s from cache-redirector

--- buildSrc/src/main/kotlin/toolkit-intellij-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/toolkit-intellij-plugin.gradle.kts
@@ -1,10 +1,12 @@
 // Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+import org.jetbrains.intellij.platform.gradle.Constants
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 import org.jetbrains.intellij.platform.gradle.tasks.aware.TestableAware
 import software.aws.toolkits.gradle.ciOnly
 import software.aws.toolkits.gradle.intellij.ToolkitIntelliJExtension
+import java.net.URI
 
 private val toolkitIntelliJ = project.extensions.create<ToolkitIntelliJExtension>("intellijToolkit")
 
@@ -19,8 +21,13 @@ intellijPlatform {
 // there is an issue if this is declared more than once in a project (either directly or through script plugins)
 repositories {
     intellijPlatform {
-        defaultRepositories()
-        jetbrainsRuntime()
+        jetbrainsIdeInstallers()
+        localPlatformArtifacts()
+        intellijDependencies { url = URI(Constants.Locations.INTELLIJ_DEPENDENCIES_REPOSITORY) }
+        releases { url = URI(Constants.Locations.INTELLIJ_REPOSITORY_RELEASES) }
+        snapshots { url = URI(Constants.Locations.INTELLIJ_REPOSITORY_SNAPSHOTS) }
+        marketplace { url = URI("https://dtahfujkndrht.cloudfront.net/plugins.jetbrains.com/maven/") }
+        jetbrainsRuntime { url = URI("https://d2xrhe97vsfxuc.cloudfront.net") }
     }
 }
 
