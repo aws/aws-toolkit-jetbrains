b21e62185f49d90ec57f04d27a1aed876af4d1f0
Richard Li <richali@amazon.com>
2024-07-30 13:49:14 -0700

    switch

--- buildSrc/src/main/kotlin/toolkit-intellij-plugin.gradle.kts
+++ buildSrc/src/main/kotlin/toolkit-intellij-plugin.gradle.kts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.tasks.aware.SandboxAware
 import software.aws.toolkits.gradle.ciOnly
 import software.aws.toolkits.gradle.intellij.ToolkitIntelliJExtension
 
@@ -36,7 +37,7 @@ ciOnly {
         maxParallelUsages = 4
     }
 
-    tasks.withType<Test>().configureEach {
+    tasks.matching { it is Test || it is SandboxAware }.configureEach {
         usesService(noopService)
     }
 }
