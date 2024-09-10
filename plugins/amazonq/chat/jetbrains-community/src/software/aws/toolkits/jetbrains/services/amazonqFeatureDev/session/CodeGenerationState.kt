6afea396256c33e32364f44411b232d0ead474a5
wilson <wlsun@amazon.com>
2024-09-10 14:02:04 -0400

    Remove feature dev plan generation phase (#4844)
    Us (QSDA) team is launching a new code generation agent.
    
    We have conducted experiments wIth the new code generation agent, we observed:
    
        Removing planning phase produces higher quality code
        Removing planning phase has general better user experience and remove the inconsistency of code gen & plan gen
    
    We have discussed with product that the rollout of new code generation agent should also include planning phase removal as a whole, without an A/B testing mechanism nor backward compatible implementation. The endpoint for plan generation will still exist to support older toolkit versions.
    
    We plan to release the new code generation agent experience in the week of September 9th.

--- plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/session/CodeGenerationState.kt
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/session/CodeGenerationState.kt
@@ -12,6 +12,7 @@ import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.FEATURE_NAME
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.codeGenerationFailedError
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.featureDevServiceError
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.messages.sendAnswerPart
+import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.messages.sendUpdatePlaceholder
 import software.aws.toolkits.jetbrains.services.cwc.controller.chat.telemetry.getStartUrl
 import software.aws.toolkits.resources.message
 import software.aws.toolkits.telemetry.AmazonqTelemetry
@@ -49,6 +50,10 @@ class CodeGenerationState(
             messenger.sendAnswerPart(
                 tabId = tabID,
                 message = message("amazonqFeatureDev.code_generation.generating_code")
+            )
+            messenger.sendUpdatePlaceholder(
+                tabId = tabID,
+                newPlaceholder = message("amazonqFeatureDev.code_generation.generating_code")
             )
 
             val codeGenerationResult = generateCode(codeGenerationId = response.codeGenerationId(), messenger = messenger)
