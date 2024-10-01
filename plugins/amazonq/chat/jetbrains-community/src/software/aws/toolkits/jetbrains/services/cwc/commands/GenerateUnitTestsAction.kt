7ff4f29924078db65cbb4981b017929fa6c9cd54
Bodie Weedop <kbweedop@gmail.com>
2024-10-01 10:25:04 -0700

    feat(amazonq): Add command to generate unit tests for selected code (#4869)
    * feat (Amazon Q): Add basic UX and logic for generating unit tests
    
    * Update cwspr model with GENERATE_UNIT_TESTS user intent
    
    * fix (Amazon Q): Pass context specific intent and trigger type to chat (#4842)
    
    * Added authType to loginWithBrowser metric (#4843)
    
    * Added authType to loginWithBrowser metric
    
    * Add open and close sign in metrics
    
    * detekt
    
    * addressed feedback
    
    * feedback
    
    * moved the util function
    
    * Added UI click metrics on all login options (#4838)
    
    * Added UI click metrics on all login options
    
    * Combined calls for continue and back button
    
    * Updating version to 3.25
    
    * Updating SNAPSHOT version to 3.26-SNAPSHOT
    
    * Revert open and close sign in webview telemetry (#4850)
    
    * Revert open and close sign in webview telemetry
    
    * add changelog
    
    * added issue in changelog
    
    * Update bugfix-64bb0c4b-8d3b-45b0-b615-8722bb3e9e4a.json
    
    ---------
    
    Co-authored-by: Richard Li <742829+rli@users.noreply.github.com>
    
    * Updating version to 3.26
    
    * Updating SNAPSHOT version to 3.27-SNAPSHOT
    
    * telemetry(amazonq): update telemetry (#4847)
    
    * telemetry(amazonq): update telemetry
    
    * remove unused import
    
    ---------
    
    Co-authored-by: David Hasani <davhasan@amazon.com>
    
    * feat(amazonq): Reduce @workspace indexing time by 50% (#4846)
    
    * Hash startUrl
    
    * Revert CodeWhisperer endpoint back gamma endpoint
    
    * Revert adding user intent to interact with message metadata
    
    * Add changelog
    
    * Refactor for consistent hashing of startUrl
    
    * Remove hashing logic on startUrl and changelog entry
    
    * Add (Beta) suffix to generate unit tests command
    
    * Add check for startUrl when getting user intent from prompt
    
    * Update TestCodeAction to resolve lint failure
    
    Co-authored-by: Richard Li <742829+rli@users.noreply.github.com>
    
    * address comments: Better GenerateUnitTest Action naming
    
    * Refactor to use more specific command for UTG
    
    * Change name of action to GenerateUnitTests
    
    * Fix linting errors
    
    * Override 'getActionUpdateThread'
    
    * Reorder MessageBundle.properties
    
    * Fix syntax error
    
    ---------
    
    Co-authored-by: manodnyab <66754471+manodnyab@users.noreply.github.com>
    Co-authored-by: aws-toolkit-automation <>
    Co-authored-by: Richard Li <742829+rli@users.noreply.github.com>
    Co-authored-by: David <60020664+dhasani23@users.noreply.github.com>
    Co-authored-by: David Hasani <davhasan@amazon.com>
    Co-authored-by: Lei Gao <97199248+leigaol@users.noreply.github.com>
    Co-authored-by: David Lin <daveln@amazon.com>

---
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/cwc/commands/GenerateUnitTestsAction.kt
@@ -0,0 +1,21 @@
+// Copyright 2023 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.cwc.commands
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import software.aws.toolkits.jetbrains.core.credentials.AwsBearerTokenConnection
+import software.aws.toolkits.jetbrains.core.credentials.ToolkitConnectionManager
+import software.aws.toolkits.jetbrains.core.credentials.pinning.QConnection
+
+class GenerateUnitTestsAction : CustomAction(EditorContextCommand.GenerateUnitTests) {
+    override fun getActionUpdateThread() = ActionUpdateThread.BGT
+
+    override fun update(e: AnActionEvent) {
+        val project = e.getData(CommonDataKeys.PROJECT) ?: return
+        val connection = ToolkitConnectionManager.getInstance(project).activeConnectionForFeature(QConnection.getInstance()) as? AwsBearerTokenConnection
+        e.presentation.isEnabledAndVisible = connection?.startUrl == "https://amzn.awsapps.com/start"
+    }
+}
