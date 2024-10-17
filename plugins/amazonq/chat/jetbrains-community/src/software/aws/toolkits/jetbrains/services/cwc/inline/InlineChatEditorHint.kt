fa5317311f613be46730def962a439e3fa6540ec
Zoe Lin <zoelin@amazon.com>
2024-10-16 17:07:52 -0700

    improve hint location

--- plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/cwc/inline/InlineChatEditorHint.kt
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/cwc/inline/InlineChatEditorHint.kt
@@ -6,6 +6,7 @@ import com.intellij.codeInsight.hint.HintManager
 import com.intellij.codeInsight.hint.HintManagerImpl
 import com.intellij.codeInsight.hint.HintUtil
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.util.SystemInfo
 import com.intellij.ui.LightweightHint
 import com.intellij.ui.SimpleColoredText
@@ -19,24 +20,17 @@ import javax.swing.JPanel
 
 class InlineChatEditorHint {
     private val hint = createHint()
-    private val HINT_BUFFER = 50
+    private val HINT_LOCATION_BUFFER = 50
 
     private fun getHintLocation (editor: Editor): Point {
         val selectionModel = editor.selectionModel
-        val selectionEnd = selectionModel.selectionEnd
-        val selectionLineEnd = editor.document.getLineEndOffset(editor.document.getLineNumber(selectionEnd))
-
-        val xyPosition = editor.offsetToXY(selectionLineEnd)
-        val editorLocation = editor.component.locationOnScreen
-        val editorContentLocation = editor.contentComponent.locationOnScreen
-        val position = Point(
-            editorContentLocation.x + xyPosition.x,
-            editorLocation.y + xyPosition.y - editor.scrollingModel.verticalScrollOffset - HINT_BUFFER)
-
+        val lineSelected = selectionModel.selectedText?.split("\n")?.size
+        val offset: Int = if (lineSelected != null && lineSelected > 1) {(lineSelected - 1).times(editor.lineHeight)} else {0}
+        val bestPosition = JBPopupFactory.getInstance().guessBestPopupLocation(editor).point
         val visibleArea = editor.scrollingModel.visibleArea
 
-        val adjustedX = (position.x ).coerceAtMost(visibleArea.x + visibleArea.width - HINT_BUFFER)
-        val adjustedY = (position.y ).coerceAtMost(visibleArea.y + visibleArea.height - HINT_BUFFER)
+        val adjustedX = (bestPosition.x + 200).coerceAtMost(visibleArea.x + visibleArea.width - HINT_LOCATION_BUFFER)
+        val adjustedY = (bestPosition.y + offset).coerceAtMost(visibleArea.y + visibleArea.height - HINT_LOCATION_BUFFER)
         val adjustedPosition = Point(adjustedX, adjustedY)
 
         return adjustedPosition
