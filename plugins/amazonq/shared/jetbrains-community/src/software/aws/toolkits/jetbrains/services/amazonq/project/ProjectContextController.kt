969fbdad13bd91351dbe5ea69a6b423a55e836a4
Will Lo <96078566+Will-ShaoHua@users.noreply.github.com>
2024-10-18 13:16:48 -0700

    refactor(amazonq): move projectContext folder to amazonq/shared (#4972)
    preparation work for #4976
    depends on #4970, #4971

--- plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/project/ProjectContextController.kt
+++ plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/project/ProjectContextController.kt
@@ -1,7 +1,7 @@
 // Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-package software.aws.toolkits.jetbrains.services.cwc.editor.context.project
+package software.aws.toolkits.jetbrains.services.amazonq.project
 
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
