c3c01cd7e3eec6da9dd65c9e6ae83d678099e60b
Zoe Lin <zoelin@amazon.com>
2024-10-29 16:17:48 -0700

    add editor and project check

--- plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/cwc/inline/InlineChatActionPromoter.kt
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/cwc/inline/InlineChatActionPromoter.kt
@@ -5,6 +5,7 @@ package software.aws.toolkits.jetbrains.services.cwc.inline
 
 import com.intellij.openapi.actionSystem.ActionPromoter
 import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.DataContext
 import com.intellij.openapi.keymap.KeymapUtil
 import com.intellij.openapi.util.SystemInfo
@@ -13,6 +14,7 @@ class InlineChatActionPromoter : ActionPromoter {
     // temporary until we find a better key binding
     override fun promote(actions: MutableList<out AnAction>, context: DataContext): MutableList<AnAction> {
         val results = actions.toMutableList()
+        if (context.getData(CommonDataKeys.EDITOR) == null || context.getData(CommonDataKeys.PROJECT) == null) return results
         val shortCut = KeymapUtil.getShortcutText("aws.toolkit.jetbrains.core.services.cwc.inline.openChat")
         // only promote for the default key bindings
         if (SystemInfo.isMac && shortCut != "⌘I") return results
