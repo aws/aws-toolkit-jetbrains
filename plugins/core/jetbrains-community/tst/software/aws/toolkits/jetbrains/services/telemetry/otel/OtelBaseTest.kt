c457e1f87a0e1d7afbe8b4708ba1c97ad61ee10e
Richard Li <742829+rli@users.noreply.github.com>
2024-11-04 15:47:40 -0800

    Update OtelBaseTest.kt

--- plugins/core/jetbrains-community/tst/software/aws/toolkits/jetbrains/services/telemetry/otel/OtelBaseTest.kt
+++ plugins/core/jetbrains-community/tst/software/aws/toolkits/jetbrains/services/telemetry/otel/OtelBaseTest.kt
@@ -289,7 +289,6 @@ class OtelExtension : AfterEachCallback, AfterAllCallback {
                 }
 
                 override fun onEnd(span: ReadableSpan) {
-                    println(span)
                     _completedSpans.add(span)
 
                     if (!openSpans.contains(span)) {
