5a8faaea2330d45ae236adbc10053721a21f759c
Zoe Lin <zoelin@amazon.com>
2024-11-12 18:04:27 -0800

    patch for #5076

--- plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/cwc/inline/OpenChatInputAction.kt
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/cwc/inline/OpenChatInputAction.kt
@@ -3,15 +3,21 @@
 
 package software.aws.toolkits.jetbrains.services.cwc.inline
 
-import com.intellij.execution.impl.ConsoleViewImpl
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.util.Key
 
 class OpenChatInputAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val editor = e.getData(CommonDataKeys.EDITOR) ?: return
-        val isConsole = editor.document.getUserData(ConsoleViewImpl.IS_CONSOLE_DOCUMENT)
+        // this is a hack since this key does not exist in 233
+        // TODO: change below to use ConsoleViewImpl.IS_CONSOLE_DOCUMENT after 233 support is removed
+        var isConsole: Any? = null
+        val key: Key<*>? = Key.findKeyByName("IS_CONSOLE_DOCUMENT")
+        if (key != null) {
+            isConsole = editor.document.getUserData(key)
+        }
         if (isConsole == true) return
         if (!editor.document.isWritable) return
         val project = editor.project ?: return
