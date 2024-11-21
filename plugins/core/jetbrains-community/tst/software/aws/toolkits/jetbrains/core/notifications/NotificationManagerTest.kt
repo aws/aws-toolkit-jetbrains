87ec1d909c0ac95d8dcc5fc57775be8638204111
Manodnya Bhoite <manodnyb@amazon.com>
2024-11-21 12:47:19 -0800

    fixed tests

--- plugins/core/jetbrains-community/tst/software/aws/toolkits/jetbrains/core/notifications/NotificationManagerTest.kt
+++ plugins/core/jetbrains-community/tst/software/aws/toolkits/jetbrains/core/notifications/NotificationManagerTest.kt
@@ -3,29 +3,37 @@
 
 package software.aws.toolkits.jetbrains.core.notifications
 
+import com.intellij.testFramework.ApplicationExtension
+import com.intellij.testFramework.ProjectRule
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.Rule
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(ApplicationExtension::class)
 class NotificationManagerTest {
+    @Rule
+    @JvmField
+    val projectRule = ProjectRule()
 
     @Test
     fun `If no follow-up actions, expand action is present`() {
-        val sut = NotificationManager.createActions(null, "Dummy Test Action", "Dummy title")
+        val sut = NotificationManager.createActions(projectRule.project, listOf(), "Dummy Test Action", "Dummy title")
         assertThat(sut).isNotNull
         assertThat(sut).hasSize(1)
-        assertThat(sut.first().title).isEqualTo("Expand")
+        assertThat(sut.first().title).isEqualTo("More...")
     }
 
     @Test
     fun `Show Url action shows the option to learn more`() {
         val followupActions = NotificationFollowupActions(
-            "ShowUrl",
-            NotificationFollowupActionsContent(NotificationActionDescription("title", "http://leadsnowhere"))
+            "UpdateExtension",
+            NotificationFollowupActionsContent(NotificationActionDescription("title", null))
         )
-        val sut = NotificationManager.createActions(listOf(followupActions), "Dummy Test Action", "Dummy title")
+        val sut = NotificationManager.createActions(projectRule.project,listOf(followupActions), "Dummy Test Action", "Dummy title")
         assertThat(sut).isNotNull
         assertThat(sut).hasSize(2)
-        assertThat(sut.first().title).isEqualTo("Expand")
-        assertThat(sut[1].title).isEqualTo("Learn more")
+        assertThat(sut.first().title).isEqualTo("Update")
+        assertThat(sut[1].title).isEqualTo("More...")
     }
 }
