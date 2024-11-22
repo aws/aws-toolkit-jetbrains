dfe18e99329d2bfa0c12afd58ec9db5d6f9e2253
manodnyab <66754471+manodnyab@users.noreply.github.com>
2024-11-22 15:54:35 -0800

    Show toasts for notifications and notification banner on critical notifications(#5097)
    * Display toast notifications with actions
    
    * Condition matcher for displaying notifications
    
    * Show notification banner
    
    * feedback 1
    
    * Modified deserialization cases and added tests
    
    * not required file change
    
    * feedback 1
    
    * feedback 1
    
    * modified the base class
    
    * merge conflicts resolved
    
    * rearranged call site
    
    * show notifications when panel is opened
    
    * fixed tests
    
    * detekt
    
    * feedback
    
    * convert panels into wrappers
    
    * fixed test

--- plugins/core/jetbrains-community/src/software/aws/toolkits/jetbrains/core/notifications/ProcessNotificationsBase.kt
+++ plugins/core/jetbrains-community/src/software/aws/toolkits/jetbrains/core/notifications/ProcessNotificationsBase.kt
@@ -3,9 +3,16 @@
 
 package software.aws.toolkits.jetbrains.core.notifications
 
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import software.aws.toolkits.jetbrains.utils.notifyStickyWithData
 
+@Service(Service.Level.PROJECT)
 class ProcessNotificationsBase {
+    private val notifListener = mutableListOf<NotifListener>()
     init {
         // TODO: install a listener for the polling class
     }
@@ -22,13 +29,49 @@ class ProcessNotificationsBase {
     fun processNotification(project: Project, notificationData: NotificationData) {
         val shouldShow = RulesEngine.displayNotification(project, notificationData)
         if (shouldShow) {
-            // TODO: notifies listeners
+            val notificationContent = notificationData.content.locale
+            val severity = notificationData.severity
+            val followupActions = NotificationManager.createActions(
+                project,
+                notificationData.actions,
+                notificationContent.description,
+                notificationContent.title
+            )
+            showToast(
+                notificationContent.title,
+                notificationContent.description,
+                NotificationManager.buildNotificationActions(followupActions),
+                checkSeverity(severity),
+                notificationData.id
+            )
+            if (severity == "Critical") {
+                val bannerContent = BannerContent(notificationContent.title, notificationContent.description, followupActions, notificationData.id)
+                showBannerNotification[notificationData.id] = bannerContent
+                notifyListenerForNotification(bannerContent)
+            }
         }
     }
 
-    fun notifyListenerForNotification() {
+    private fun showToast(title: String, message: String, action: List<AnAction>, notificationType: NotificationSeverity, notificationId: String) {
+        val notifyType = when (notificationType) {
+            NotificationSeverity.CRITICAL -> NotificationType.ERROR
+            NotificationSeverity.WARNING -> NotificationType.WARNING
+            NotificationSeverity.INFO -> NotificationType.INFORMATION
+        }
+        notifyStickyWithData(notifyType, title, message, null, action, notificationId)
     }
 
-    fun addListenerForNotification() {
+    fun notifyListenerForNotification(bannerContent: BannerContent) =
+        notifListener.forEach { it(bannerContent) }
+
+    fun addListenerForNotification(newNotifListener: NotifListener) =
+        notifListener.add(newNotifListener)
+
+    companion object {
+        fun getInstance(project: Project): ProcessNotificationsBase = project.service()
+
+        val showBannerNotification = mutableMapOf<String, BannerContent>()
     }
 }
+
+typealias NotifListener = (bannerContent: BannerContent) -> Unit
