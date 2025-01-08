18e826533665cf694865fd470dfaa2eb4717c8cd
Ashish Reddy Podduturi <ashishrp@amazon.com>
2025-01-07 19:55:16 -0800

    Fix for build failure

--- plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqCodeTest/CodeWhispererUTGChatManager.kt
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqCodeTest/CodeWhispererUTGChatManager.kt
@@ -512,6 +512,7 @@ class CodeWhispererUTGChatManager(val project: Project, private val cs: Coroutin
                 AmazonqTelemetry.utgGenerateTests(
                     cwsprChatProgrammingLanguage = session.programmingLanguage.languageId,
                     hasUserPromptSupplied = session.hasUserPromptSupplied,
+                    isSupportedFile = true,
                     isSupportedLanguage = true,
                     credentialStartUrl = getStartUrl(project),
                     jobGroup = session.testGenerationJobGroupName,
