81fe43553c4355a1ed42a629231cca9fd171bcfd
Ashish Reddy Podduturi <ashishrp@amazon.com>
2025-01-07 18:18:24 -0800

    fix for /test for files outside project scope.

--- plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqCodeTest/controller/CodeTestChatController.kt
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqCodeTest/controller/CodeTestChatController.kt
@@ -12,6 +12,7 @@ import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
@@ -157,6 +158,10 @@ class CodeTestChatController(
         // check if IDE has active file open, yes return (fileName and filePath) else return null
         val project = context.project
         val fileInfo = checkActiveFileInIDE(project, message) ?: return
+        val projectRoot = Path.of(
+            project.basePath ?: project.guessProjectDir()?.path
+                ?: error("Cannot guess base directory for project ${project.name}")
+        )
         session.programmingLanguage = fileInfo.fileLanguage
         if (session.isGeneratingTests === true) {
             return
@@ -184,7 +189,8 @@ class CodeTestChatController(
             message.tabId,
             false
         )
-        if (isLanguageSupported(fileInfo.fileLanguage.languageId)) {
+        val supported = fileInfo.filePath.startsWith(projectRoot.toString()) && isLanguageSupported(fileInfo.fileLanguage.languageId)
+        if (supported) {
             // Send Capability card to chat
             codeTestChatHelper.addNewMessage(
                 CodeTestChatMessageContent(informationCard = true, message = null, type = ChatMessageType.Answer, canBeVoted = false),
@@ -230,9 +236,15 @@ class CodeTestChatController(
                 }
                 .build()
 
-            val messageContent = "<span style=\"color: #EE9D28;\">&#9888;<b> ${fileInfo.fileLanguage.languageId} is not a " +
-                "language I support specialized unit test generation for at the moment.</b><br></span>The languages " +
-                "I support now are Python and Java. I can still provide examples, instructions and code suggestions."
+            val messageContent = if (fileInfo.filePath.startsWith(projectRoot.toString())) {
+                "<span style=\"color: #EE9D28;\">&#9888;<b> ${fileInfo.fileLanguage.languageId} is not a " +
+                    "language I support specialized unit test generation for at the moment.</b><br></span>The languages " +
+                    "I support now are Python and Java. I can still provide examples, instructions and code suggestions."
+            } else {
+                "<span style=\"color: #EE9D28;\">&#9888;<b>External File Detected: ${fileInfo.fileName} \n" +
+                    " is outside the project directory.</b><br></span>" +
+                    "However, I can still help you create unit tests for ${fileInfo.fileName} here"
+            }
 
             codeTestChatHelper.addNewMessage(
                 CodeTestChatMessageContent(
