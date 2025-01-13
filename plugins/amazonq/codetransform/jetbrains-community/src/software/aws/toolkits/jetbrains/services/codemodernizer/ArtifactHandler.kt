479f98a9793afd6ceaaa8c50a5dc68ac9ea02c61
David Hasani <davhasan@amazon.com>
2025-01-13 14:24:03 -0800

    fix(amazonq): add start a new transformation button

--- plugins/amazonq/codetransform/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/ArtifactHandler.kt
+++ plugins/amazonq/codetransform/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/ArtifactHandler.kt
@@ -317,7 +317,6 @@ class ArtifactHandler(
                         message = message("codemodernizer.chat.message.changes_applied"),
                     )
                     codeTransformChatHelper?.updateLastPendingMessage(resultContent)
-                    codeTransformChatHelper?.addNewMessage(buildStartNewTransformFollowup())
                 } else {
                     if (getCurrentPatchIndex() < totalPatchFiles) {
                         val message = "I applied the changes in diff patch ${getCurrentPatchIndex() + 1} of $totalPatchFiles. " +
@@ -331,7 +330,6 @@ class ArtifactHandler(
                             codeTransformChatHelper?.updateLastPendingMessage(
                                 CodeTransformChatMessageContent(type = CodeTransformChatMessageType.PendingAnswer, message = message)
                             )
-                            codeTransformChatHelper?.addNewMessage(buildStartNewTransformFollowup())
                         } else {
                             codeTransformChatHelper?.updateLastPendingMessage(
                                 CodeTransformChatMessageContent(
@@ -345,7 +343,7 @@ class ArtifactHandler(
                             )
                         }
                     } else {
-                        codeTransformChatHelper?.addNewMessage(buildStartNewTransformFollowup())
+                        // no-op; start a new transformation button already visible at this point
                     }
                 }
             } else {
