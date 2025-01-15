29f5b540beda3e5b02e25e02a69b8ef6abe0330c
Richard Li <richali@amazon.com>
2025-01-15 13:31:53 -0800

    report

--- coverage-report/build.gradle.kts
+++ coverage-report/build.gradle.kts
@@ -24,6 +24,15 @@ reporting {
     reports {
         val testCodeCoverageReport by creating(JacocoCoverageReport::class) {
             testType = TestSuiteType.UNIT_TEST
+            // https://github.com/gradle/gradle/issues/20026
+            reportTask.configure {
+                classDirectories.setFrom(classDirectories.map {
+                    fileTree(it).matching {
+                        include("**/software/aws/toolkits/**")
+                        exclude("**/software/aws/toolkits/telemetry/**")
+                    }
+                })
+            }
         }
     }
 }
