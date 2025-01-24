aws/aws-toolkit-jetbrains

commit eb22f093a322ffb2149941a6cad0d3eade2a0c3e

feat(feature dev): Add setting to allow Q to run code and test commands (#5077)
Q /dev team is launching a new feature: Allow Q to execute build/test based on customer's configuration 🎉🎉

We have made a couple tweaks to incorporate the user experience
- Added a new setting to allow Q to run code and test commands per project
- Added a pre-filled prompt option for customer to generate a configuration for this feature
- Added a couple follow up buttons throughout the /dev flow and added logic around them
- Updated copies
- Updated `codefileExtensions` set

plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/FeatureDevConstants.kt
@@ -7,6 +7,9 @@ const val FEATURE_EVALUATION_PRODUCT_NAME = "FeatureDev"
 
 const val FEATURE_NAME = "Amazon Q Developer Agent for software development"
 
+@Suppress("MaxLineLength")
+const val GENERATE_DEV_FILE_PROMPT = "generate a devfile in my repository. Note that you should only use devfile version 2.0.0 and the only supported command is test, so you should bundle all install, build and test commands in “test”. also you can use “public.ecr.aws/aws-mde/universal-image:latest” as universal image if you aren’t sure which image to use. here is an example for a node repository (but don't assume it's always a node project. look at the existing repository structure before generating the devfile): schemaVersion: 2.0.0 components: - name: dev container: image: public.ecr.aws/aws-mde/universal-image:latest commands: - id: test exec: component: dev commandLine: \"npm install && npm run build && npm run test\""
+
 // Max number of times a user can attempt to retry a code generation request if it fails
 const val CODE_GENERATION_RETRY_LIMIT = 3
 

plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/controller/FeatureDevController.kt
@@ -31,11 +31,13 @@ import software.aws.toolkits.jetbrains.core.coroutines.EDT
 import software.aws.toolkits.jetbrains.services.amazonq.RepoSizeError
 import software.aws.toolkits.jetbrains.services.amazonq.apps.AmazonQAppInitContext
 import software.aws.toolkits.jetbrains.services.amazonq.auth.AuthController
+import software.aws.toolkits.jetbrains.services.amazonq.messages.MessagePublisher
 import software.aws.toolkits.jetbrains.services.amazonq.toolwindow.AmazonQToolWindowFactory
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.CodeIterationLimitException
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.DEFAULT_RETRY_LIMIT
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.FEATURE_NAME
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.FeatureDevException
+import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.GENERATE_DEV_FILE_PROMPT
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.InboundAppMessagesHandler
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.ModifySourceFolderErrorReason
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.MonthlyConversationLimitError
@@ -75,6 +77,7 @@ import software.aws.toolkits.jetbrains.services.codewhisperer.util.content
 import software.aws.toolkits.jetbrains.services.cwc.controller.chat.telemetry.FeedbackComment
 import software.aws.toolkits.jetbrains.services.cwc.controller.chat.telemetry.getStartUrl
 import software.aws.toolkits.jetbrains.services.telemetry.TelemetryService
+import software.aws.toolkits.jetbrains.settings.CodeWhispererSettings
 import software.aws.toolkits.jetbrains.ui.feedback.FeatureDevFeedbackDialog
 import software.aws.toolkits.jetbrains.utils.notifyError
 import software.aws.toolkits.resources.message
@@ -133,6 +136,16 @@ class FeatureDevController(
             FollowUpTypes.PROVIDE_FEEDBACK_AND_REGENERATE_CODE -> provideFeedbackAndRegenerateCode(message.tabId)
             FollowUpTypes.NEW_TASK -> newTask(message.tabId)
             FollowUpTypes.CLOSE_SESSION -> closeSession(message.tabId)
+            FollowUpTypes.ACCEPT_AUTO_BUILD -> handleDevCommandUserSetting(message.tabId, true)
+            FollowUpTypes.DENY_AUTO_BUILD -> handleDevCommandUserSetting(message.tabId, false)
+            FollowUpTypes.GENERATE_DEV_FILE -> {
+                messenger.sendAnswer(
+                    tabId = message.tabId,
+                    messageType = FeatureDevMessageType.SystemPrompt,
+                    message = message("amazonqFeatureDev.follow_up.generate_dev_file")
+                )
+                newTask(tabId = message.tabId, prefilledPrompt = GENERATE_DEV_FILE_PROMPT)
+            }
         }
     }
 
@@ -439,20 +452,38 @@ class FeatureDevController(
                 canBeVoted = true
             )
 
-            messenger.sendSystemPrompt(
-                tabId = tabId,
-                followUp = listOf(
+            val followUps = mutableListOf(
+                FollowUp(
+                    pillText = message("amazonqFeatureDev.follow_up.new_task"),
+                    type = FollowUpTypes.NEW_TASK,
+                    status = FollowUpStatusType.Info
+                ),
+                FollowUp(
+                    pillText = message("amazonqFeatureDev.follow_up.close_session"),
+                    type = FollowUpTypes.CLOSE_SESSION,
+                    status = FollowUpStatusType.Info
+                ),
+            )
+
+            if (!session.context.checkForDevFile()) {
+                followUps.add(
                     FollowUp(
-                        pillText = message("amazonqFeatureDev.follow_up.new_task"),
-                        type = FollowUpTypes.NEW_TASK,
-                        status = FollowUpStatusType.Info
-                    ),
-                    FollowUp(
-                        pillText = message("amazonqFeatureDev.follow_up.close_session"),
-                        type = FollowUpTypes.CLOSE_SESSION,
+                        pillText = message("amazonqFeatureDev.follow_up.generate_dev_file"),
+                        type = FollowUpTypes.GENERATE_DEV_FILE,
                         status = FollowUpStatusType.Info
                     )
                 )
+
+                messenger.sendAnswer(
+                    tabId = tabId,
+                    message = message("amazonqFeatureDev.chat_message.generate_dev_file"),
+                    messageType = FeatureDevMessageType.Answer
+                )
+            }
+
+            messenger.sendSystemPrompt(
+                tabId = tabId,
+                followUp = followUps
             )
 
             messenger.sendUpdatePlaceholder(
@@ -470,9 +501,7 @@ class FeatureDevController(
         }
     }
 
-    private suspend fun newTask(tabId: String, isException: Boolean? = false) {
-        this.disablePreviousFileList(tabId)
-
+    private suspend fun newTask(tabId: String, isException: Boolean? = false, prefilledPrompt: String? = null) {
         val session = getSessionInfo(tabId)
         val sessionLatency = System.currentTimeMillis() - session.sessionStartTime
 
@@ -484,15 +513,30 @@ class FeatureDevController(
         chatSessionStorage.deleteSession(tabId)
 
         newTabOpened(tabId)
-        if (isException != null && !isException) {
-            messenger.sendAnswer(
-                tabId = tabId,
-                messageType = FeatureDevMessageType.Answer,
-                message = message("amazonqFeatureDev.chat_message.ask_for_new_task")
-            )
+
+        if (prefilledPrompt != null && isException != null && !isException) {
+            handleChat(tabId = tabId, message = prefilledPrompt)
+        } else {
+            if (isException != null && !isException) {
+                messenger.sendAnswer(
+                    tabId = tabId,
+                    messageType = FeatureDevMessageType.Answer,
+                    message = message("amazonqFeatureDev.chat_message.ask_for_new_task")
+                )
+            }
+            messenger.sendUpdatePlaceholder(tabId = tabId, newPlaceholder = message("amazonqFeatureDev.placeholder.new_plan"))
+            messenger.sendChatInputEnabledMessage(tabId = tabId, enabled = true)
         }
-        messenger.sendUpdatePlaceholder(tabId = tabId, newPlaceholder = message("amazonqFeatureDev.placeholder.new_plan"))
-        messenger.sendChatInputEnabledMessage(tabId = tabId, enabled = true)
+    }
+
+    private suspend fun handleDevCommandUserSetting(tabId: String, value: Boolean) {
+        CodeWhispererSettings.getInstance().toggleAutoBuildFeature(context.project.basePath, value)
+        messenger.sendAnswer(
+            tabId = tabId,
+            message = message("amazonqFeatureDev.chat_message.setting_updated"),
+            messageType = FeatureDevMessageType.Answer,
+        )
+        this.retryRequests(tabId)
     }
 
     private suspend fun closeSession(tabId: String) {
@@ -669,6 +713,7 @@ class FeatureDevController(
         try {
             logger.debug { "$FEATURE_NAME: Processing message: $message" }
             session = getSessionInfo(tabId)
+            session.latestMessage = message
 
             val credentialState = authController.getAuthNeededStates(context.project).amazonQ
             if (credentialState != null) {
@@ -681,7 +726,16 @@ class FeatureDevController(
                 return
             }
 
-            session.preloader(message, messenger)
+            val codeWhispererSettings = CodeWhispererSettings.getInstance().getAutoBuildFeatureConfiguration()
+            val hasDevFile = session.context.checkForDevFile()
+            val isPromptedForAutoBuildFeature = codeWhispererSettings.containsKey(session.context.getWorkspaceRoot())
+
+            if (hasDevFile && !isPromptedForAutoBuildFeature) {
+                promptAllowQCommandsConsent(messenger, tabId)
+                return
+            }
+
+            session.preloader(messenger)
 
             when (session.sessionState.phase) {
                 SessionStatePhase.CODEGEN -> onCodeGeneration(session, message, tabId)
@@ -693,6 +747,30 @@ class FeatureDevController(
             // Lock the chat input until they explicitly click one of the follow-ups
             messenger.sendChatInputEnabledMessage(tabId, enabled = false)
         }
+    }
+
+    private suspend fun promptAllowQCommandsConsent(messenger: MessagePublisher, tabID: String) {
+        messenger.sendAnswer(
+            tabId = tabID,
+            message = message("amazonqFeatureDev.chat_message.devFileInRepository"),
+            messageType = FeatureDevMessageType.Answer
+        )
+        messenger.sendAnswer(
+            tabId = tabID,
+            messageType = FeatureDevMessageType.SystemPrompt,
+            followUp = listOf(
+                FollowUp(
+                    pillText = message("amazonqFeatureDev.follow_up.accept_for_project"),
+                    type = FollowUpTypes.ACCEPT_AUTO_BUILD,
+                    status = FollowUpStatusType.Success
+                ),
+                FollowUp(
+                    pillText = message("amazonqFeatureDev.follow_up.decline_for_project"),
+                    type = FollowUpTypes.DENY_AUTO_BUILD,
+                    status = FollowUpStatusType.Error
+                )
+            )
+        )
     }
 
     private suspend fun retryRequests(tabId: String) {

plugins/core/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/FeatureDevSessionContext.kt
@@ -21,6 +21,7 @@ import software.aws.toolkits.core.utils.putNextEntry
 import software.aws.toolkits.jetbrains.core.coroutines.EDT
 import software.aws.toolkits.jetbrains.core.coroutines.getCoroutineBgContext
 import software.aws.toolkits.jetbrains.services.telemetry.ALLOWED_CODE_EXTENSIONS
+import software.aws.toolkits.jetbrains.utils.isDevFile
 import software.aws.toolkits.resources.AwsCoreBundle
 import software.aws.toolkits.telemetry.AmazonqTelemetry
 import java.io.File
@@ -47,7 +48,6 @@ class FeatureDevSessionContext(val project: Project, val maxProjectSizeBytes: Lo
         "\\.hg/?",
         "\\.rvm",
         "\\.git/?",
-        "\\.gitignore",
         "\\.project",
         "\\.gem",
         "/\\.idea/?",
@@ -71,7 +71,7 @@ class FeatureDevSessionContext(val project: Project, val maxProjectSizeBytes: Lo
     // projectRoot: is the directory where the project is located when selected to open a project.
     val projectRoot = project.guessProjectDir() ?: error("Cannot guess base directory for project ${project.name}")
 
-    // selectedSourceFolder": is the directory selected in replacement of the root, this happens when the project is too big to bundle for uploading.
+    // selectedSourceFolder: is the directory selected in replacement of the root, this happens when the project is too big to bundle for uploading.
     private var _selectedSourceFolder = projectRoot
     private var ignorePatternsWithGitIgnore = emptyList<Regex>()
     private val gitIgnoreFile = File(selectedSourceFolder.path, ".gitignore")
@@ -80,10 +80,18 @@ class FeatureDevSessionContext(val project: Project, val maxProjectSizeBytes: Lo
         ignorePatternsWithGitIgnore = (ignorePatterns + parseGitIgnore().map { Regex(it) }).toList()
     }
 
-    fun getProjectZip(): ZipCreationResult {
+    // This function checks for existence of `devfile.yaml` in customer's repository, currently only `devfile.yaml` is supported for this feature.
+    fun checkForDevFile(): Boolean {
+        val devFile = File(projectRoot.path, "/devfile.yaml")
+        return devFile.exists()
+    }
+
+    fun getWorkspaceRoot(): String = projectRoot.path
+
+    fun getProjectZip(isAutoBuildFeatureEnabled: Boolean?): ZipCreationResult {
         val zippedProject = runBlocking {
             withBackgroundProgress(project, AwsCoreBundle.message("amazonqFeatureDev.placeholder.generating_code")) {
-                zipFiles(selectedSourceFolder)
+                zipFiles(selectedSourceFolder, isAutoBuildFeatureEnabled)
             }
         }
         val checkSum256: String = Base64.getEncoder().encodeToString(DigestUtils.sha256(FileInputStream(zippedProject)))
@@ -117,7 +125,7 @@ class FeatureDevSessionContext(val project: Project, val maxProjectSizeBytes: Lo
         return deferredResults.any { it.await() }
     }
 
-    suspend fun zipFiles(projectRoot: VirtualFile): File = withContext(getCoroutineBgContext()) {
+    suspend fun zipFiles(projectRoot: VirtualFile, isAutoBuildFeatureEnabled: Boolean?): File = withContext(getCoroutineBgContext()) {
         val files = mutableListOf<VirtualFile>()
         val ignoredExtensionMap = mutableMapOf<String, Long>().withDefault { 0L }
         var totalSize: Long = 0
@@ -127,11 +135,18 @@ class FeatureDevSessionContext(val project: Project, val maxProjectSizeBytes: Lo
             object : VirtualFileVisitor<Unit>() {
                 override fun visitFile(file: VirtualFile): Boolean {
                     val isFileIgnoredByExtension = runBlocking { ignoreFileByExtension(file) }
+                    // if `isAutoBuildFeatureEnabled` is false, then filter devfile
+                    val isFilterDevFile = if (isAutoBuildFeatureEnabled == true) false else isDevFile(file)
                     if (isFileIgnoredByExtension) {
                         val extension = file.extension.orEmpty()
                         ignoredExtensionMap[extension] = (ignoredExtensionMap[extension] ?: 0) + 1
                         return false
                     }
+
+                    if (isFilterDevFile) {
+                        return false
+                    }
+
                     val isFileIgnoredByPattern = runBlocking { ignoreFile(file.name) }
                     if (isFileIgnoredByPattern) {
                         return false

plugins/amazonq/chat/jetbrains-community/tst/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/FeatureDevSessionContextTest.kt
@@ -46,7 +46,7 @@ class FeatureDevSessionContextTest : FeatureDevTestBase() {
     @Test
     fun testWithInvalidFile() {
         val txtFile = mock<VirtualFile>()
-        whenever(txtFile.extension).thenReturn("txt")
+        whenever(txtFile.extension).thenReturn("mp4")
         assertFalse(featureDevSessionContext.isFileExtensionAllowed(txtFile))
     }
 }

plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/messages/FeatureDevMessage.kt
@@ -247,6 +247,9 @@ enum class FollowUpTypes(
     PROVIDE_FEEDBACK_AND_REGENERATE_CODE("ProvideFeedbackAndRegenerateCode"),
     NEW_TASK("NewTask"),
     CLOSE_SESSION("CloseSession"),
+    ACCEPT_AUTO_BUILD("AcceptAutoBuild"),
+    DENY_AUTO_BUILD("DenyAutoBuild"),
+    GENERATE_DEV_FILE("GenerateDevFile"),
 }
 
 // Util classes

plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/session/PrepareCodeGenerationState.kt
@@ -13,6 +13,7 @@ import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.util.Cancellat
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.util.deleteUploadArtifact
 import software.aws.toolkits.jetbrains.services.amazonqFeatureDev.util.uploadArtifactToS3
 import software.aws.toolkits.jetbrains.services.cwc.controller.chat.telemetry.getStartUrl
+import software.aws.toolkits.jetbrains.settings.CodeWhispererSettings
 import software.aws.toolkits.resources.message
 import software.aws.toolkits.telemetry.AmazonqTelemetry
 import software.aws.toolkits.telemetry.AmazonqUploadIntent
@@ -47,7 +48,8 @@ class PrepareCodeGenerationState(
             messenger.sendAnswerPart(tabId = this.tabID, message = message("amazonqFeatureDev.chat_message.uploading_code"))
             messenger.sendUpdatePlaceholder(tabId = this.tabID, newPlaceholder = message("amazonqFeatureDev.chat_message.uploading_code"))
 
-            val repoZipResult = config.repoContext.getProjectZip()
+            val isAutoBuildFeatureEnabled = CodeWhispererSettings.getInstance().isAutoBuildFeatureEnabled(this.config.repoContext.getWorkspaceRoot())
+            val repoZipResult = config.repoContext.getProjectZip(isAutoBuildFeatureEnabled = isAutoBuildFeatureEnabled)
             val zipFileChecksum = repoZipResult.checksum
             zipFileLength = repoZipResult.contentLength
             val fileToUpload = repoZipResult.payload
@@ -94,7 +96,7 @@ class PrepareCodeGenerationState(
                 credentialStartUrl = getStartUrl(config.featureDevService.project)
             )
         }
-        // It is essential to interact with the next state outside of try-catch block for  the telemetry to capture events for the states separately
+        // It is essential to interact with the next state outside try-catch block for  the telemetry to capture events for the states separately
         return nextState.interact(action)
     }
 }

commit 232a273120a1843db9c3f4871732f00dd050b7a5

Update file generation logic and file upload functionality for dev-execution. (#5295)
#### Parameter Update for Testing:
- Updated function parameters to address testing issues introduced by changes made in PR #5077.
#### Enhanced Development Prompt:
- Modified GENERATE_DEV_FILE_PROMPT to support running install, build, and test commands independently.
#### Improved Build and Code File Upload Logic:
- Updated the logic to enable uploading build files alongside code files.

plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqDoc/session/PrepareDocGenerationState.kt
@@ -39,7 +39,7 @@ class PrepareDocGenerationState(
         var zipFileLength: Long? = null
         val nextState: SessionState
         try {
-            val repoZipResult = config.repoContext.getProjectZip()
+            val repoZipResult = config.repoContext.getProjectZip(false)
             val zipFileChecksum = repoZipResult.checksum
             zipFileLength = repoZipResult.contentLength
             val fileToUpload = repoZipResult.payload

plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/FeatureDevConstants.kt
@@ -8,7 +8,7 @@ const val FEATURE_EVALUATION_PRODUCT_NAME = "FeatureDev"
 const val FEATURE_NAME = "Amazon Q Developer Agent for software development"
 
 @Suppress("MaxLineLength")
-const val GENERATE_DEV_FILE_PROMPT = "generate a devfile in my repository. Note that you should only use devfile version 2.0.0 and the only supported command is test, so you should bundle all install, build and test commands in “test”. also you can use “public.ecr.aws/aws-mde/universal-image:latest” as universal image if you aren’t sure which image to use. here is an example for a node repository (but don't assume it's always a node project. look at the existing repository structure before generating the devfile): schemaVersion: 2.0.0 components: - name: dev container: image: public.ecr.aws/aws-mde/universal-image:latest commands: - id: test exec: component: dev commandLine: \"npm install && npm run build && npm run test\""
+const val GENERATE_DEV_FILE_PROMPT = "generate a devfile in my repository. Note that you should only use devfile version 2.0.0 and the only supported commands are install, build and test (are all optional). so you may have to bundle some commands together using '&&'. also you can use \"public.ecr.aws/aws-mde/universal-image:latest\" as universal image if you aren’t sure which image to use. here is an example for a node repository (but don't assume it's always a node project. look at the existing repository structure before generating the devfile): schemaVersion: 2.0.0 components: - name: dev container: image: public.ecr.aws/aws-mde/universal-image:latest commands: - id: install exec: component: dev commandLine: \"npm install\" - id: build exec: component: dev commandLine: \"npm run build\" - id: test exec: component: dev commandLine: \"npm run test\""
 
 // Max number of times a user can attempt to retry a code generation request if it fails
 const val CODE_GENERATION_RETRY_LIMIT = 3

plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/controller/FeatureDevController.kt
@@ -736,7 +736,7 @@ class FeatureDevController(
                 return
             }
 
-            session.preloader(message, messenger)
+            session.preloader(messenger)
             broadcastQEvent(QFeatureEvent.INVOCATION)
 
             when (session.sessionState.phase) {

plugins/core/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/FeatureDevSessionContext.kt
@@ -47,11 +47,11 @@ class RepoSizeLimitError(override val message: String) : RuntimeException(), Rep
 class FeatureDevSessionContext(val project: Project, val maxProjectSizeBytes: Long? = null) {
     // TODO: Need to correct this class location in the modules going further to support both amazonq and codescan.
 
+    private val requiredFilesForExecution = setOf("gradle/wrapper/gradle-wrapper.jar")
     private val additionalGitIgnoreRules = setOf(
         ".aws-sam",
         ".gem",
         ".git",
-        ".gitignore",
         ".gradle",
         ".hg",
         ".idea",
@@ -136,6 +136,9 @@ class FeatureDevSessionContext(val project: Project, val maxProjectSizeBytes: Lo
     suspend fun ignoreFile(file: VirtualFile): Boolean = ignoreFile(file.presentableUrl)
 
     suspend fun ignoreFile(path: String): Boolean {
+        if (requiredFilesForExecution.any { path.endsWith(it) }) {
+            return false
+        }
         // this method reads like something a JS dev would write and doesn't do what the author thinks
         val deferredResults = ignorePatternsWithGitIgnore.map { pattern ->
             withContext(coroutineContext) {

plugins/amazonq/chat/jetbrains-community/tst/software/aws/toolkits/jetbrains/services/amazonqFeatureDev/FeatureDevSessionContextTest.kt
@@ -55,10 +55,9 @@ class FeatureDevSessionContextTest : FeatureDevTestBase(HeavyJavaCodeInsightTest
 
     @Test
     fun testWithInvalidFile() {
-        val txtFile = mock<VirtualFile>()
-        whenever(txtFile.extension).thenReturn("txt")
-        whenever(txtFile.path).thenReturn("file.txt")
-        assertFalse(featureDevSessionContext.isFileExtensionAllowed(txtFile))
+        val mediaFile = mock<VirtualFile>()
+        whenever(mediaFile.extension).thenReturn("mp4")
+        assertFalse(featureDevSessionContext.isFileExtensionAllowed(mediaFile))
     }
 
     @Test
@@ -96,10 +95,11 @@ class FeatureDevSessionContextTest : FeatureDevTestBase(HeavyJavaCodeInsightTest
             "License.md",
             "node_modules/express",
             "build/outputs",
-            "dist/bundle.js"
+            "dist/bundle.js",
+            "gradle/wrapper/gradle-wrapper.jar",
         )
 
-        val zipResult = featureDevSessionContext.getProjectZip()
+        val zipResult = featureDevSessionContext.getProjectZip(false)
         val zipPath = zipResult.payload.path
 
         val zippedFiles = mutableSetOf<String>()
@@ -120,6 +120,8 @@ class FeatureDevSessionContextTest : FeatureDevTestBase(HeavyJavaCodeInsightTest
             "builder/GetTestBuilder.java",
             "settings.gradle",
             "build.gradle",
+            "gradle/wrapper/gradle-wrapper.jar",
+            ".gitignore",
         )
 
         assertTrue(zippedFiles == expectedFiles)

plugins/amazonq/codewhisperer/jetbrains-community/tst/software/aws/toolkits/jetbrains/services/codewhisperer/codescan/CodeWhispererProjectCodeScanTest.kt
@@ -34,6 +34,7 @@ class CodeWhispererProjectCodeScanTest : CodeWhispererCodeScanTestBase(PythonCod
     private lateinit var testYaml: VirtualFile
     private lateinit var helperPy: VirtualFile
     private lateinit var testTf: VirtualFile
+    private lateinit var gitIgnore: VirtualFile
 
     private lateinit var sessionConfigSpy: CodeScanSessionConfig
     private lateinit var sessionConfigSpy2: CodeScanSessionConfig
@@ -63,9 +64,9 @@ class CodeWhispererProjectCodeScanTest : CodeWhispererCodeScanTestBase(PythonCod
     fun `test createPayload`() {
         val payload = sessionConfigSpy.createPayload()
         assertNotNull(payload)
-        assertThat(payload.context.totalFiles).isEqualTo(10)
+        assertThat(payload.context.totalFiles).isEqualTo(11)
 
-        assertThat(payload.context.scannedFiles.size).isEqualTo(10)
+        assertThat(payload.context.scannedFiles.size).isEqualTo(11)
         assertThat(payload.context.scannedFiles).contains(testYaml, testTf, readMeMd, utilsJs, utilsCs, testJson, testCs, helperPy, helperCs, helpGo)
 
         assertThat(payload.context.srcPayloadSize).isEqualTo(totalSize)
@@ -80,12 +81,12 @@ class CodeWhispererProjectCodeScanTest : CodeWhispererCodeScanTestBase(PythonCod
             filesInZip += 1
         }
 
-        assertThat(filesInZip).isEqualTo(10)
+        assertThat(filesInZip).isEqualTo(11)
     }
 
     @Test
     fun `getProjectPayloadMetadata()`() {
-        getProjectPayloadMetadata(sessionConfigSpy, 10, totalSize, this.totalLines, CodewhispererLanguage.Csharp)
+        getProjectPayloadMetadata(sessionConfigSpy, 11, totalSize, this.totalLines, CodewhispererLanguage.Csharp)
     }
 
     @Test
@@ -105,7 +106,7 @@ class CodeWhispererProjectCodeScanTest : CodeWhispererCodeScanTestBase(PythonCod
 
     @Test
     fun `e2e happy path integration test`() = runTest {
-        assertE2ERunsSuccessfully(sessionConfigSpy, project, totalLines, 10, totalSize, 1)
+        assertE2ERunsSuccessfully(sessionConfigSpy, project, totalLines, 11, totalSize, 1)
     }
 
     private fun setupCsharpProject() {
@@ -371,7 +372,10 @@ class CodeWhispererProjectCodeScanTest : CodeWhispererCodeScanTestBase(PythonCod
 
         // Adding gitignore file and gitignore file member for testing.
         // The tests include the markdown file but not these two files.
-        projectRule.fixture.addFileToProject("/.gitignore", "node_modules\n.idea\n.vscode\n.DS_Store").virtualFile
+        gitIgnore = projectRule.fixture.addFileToProject("/.gitignore", "node_modules\n.idea\n.vscode\n.DS_Store").virtualFile
+        totalSize += gitIgnore.length
+        totalLines += gitIgnore.toNioPath().toFile().readLines().size
+
         projectRule.fixture.addFileToProject("/.idea/ref", "ref: refs/heads/main")
     }
 }