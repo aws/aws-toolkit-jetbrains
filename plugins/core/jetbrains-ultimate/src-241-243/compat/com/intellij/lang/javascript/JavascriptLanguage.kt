03185288fe4f9d6d146320b42c972e29b64dcac3
Richard Li <richali@amazon.com>
2025-02-06 08:47:02 -0800

    inline

--- plugins/core/jetbrains-ultimate/src-241-243/compat/com/intellij/lang/javascript/JavascriptLanguage.kt
+++ plugins/core/jetbrains-ultimate/src-241-243/compat/com/intellij/lang/javascript/JavascriptLanguage.kt
@@ -3,4 +3,6 @@
 
 package compat.com.intellij.lang.javascript
 
-val JavascriptLanguage = com.intellij.lang.javascript.JavascriptLanguage.INSTANCE
+// probably not necessary, but inline to avoid loading this through core classpath
+inline val JavascriptLanguage
+    get() = com.intellij.lang.javascript.JavascriptLanguage.INSTANCE
