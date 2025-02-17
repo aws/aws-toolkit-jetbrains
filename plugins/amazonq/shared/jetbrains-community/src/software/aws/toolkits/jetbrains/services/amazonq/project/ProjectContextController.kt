f085ce895963ad82dfe68a93779e159f3f0abbe6
Will Lo <xshaohua@amazon.com>
2025-02-17 09:29:43 -0800

    Revert "add safe checks of workspace process status before making calls"
    This reverts commit cd4a20ea63805758312332f550399874ce335f13.

--- plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/project/ProjectContextController.kt
+++ plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/project/ProjectContextController.kt
@@ -52,10 +52,6 @@ class ProjectContextController(private val project: Project, private val cs: Cor
     fun getProjectContextIndexComplete() = projectContextProvider.isIndexComplete.get()
 
     suspend fun queryChat(prompt: String, timeout: Long?): List<RelevantDocument> {
-        if (!getProjectContextIndexComplete()) {
-            logger.warn { "attempt to query project context while index for ${project.name} isn't ready" }
-            return emptyList()
-        }
         try {
             return projectContextProvider.query(prompt, timeout)
         } catch (e: Exception) {
@@ -64,13 +60,8 @@ class ProjectContextController(private val project: Project, private val cs: Cor
         }
     }
 
-    suspend fun queryInline(query: String, filePath: String): List<InlineBm25Chunk> {
-        if (!getProjectContextIndexComplete()) {
-            logger.warn { "attempt to query project context while index for ${project.name} isn't ready" }
-            return emptyList()
-        }
-
-        return try {
+    suspend fun queryInline(query: String, filePath: String): List<InlineBm25Chunk> =
+        try {
             projectContextProvider.queryInline(query, filePath, InlineContextTarget.CODEMAP)
         } catch (e: Exception) {
             var logStr = "error while querying inline for project context $e.message"
@@ -80,14 +71,9 @@ class ProjectContextController(private val project: Project, private val cs: Cor
             logger.warn { logStr }
             emptyList()
         }
-    }
 
     @RequiresBackgroundThread
     fun updateIndex(filePaths: List<String>, mode: IndexUpdateMode) {
-        if (!getProjectContextIndexComplete()) {
-            return
-        }
-
         try {
             return projectContextProvider.updateIndex(filePaths, mode)
         } catch (e: Exception) {
