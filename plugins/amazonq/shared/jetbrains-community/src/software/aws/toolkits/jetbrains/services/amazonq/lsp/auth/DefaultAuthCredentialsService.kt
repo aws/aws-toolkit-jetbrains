a74f884991aaadb6a494655fd28863bfd34313a3
Sam Stewart <samgst@amazon.com>
2025-02-28 11:04:56 -0800

    implement token change listener (#5423)

--- plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/lsp/auth/DefaultAuthCredentialsService.kt
+++ plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/lsp/auth/DefaultAuthCredentialsService.kt
@@ -3,8 +3,14 @@
 
 package software.aws.toolkits.jetbrains.services.amazonq.lsp.auth
 
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.project.Project
 import org.eclipse.lsp4j.jsonrpc.messages.ResponseMessage
+import software.aws.toolkits.core.TokenConnectionSettings
+import software.aws.toolkits.jetbrains.core.credentials.ToolkitConnectionManager
+import software.aws.toolkits.jetbrains.core.credentials.pinning.QConnection
+import software.aws.toolkits.jetbrains.core.credentials.sso.bearer.BearerTokenProvider
+import software.aws.toolkits.jetbrains.core.credentials.sso.bearer.BearerTokenProviderListener
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.AmazonQLspService
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.encryption.JwtEncryptionManager
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.credentials.BearerCredentials
@@ -15,7 +21,31 @@ import java.util.concurrent.CompletableFuture
 class DefaultAuthCredentialsService(
     private val project: Project,
     private val encryptionManager: JwtEncryptionManager,
-) : AuthCredentialsService {
+    serverInstance: Disposable,
+) : AuthCredentialsService,
+    BearerTokenProviderListener {
+    init {
+        project.messageBus.connect(serverInstance).subscribe(BearerTokenProviderListener.TOPIC, this)
+    }
+
+    override fun onChange(providerId: String, newScopes: List<String>?) {
+        val connection = ToolkitConnectionManager.getInstance(project)
+            .activeConnectionForFeature(QConnection.getInstance())
+            ?: return
+
+        val provider = (connection.getConnectionSettings() as? TokenConnectionSettings)
+            ?.tokenProvider
+            ?.delegate as? BearerTokenProvider
+            ?: return
+
+        provider.currentToken()?.accessToken?.let { token ->
+            updateTokenCredentials(token, false)
+        }
+    }
+
+    override fun invalidate(providerId: String) {
+        deleteTokenCredentials()
+    }
 
     override fun updateTokenCredentials(accessToken: String, encrypted: Boolean): CompletableFuture<ResponseMessage> {
         val token = if (encrypted) {
