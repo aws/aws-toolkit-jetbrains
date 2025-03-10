38920dc6abbb00f8c4267e708e382ad1194746af
Ashish Reddy Podduturi <ashishrp@amazon.com>
2025-03-09 23:54:34 -0700

    Correction for nit

--- ui-tests-starter/tstData/qTestGenerationTestProject/testModule1/ErrorPath.java
+++ ui-tests-starter/tstData/qTestGenerationTestProject/testModule1/ErrorPath.java
@@ -1,7 +1,7 @@
 // Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-public class HappyPath {
+public class ErrorPath {
 
     // test method for Q Test Generation.
     private static double multiply(double num1, double num2) {
