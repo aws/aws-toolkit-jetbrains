aws/aws-toolkit-jetbrains

commit 330f565fd7c1e6b4f34de624db48187d206895d9

fix(amazonq): confusing message during loading profiles (#5552)

plugins/core/webview/src/q-ui/components/profileSelection.vue
@@ -3,67 +3,73 @@
 <template>
     <div @keydown.enter="handleContinueClick">
         <div class="font-amazon">
-            <!-- Title & Subtitle -->
-            <div id="profile-page" class="profile-header">
-                <h2 class="title bottom-small-gap">Choose a Q Developer profile</h2>
-                <p class="profile-subtitle">
-                    Your administrator has given you access to Q from multiple profiles.
-                    Choose the profile that meets your current working needs. You can change your profile at any time.
-                </p>
-            </div>
-            <!-- Profile List -->
-            <div class="profile-list">
-                <div
-                    v-for="(profile, index) in availableProfiles"
-                    :key="index"
-                    class="profile-item bottom-small-gap"
-                    :class="{ selected: selectedProfile?.arn === profile.arn }"
-                    @click="toggleItemSelection(profile)"
-                    tabindex="0"
-                >
-                    <div class="text">
-                        <div class="profile-name">{{ profile.profileName }} - <span class="profile-region">{{ profile.region }}</span></div>
-                        <div class="profile-id">Account: {{ profile.accountId }}</div>
+            <template v-if="isWaitingResponse">
+                <div class="title bottom-small-gap">Fetching Q Developer profiles...this may take a minute.</div>
+            </template>
+
+            <template v-else>
+                <!-- Title & Subtitle -->
+                <div id="profile-page" class="profile-header">
+                    <h2 class="title bottom-small-gap">Choose a Q Developer profile</h2>
+                    <p class="profile-subtitle">
+                        Your administrator has given you access to Q from multiple profiles.
+                        Choose the profile that meets your current working needs. You can change your profile at any time.
+                    </p>
+                </div>
+                <!-- Profile List -->
+                <div class="profile-list">
+                    <div
+                        v-for="(profile, index) in availableProfiles"
+                        :key="index"
+                        class="profile-item bottom-small-gap"
+                        :class="{ selected: selectedProfile?.arn === profile.arn }"
+                        @click="toggleItemSelection(profile)"
+                        tabindex="0"
+                    >
+                        <div class="text">
+                            <div class="profile-name">{{ profile.profileName }} - <span class="profile-region">{{ profile.region }}</span></div>
+                            <div class="profile-id">Account: {{ profile.accountId }}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div v-if="errorMessage" style="color: white; margin-bottom: 10px;">
-                {{ errorMessage }}
-            </div>
-            <div v-if="errorMessage" class="button-row">
-                <button
-                    class="login-flow-button continue-button font-amazon"
-                    :disabled="isRefreshing"
-                    @click="handleRetryClick"
-                >
-                    {{ isRefreshing ? 'Refreshing...' : 'Try Again' }}
-                </button>
-                <button
-                    class="login-flow-button continue-button font-amazon"
-                    @click="handleSignoutClick()"
-                >
-                    Sign Out
-                </button>
-            </div>
-            <!-- Continue Button -->
-            <div v-else>
-                <button
-                    class="login-flow-button continue-button font-amazon"
-                    :disabled="selectedProfile === null"
-                    v-on:click="handleContinueClick()"
-                    tabindex="-1"
-                >
-                    Continue
-                </button>
-            </div>
+                <div v-if="errorMessage" style="color: white; margin-bottom: 10px;">
+                    {{ errorMessage }}
+                </div>
+                <div v-if="errorMessage" class="button-row">
+                    <button
+                        class="login-flow-button continue-button font-amazon"
+                        :disabled="isRefreshing"
+                        @click="handleRetryClick"
+                    >
+                        {{ isRefreshing ? 'Refreshing...' : 'Try Again' }}
+                    </button>
+                    <button
+                        class="login-flow-button continue-button font-amazon"
+                        @click="handleSignoutClick()"
+                    >
+                        Sign Out
+                    </button>
+                </div>
+                <!-- Continue Button -->
+                <div v-else>
+                    <button
+                        class="login-flow-button continue-button font-amazon"
+                        :disabled="selectedProfile === null"
+                        v-on:click="handleContinueClick()"
+                        tabindex="-1"
+                    >
+                        Continue
+                    </button>
+                </div>
+            </template>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, watch } from 'vue'
-import { Profile, GENERIC_PROFILE_LOAD_ERROR } from '../../model'
+import { defineComponent } from 'vue'
+import { Profile, GENERIC_PROFILE_LOAD_ERROR, ListProfilePendingResult, ListProfileSuccessResult, ListProfileFailureResult } from '../../model'
 
 export default defineComponent({
     name: 'ProfileSelection',
@@ -75,23 +81,31 @@ export default defineComponent({
             selectedProfile: undefined as Profile | undefined,
             availableProfiles: [] as Profile[],
             errorMessage: undefined as string | undefined,
-            isRefreshing: false as boolean
+            isRefreshing: false as boolean,
+        }
+    },
+    computed: {
+        isWaitingResponse() {
+            const profileResult = this.$store.state.listProfilesResult
+            if (profileResult instanceof ListProfilePendingResult) {
+                return true
+            }
+
+            if (profileResult instanceof ListProfileSuccessResult) {
+                this.availableProfiles = profileResult.profiles
+            } else if (profileResult instanceof ListProfileFailureResult) {
+                this.errorMessage = GENERIC_PROFILE_LOAD_ERROR
+            } else {
+                // should not be this path
+                this.errorMessage = "Unexpected error happenede while loading Q Webview page"
+            }
         }
     },
     mounted() {
-      console.debug("Vuex raw profiles:", this.$store.state.profiles);
-      this.availableProfiles = this.$store.state.profiles;
-      this.errorMessage = this.$store.state.errorMessage ? GENERIC_PROFILE_LOAD_ERROR : undefined;
-      this.isRefreshing = false;
-      watch(() => this.$store.state.profiles, (newVal, oldVal) => {
-          this.availableProfiles = newVal
-          this.isRefreshing = false
-      });
-      watch(() => this.$store.state.errorMessage, (newVal) => {
-          this.errorMessage = newVal ? GENERIC_PROFILE_LOAD_ERROR : undefined;
-      })
+        window.ideApi.postMessage({command: 'listProfiles'})
     },
-  methods: {
+
+    methods: {
         toggleItemSelection(profile: Profile) {
             this.selectedProfile = profile;
         },
@@ -144,9 +158,11 @@ export default defineComponent({
     cursor: pointer;
     transition: background 0.2s ease-in-out;
 }
+
 .button-row :deep(.login-flow-button) {
     margin-bottom: 10px;
 }
+
 .button-row {
     display: flex;
     flex-direction: column;
@@ -154,6 +170,7 @@ export default defineComponent({
     gap: 10px;
     margin-top: 20px;
 }
+
 .selected {
     user-select: none;
 }
@@ -163,23 +180,28 @@ export default defineComponent({
     flex-direction: column;
     font-size: 15px;
 }
+
 .profile-name {
     font-weight: bold;
     margin-bottom: 2px;
     color: white;
 }
+
 .profile-region {
     font-style: italic;
     color: #bbbbbb;
 }
+
 .profile-description {
     font-size: 12px;
     color: #bbbbbb;
 }
+
 body.jb-dark {
     .profile-item {
         border: 1px solid white;
     }
+
     .selected {
         border: 1px solid #29a7ff;
     }
@@ -189,6 +211,7 @@ body.jb-light {
     .profile-item {
         border: 1px solid black;
     }
+
     .selected {
         border: 1px solid #3574f0;
     }

plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/QLoginWebview.kt
@@ -220,6 +220,10 @@ class QWebviewBrowser(val project: Project, private val parentDisposable: Dispos
                 )
             }
 
+            is BrowserMessage.ListProfiles -> {
+                handleListProfilesMessage()
+            }
+
             is BrowserMessage.PublishWebviewTelemetry -> {
                 publishTelemetry(message)
             }
@@ -262,60 +266,41 @@ class QWebviewBrowser(val project: Project, private val parentDisposable: Dispos
             writeValueAsString(it)
         }
 
-        // TODO: pass "REAUTH" if connection expires
-        // Perform the potentially blocking AWS call outside the EDT to fetch available region profiles.
-        ApplicationManager.getApplication().executeOnPooledThread {
-            val stage = if (isQExpired(project)) {
-                "REAUTH"
-            } else if (isQConnected(project) && QRegionProfileManager.getInstance().isPendingProfileSelection(project)) {
-                "PROFILE_SELECT"
-            } else {
-                "START"
-            }
+        val stage = if (isQExpired(project)) {
+            "REAUTH"
+        } else if (isQConnected(project) && QRegionProfileManager.getInstance().isPendingProfileSelection(project)) {
+            "PROFILE_SELECT"
+        } else {
+            "START"
+        }
 
-            var errorMessage: String? = null
-            var profiles: List<QRegionProfile> = emptyList()
-
-            if (stage == "PROFILE_SELECT") {
-                try {
-                    profiles = QRegionProfileManager.getInstance().listRegionProfiles(project).orEmpty()
-                    if (profiles.size == 1) {
-                        LOG.debug { "User only have access to 1 Q profile, auto-selecting profile ${profiles.first().profileName} for ${project.name}" }
-                        QRegionProfileManager.getInstance().switchProfile(project, profiles.first(), QProfileSwitchIntent.Update)
+        when (stage) {
+            "PROFILE_SELECT" -> {
+                val jsonData = """
+                    {
+                        stage: '$stage',
+                        status: 'pending'
                     }
-                } catch (e: Exception) {
-                    errorMessage = e.message
-                    LOG.warn { "Failed to call listRegionProfiles API" }
-                    val qConn = ToolkitConnectionManager.getInstance(project).activeConnectionForFeature(QConnection.getInstance())
-                    Telemetry.amazonq.didSelectProfile.use { span ->
-                        span.source(QProfileSwitchIntent.Auth.value)
-                            .amazonQProfileRegion(QRegionProfileManager.getInstance().activeProfile(project)?.region ?: "not-set")
-                            .ssoRegion((qConn as? AwsBearerTokenConnection)?.region)
-                            .credentialStartUrl((qConn as? AwsBearerTokenConnection)?.startUrl)
-                            .result(MetricResult.Failed)
-                            .reason(e.message)
+                """.trimIndent()
+                executeJS("window.ideClient.prepareUi($jsonData)")
+            }
+
+            else -> {
+                val jsonData = """
+                    {
+                        stage: '$stage',
+                        regions: $regions,
+                        idcInfo: {
+                            profileName: '${lastLoginIdcInfo.profileName}',
+                            startUrl: '${lastLoginIdcInfo.startUrl}',
+                            region: '${lastLoginIdcInfo.region}'
+                        },
+                        cancellable: ${state.browserCancellable},
+                        feature: '${state.feature}',
+                        existConnections: ${writeValueAsString(selectionSettings.values.map { it.currentSelection }.toList())},
                     }
-                }
-            }
+                """.trimIndent()
 
-            val jsonData = """
-            {
-                stage: '$stage',
-                regions: $regions,
-                idcInfo: {
-                    profileName: '${lastLoginIdcInfo.profileName}',
-                    startUrl: '${lastLoginIdcInfo.startUrl}',
-                    region: '${lastLoginIdcInfo.region}'
-                },
-                cancellable: ${state.browserCancellable},
-                feature: '${state.feature}',
-                existConnections: ${writeValueAsString(selectionSettings.values.map { it.currentSelection }.toList())},
-                profiles: ${writeValueAsString(profiles)},
-                errorMessage: ${errorMessage?.let { "\"$it\"" } ?: "null"}
-            }
-            """.trimIndent()
-
-            runInEdt {
                 executeJS("window.ideClient.prepareUi($jsonData)")
             }
         }
@@ -328,6 +313,52 @@ class QWebviewBrowser(val project: Project, private val parentDisposable: Dispos
 
     override fun loadWebView(query: JBCefJSQuery) {
         jcefBrowser.loadHTML(getWebviewHTML(webScriptUri, query))
+    }
+
+    private fun handleListProfilesMessage() {
+        ApplicationManager.getApplication().executeOnPooledThread {
+            var errorMessage = ""
+            val profiles = try {
+                QRegionProfileManager.getInstance().listRegionProfiles(project)
+            } catch (e: Exception) {
+                e.message?.let {
+                    errorMessage = it
+                }
+                LOG.warn { "Failed to call listRegionProfiles API: $errorMessage" }
+                val qConn = ToolkitConnectionManager.getInstance(project).activeConnectionForFeature(QConnection.getInstance())
+                Telemetry.amazonq.didSelectProfile.use { span ->
+                    span.source(QProfileSwitchIntent.Auth.value)
+                        .amazonQProfileRegion(QRegionProfileManager.getInstance().activeProfile(project)?.region ?: "not-set")
+                        .ssoRegion((qConn as? AwsBearerTokenConnection)?.region)
+                        .credentialStartUrl((qConn as? AwsBearerTokenConnection)?.startUrl)
+                        .result(MetricResult.Failed)
+                        .reason(e.message)
+                }
+
+                null
+            }
+
+            // auto-select the profile if users only have 1 and don't show the UI
+            if (profiles?.size == 1) {
+                LOG.debug { "User only have access to 1 Q profile, auto-selecting profile ${profiles.first().profileName} for ${project.name}" }
+                QRegionProfileManager.getInstance().switchProfile(project, profiles.first(), QProfileSwitchIntent.Update)
+                return@executeOnPooledThread
+            }
+
+            // required EDT as this entire block is executed on thread pool
+            runInEdt {
+                val jsonData = """
+                        {
+                            stage: 'PROFILE_SELECT',
+                            status: '${if (profiles != null) "succeeded" else "failed"}',
+                            profiles: ${writeValueAsString(profiles ?: "")},
+                            errorMessage: '$errorMessage'
+                        }
+                """.trimIndent()
+
+                executeJS("window.ideClient.prepareUi($jsonData)")
+            }
+        }
     }
 
     companion object {

plugins/core/jetbrains-community/src/software/aws/toolkits/jetbrains/core/webview/BrowserMessage.kt
@@ -27,7 +27,8 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo
     JsonSubTypes.Type(value = BrowserMessage.Reauth::class, name = "reauth"),
     JsonSubTypes.Type(value = BrowserMessage.SendUiClickTelemetry::class, name = "sendUiClickTelemetry"),
     JsonSubTypes.Type(value = BrowserMessage.SwitchProfile::class, name = "switchProfile"),
-    JsonSubTypes.Type(value = BrowserMessage.PublishWebviewTelemetry::class, name = "webviewTelemetry")
+    JsonSubTypes.Type(value = BrowserMessage.PublishWebviewTelemetry::class, name = "webviewTelemetry"),
+    JsonSubTypes.Type(value = BrowserMessage.ListProfiles::class, name = "listProfiles")
 )
 sealed interface BrowserMessage {
 
@@ -65,6 +66,8 @@ sealed interface BrowserMessage {
         val region: String,
         val arn: String,
     ) : BrowserMessage
+
+    object ListProfiles : BrowserMessage
 
     data class SendUiClickTelemetry(val signInOptionClicked: String?) : BrowserMessage
 

plugins/core/webview/src/ideClient.ts
@@ -2,7 +2,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import {Store} from "vuex";
-import {IdcInfo, Region, Stage, State, BrowserSetupData, AwsBearerTokenConnection, Profile} from "./model";
+import {
+    IdcInfo,
+    State,
+    AuthSetupMessageFromIde,
+    ListProfileResult,
+    ListProfileSuccessResult,
+    ListProfileFailureResult, ListProfilePendingResult, ListProfilesMessageFromIde
+} from "./model";
 import {WebviewTelemetry} from './webviewTelemetry'
 
 export class IdeClient {
@@ -10,20 +17,47 @@ export class IdeClient {
 
     // TODO: design and improve the API here
 
-    prepareUi(state: BrowserSetupData) {
+    prepareUi(state: AuthSetupMessageFromIde | ListProfilesMessageFromIde) {
         WebviewTelemetry.instance.reset()
         console.log('browser is preparing UI with state ', state)
-        this.store.commit('setStage', state.stage)
         // hack as window.onerror don't have access to vuex store
         void ((window as any).uiState = state.stage)
         WebviewTelemetry.instance.willShowPage(state.stage)
-        this.store.commit('setSsoRegions', state.regions)
-        this.updateLastLoginIdcInfo(state.idcInfo)
-        this.store.commit("setCancellable", state.cancellable)
-        this.store.commit("setFeature", state.feature)
-        this.store.commit('setProfiles', state.profiles);
-        this.store.commit("setErrorMessage", state.errorMessage)
-        const existConnections = state.existConnections.map(it => {
+
+        this.store.commit('setStage', state.stage)
+
+        switch (state.stage) {
+            case "PROFILE_SELECT":
+                this.handleProfileSelectMessage(state as ListProfilesMessageFromIde)
+                break
+
+            default:
+                this.handleAuthSetupMessage(state as AuthSetupMessageFromIde)
+        }
+    }
+
+    private handleProfileSelectMessage(msg: ListProfilesMessageFromIde) {
+        let result: ListProfileResult | undefined
+        switch (msg.status) {
+            case 'succeeded':
+                result = new ListProfileSuccessResult(msg.profiles)
+                break
+            case 'failed':
+                result = new ListProfileFailureResult(msg.errorMessage)
+                break
+            case 'pending':
+                result = new ListProfilePendingResult()
+                break
+        }
+        this.store.commit('setProfilesResult', result)
+    }
+
+    private handleAuthSetupMessage(msg: AuthSetupMessageFromIde) {
+        this.store.commit('setSsoRegions', msg.regions)
+        this.updateLastLoginIdcInfo(msg.idcInfo)
+        this.store.commit("setCancellable", msg.cancellable)
+        this.store.commit("setFeature", msg.feature)
+        const existConnections = msg.existConnections.map(it => {
             return {
                 sessionName: it.sessionName,
                 startUrl: it.startUrl,
@@ -35,13 +69,6 @@ export class IdeClient {
 
         this.store.commit("setExistingConnections", existConnections)
         this.updateAuthorization(undefined)
-    }
-
-    handleProfiles(profilesData: { profiles: Profile[] }) {
-        this.store.commit('setStage', 'PROFILE_SELECT')
-        console.debug("received profile data")
-        const availableProfiles: Profile[] = profilesData.profiles;
-        this.store.commit('setProfiles', availableProfiles);
     }
 
     updateAuthorization(code: string | undefined) {

plugins/core/webview/src/model.ts
@@ -1,16 +1,22 @@
 // Copyright 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export type BrowserSetupData = {
+export type AuthSetupMessageFromIde = {
     stage: Stage,
     regions: Region[],
     idcInfo: IdcInfo,
     cancellable: boolean,
     feature: string,
     existConnections: AwsBearerTokenConnection[],
+}
+
+export type ListProfilesMessageFromIde = {
+    stage: Stage,
+    status: 'succeeded' | 'failed' | 'pending',
     profiles: Profile[],
     errorMessage: string
 }
+
 
 // plugin interface [AwsBearerTokenConnection]
 export interface AwsBearerTokenConnection {
@@ -20,6 +26,7 @@ export interface AwsBearerTokenConnection {
     scopes: string[],
     id: string
 }
+
 export const SONO_URL = "https://view.awsapps.com/start"
 
 export type Stage =
@@ -54,9 +61,27 @@ export interface State {
     feature: Feature,
     cancellable: boolean,
     existingConnections: AwsBearerTokenConnection[],
-    profiles: Profile[],
-    selectedProfile: Profile | undefined,
-    errorMessage: string | undefined
+    listProfilesResult: ListProfileResult | undefined,
+    selectedProfile: Profile | undefined
+}
+
+export interface ListProfileResult {
+    status: 'succeeded' | 'failed' | 'pending'
+}
+
+export class ListProfileSuccessResult implements ListProfileResult {
+    status: 'succeeded' = 'succeeded'
+    constructor(readonly profiles: Profile[]) {}
+}
+
+export class ListProfileFailureResult implements ListProfileResult {
+    status: 'failed' = 'failed'
+    constructor(readonly errorMessage: string) {}
+}
+
+export class ListProfilePendingResult implements ListProfileResult {
+    status: 'pending' = 'pending'
+    constructor() {}
 }
 
 export enum LoginIdentifier {

plugins/core/webview/src/q-ui/index.ts
@@ -5,7 +5,7 @@
 import { createApp } from 'vue'
 import {createStore, Store} from 'vuex'
 import HelloWorld from './components/root.vue'
-import {AwsBearerTokenConnection, Feature, IdcInfo, Profile, Region, Stage, State} from "../model";
+import {AwsBearerTokenConnection, Feature, IdcInfo, ListProfileResult, Profile, Region, Stage, State} from "../model";
 import {IdeClient} from "../ideClient";
 import './assets/common.scss'
 
@@ -22,9 +22,8 @@ const store = createStore<State>({
         feature: 'Q',
         cancellable: false,
         existingConnections: [] as AwsBearerTokenConnection[],
-        profiles: [] as Profile[],
-        selectedProfile: undefined,
-        errorMessage: undefined
+        listProfilesResult: undefined as ListProfileResult | undefined,
+        selectedProfile: undefined
     },
     getters: {},
     mutations: {
@@ -51,14 +50,11 @@ const store = createStore<State>({
         setExistingConnections(state: State, connections: AwsBearerTokenConnection[]) {
             state.existingConnections = connections
         },
-        setProfiles(state, profiles) {
-            state.profiles = Array.isArray(profiles) ? profiles : profiles.profiles || [];
+        setProfilesResult(state: State, profileResult: ListProfileResult) {
+            state.listProfilesResult = profileResult
         },
         setSelectedProfile(state, profile: Profile) {
             state.selectedProfile = profile
-        },
-        setErrorMessage(state, errorMessage: string){
-            state.errorMessage = errorMessage
         },
         reset(state: State) {
             state.stage = 'START'
@@ -68,9 +64,8 @@ const store = createStore<State>({
                 startUrl: '',
                 region: ''
             }
-            state.profiles = []
             state.selectedProfile = undefined
-            state.errorMessage = ''
+            state.listProfilesResult = undefined
         }
     },
     actions: {},

plugins/core/webview/src/q-ui/toolkit.ts
@@ -23,9 +23,8 @@ const store = createStore<State>({
         feature: 'awsExplorer',
         cancellable: false,
         existingConnections: [] as AwsBearerTokenConnection[],
-        profiles : [],
+        listProfilesResult : undefined,
         selectedProfile: undefined,
-        errorMessage: undefined
     },
     getters: {},
     mutations: {

plugins/toolkit/jetbrains-core/src/software/aws/toolkits/jetbrains/core/explorer/webview/ToolkitLoginWebview.kt
@@ -246,6 +246,8 @@ class ToolkitWebviewBrowser(val project: Project, private val parentDisposable: 
 
             is BrowserMessage.SwitchProfile -> {}
 
+            is BrowserMessage.ListProfiles -> {}
+
             is BrowserMessage.PublishWebviewTelemetry -> {
                 publishTelemetry(message)
             }

commit a797ea44a1b477db0cfcb239b0d1854f66400d68

fix retry flow broke by #5552

plugins/core/webview/src/q-ui/components/profileSelection.vue
@@ -86,6 +86,7 @@ export default defineComponent({
     },
     computed: {
         isWaitingResponse() {
+            this.errorMessage = ''
             const profileResult = this.$store.state.listProfilesResult
             if (profileResult instanceof ListProfilePendingResult) {
                 return true
@@ -95,6 +96,7 @@ export default defineComponent({
                 this.availableProfiles = profileResult.profiles
             } else if (profileResult instanceof ListProfileFailureResult) {
                 this.errorMessage = GENERIC_PROFILE_LOAD_ERROR
+                this.isRefreshing = false
             } else {
                 // should not be this path
                 this.errorMessage = "Unexpected error happenede while loading Q Webview page"
@@ -124,7 +126,7 @@ export default defineComponent({
         },
         handleRetryClick() {
             this.isRefreshing = true
-            window.ideApi.postMessage({command: 'prepareUi'})
+            window.ideApi.postMessage({command: 'listProfiles'})
         },
         handleSignoutClick() {
             window.ideApi.postMessage({command: 'signout'})