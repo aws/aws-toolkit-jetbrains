d215249b12dce2ead31c85545f118885f8b5a282
Sam Stewart <samgst@amazon.com>
2025-04-22 11:47:05 -0700

    feat(amazonq): client emits linkClick events when user clicks links in chat (#5608)
    * Set up Flare chat connection
    
    * Partial chat results
    
    * feedback
    
    * detekt
    
    * syntax error
    
    * detekt
    
    * detekt
    
    * Add quick actions to chat
    
    * adding commands from initialize result
    
    * added trace logging
    
    * link click events
    
    * feedback
    
    * rename
    
    * missing license
    
    ---------
    
    Co-authored-by: Manodnya Bhoite <manodnyb@amazon.com>
    Co-authored-by: manodnyab <66754471+manodnyab@users.noreply.github.com>

--- plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/webview/BrowserConnector.kt
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/webview/BrowserConnector.kt
@@ -17,26 +17,36 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.merge
 import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.future.await
 import kotlinx.coroutines.launch
 import org.cef.browser.CefBrowser
 import org.eclipse.lsp4j.Position
 import org.eclipse.lsp4j.Range
 import software.aws.toolkits.jetbrains.services.amazonq.apps.AppConnection
 import software.aws.toolkits.jetbrains.services.amazonq.commands.MessageSerializer
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.AmazonQLanguageServer
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.AmazonQLspService
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.encryption.JwtEncryptionManager
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.flareChat.ChatCommunicationManager
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.flareChat.getTextDocumentIdentifier
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.CHAT_INFO_LINK_CLICK
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.CHAT_LINK_CLICK
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.CHAT_QUICK_ACTION
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.CHAT_SOURCE_LINK_CLICK
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.ChatNotification
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.ChatParams
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.ChatPrompt
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.CursorState
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.EncryptedChatParams
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.EncryptedQuickActionChatParams
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.InfoLinkClickNotification
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.InfoLinkClickParams
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.LinkClickNotification
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.LinkClickParams
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.QuickChatActionRequest
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.SEND_CHAT_COMMAND_PROMPT
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.SendChatPromptRequest
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.SourceLinkClickNotification
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.SourceLinkClickParams
 import software.aws.toolkits.jetbrains.services.amazonq.util.command
 import software.aws.toolkits.jetbrains.services.amazonq.util.tabType
 import software.aws.toolkits.jetbrains.services.amazonq.webview.theme.AmazonQTheme
@@ -205,6 +215,23 @@ class BrowserConnector(
 
                 showResult(result, partialResultToken, tabId, encryptionManager, browser)
             }
+            CHAT_LINK_CLICK -> {
+                handleChatNotification<LinkClickNotification, LinkClickParams>(node) { server, params ->
+                    server.linkClick(params)
+                }
+            }
+
+            CHAT_INFO_LINK_CLICK -> {
+                handleChatNotification<InfoLinkClickNotification, InfoLinkClickParams>(node) { server, params ->
+                    server.infoLinkClick(params)
+                }
+            }
+
+            CHAT_SOURCE_LINK_CLICK -> {
+                handleChatNotification<SourceLinkClickNotification, SourceLinkClickParams>(node) { server, params ->
+                    server.sourceLinkClick(params)
+                }
+            }
         }
     }
 
@@ -225,5 +252,15 @@ class BrowserConnector(
             )
             browser.postChat(messageToChat)
         }
+    }
+
+    private inline fun <reified T, R> handleChatNotification(
+        node: JsonNode,
+        crossinline serverAction: (server: AmazonQLanguageServer, params: R) -> CompletableFuture<*>,
+    ): CompletableFuture<*> where T : ChatNotification<R> {
+        val requestFromUi = serializer.deserializeChatMessages<T>(node)
+        return AmazonQLspService.executeIfRunning(project) { server ->
+            serverAction(server, requestFromUi.params)
+        } ?: CompletableFuture.failedFuture<Unit>(IllegalStateException("LSP Server not running"))
     }
 }
