aws/aws-toolkit-jetbrains

commit 9109943ed134b4628d5358fb5dac52e6878bbe8a

fix(amazonq): fix LinkageError when starting AmazonQLspService (attempt 2) (#5627)
In certain IDE environments, for example when user has the latest Scala plugin, Amazon Q attempts to load lsp4j through
transitive optional dependencies instead of through the IDE.

Ideally we do not bundle the library, but this is not possible until JetBrains can guarantee stronger isolation between plugins

As part of this we optimistically exclude Gson in hopes that it does not have the same issue. SLF4J is additionally packaged due to the same linkage error, though it seems to be pulling in an older version from somewhere else.

We also need to bump the Kotlin version, since there is a bug where Kotlin compiler runs out of memory for some reason if we exclude Gson from the distribution bundle.

buildSrc/src/main/kotlin/toolkit-publishing-conventions.gradle.kts
@@ -54,7 +54,8 @@ configurations {
 
     // Make sure we exclude stuff we either A) ships with IDE, B) we don't use to cut down on size
     runtimeClasspath {
-        exclude(group = "org.slf4j")
+        exclude(group = "com.google.code.gson")
+        exclude(group = "org.slf4j", module = "slf4j-jdk14")
         exclude(group = "org.jetbrains.kotlin")
         exclude(group = "org.jetbrains.kotlinx")
     }

plugins/core/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
     implementation(project(":plugin-core:resources"))
     implementation(project(":plugin-core:sdk-codegen"))
     implementation(project(":plugin-core:webview"))
+    implementation(libs.slf4j.api)
 }
 
 tasks.check {

plugins/amazonq/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(project(":plugin-amazonq:codewhisperer"))
     implementation(project(":plugin-amazonq:mynah-ui"))
     implementation(project(":plugin-amazonq:shared"))
+    implementation(libs.lsp4j)
 
     testImplementation(project(":plugin-core"))
 }

plugins/toolkit/jetbrains-rider/build.gradle.kts
@@ -388,7 +388,7 @@ tasks.withType<DetektCreateBaselineTask>().configureEach {
 }
 
 configurations.all {
-    if (name.contains("detekt")) {
+    if (name.contains("detekt") || name.contains("kotlinCompiler") || name.contains("rdGen")) {
         return@all
     }
 

buildSrc/src/main/kotlin/software/aws/toolkits/gradle/changelog/ChangeLog.kt
@@ -39,7 +39,7 @@ enum class ChangeType(val sectionTitle: String) {
 
     class Serializer : StdSerializer<ChangeType>(ChangeType::class.java) {
         override fun serialize(value: ChangeType, gen: JsonGenerator?, provider: SerializerProvider?) {
-            gen?.writeString(value.name.toLowerCase())
+            gen?.writeString(value.name.lowercase())
         }
     }
 }

buildSrc/src/main/kotlin/software/aws/toolkits/gradle/changelog/tasks/NewChange.kt
@@ -65,7 +65,7 @@ open class NewChange : ChangeLogTask() {
         )
     }
 
-    private fun newFile(changeType: ChangeType) = nextReleaseDirectory.file("${changeType.name.toLowerCase()}-${UUID.randomUUID()}.json").get().asFile.apply {
+    private fun newFile(changeType: ChangeType) = nextReleaseDirectory.file("${changeType.name.lowercase()}-${UUID.randomUUID()}.json").get().asFile.apply {
         parentFile?.mkdirs()
         createNewFile()
     }

buildSrc/src/main/kotlin/toolkit-intellij-subplugin.gradle.kts
@@ -48,9 +48,11 @@ configurations {
 
         // Exclude dependencies that ship with iDE
         exclude(group = "org.slf4j")
-        // we want kotlinx-coroutines-debug and kotlinx-coroutines-test
-        exclude(group = "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm")
-        exclude(group = "org.jetbrains.kotlinx", "kotlinx-coroutines-core")
+        if (!name.startsWith("kotlinCompiler") && !name.startsWith("generateModels") && !name.startsWith("rdGen")) {
+            // we want kotlinx-coroutines-debug and kotlinx-coroutines-test
+            exclude(group = "org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm")
+            exclude(group = "org.jetbrains.kotlinx", "kotlinx-coroutines-core")
+        }
 
         resolutionStrategy.eachDependency {
             if (requested.group == "org.jetbrains.kotlinx" && requested.name.startsWith("kotlinx-coroutines")) {

gradle/libs.versions.toml
@@ -19,9 +19,10 @@ junit4 = "4.13.2"
 junit5 = "5.11.0"
 # https://plugins.jetbrains.com/docs/intellij/kotlin.html#adding-kotlin-support
 # https://kotlinlang.org/docs/releases.html#release-details
-kotlin = "2.0.0"
+kotlin = "2.1.20"
 # set in <root>/settings.gradle.kts
 kotlinCoroutines = "1.8.0"
+lsp4j = "0.24.0"
 mockito = "5.12.0"
 mockitoKotlin = "5.4.0"
 mockk = "1.13.17"
@@ -105,6 +106,7 @@ kotlin-coroutinesTest = { module = "org.jetbrains.kotlinx:kotlinx-coroutines-tes
 kotlin-reflect = { module = "org.jetbrains.kotlin:kotlin-reflect", version.ref = "kotlin" }
 kotlin-stdLibJdk8 = { module = "org.jetbrains.kotlin:kotlin-stdlib-jdk8", version.ref = "kotlin" }
 kotlin-test = { module = "org.jetbrains.kotlin:kotlin-test-junit", version.ref = "kotlin" }
+lsp4j = { module = "org.eclipse.lsp4j:org.eclipse.lsp4j", version.ref = "lsp4j" }
 mockito-core = { module = "org.mockito:mockito-core", version.ref = "mockito" }
 mockito-junit-jupiter = { module = "org.mockito:mockito-junit-jupiter", version.ref = "mockito" }
 mockito-kotlin = { module = "org.mockito.kotlin:mockito-kotlin", version.ref = "mockitoKotlin" }

commit ebf19a7d30f2351b7a4d68defd6cd6fc50aebcfd

build(amazonq): fix logging regression introduced by #5627

buildSrc/src/main/kotlin/toolkit-publishing-conventions.gradle.kts
@@ -55,7 +55,6 @@ configurations {
     // Make sure we exclude stuff we either A) ships with IDE, B) we don't use to cut down on size
     runtimeClasspath {
         exclude(group = "com.google.code.gson")
-        exclude(group = "org.slf4j", module = "slf4j-jdk14")
         exclude(group = "org.jetbrains.kotlin")
         exclude(group = "org.jetbrains.kotlinx")
     }

plugins/core/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation(project(":plugin-core:sdk-codegen"))
     implementation(project(":plugin-core:webview"))
     implementation(libs.slf4j.api)
+    implementation(libs.slf4j.jdk14)
 }
 
 tasks.check {
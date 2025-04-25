ebf19a7d30f2351b7a4d68defd6cd6fc50aebcfd
Richard Li <richali@amazon.com>
2025-04-25 15:27:22 -0700

    build(amazonq): fix logging regression introduced by #5627

--- plugins/core/build.gradle.kts
+++ plugins/core/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation(project(":plugin-core:sdk-codegen"))
     implementation(project(":plugin-core:webview"))
     implementation(libs.slf4j.api)
+    implementation(libs.slf4j.jdk14)
 }
 
 tasks.check {
