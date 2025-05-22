279a7fef593553397e8d3737719923f6897804d0
Richard Li <richali@amazon.com>
2025-05-22 16:35:54 -0700

    lint

--- plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/lsp/NodeExePatcher.kt
+++ plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/lsp/NodeExePatcher.kt
@@ -10,8 +10,8 @@ import java.nio.file.Path
 
 /**
  * Hacky nonsense to support old glibc platforms like AL2
- * @see https://github.com/microsoft/vscode/issues/231623
- * @see https://github.com/aws/aws-toolkit-vscode/commit/6081f890bdbb91fcd8b60c4cc0abb65b15d4a38d
+ * @see "https://github.com/microsoft/vscode/issues/231623"
+ * @see "https://github.com/aws/aws-toolkit-vscode/commit/6081f890bdbb91fcd8b60c4cc0abb65b15d4a38d"
  */
 object NodeExePatcher {
     const val GLIBC_LINKER_VAR = "VSCODE_SERVER_CUSTOM_GLIBC_LINKER"
@@ -22,14 +22,14 @@ object NodeExePatcher {
         val glibc = System.getenv(GLIBC_PATH_VAR)
         val nodePath = node.toAbsolutePath().toString()
 
-        if (!linker.isNullOrEmpty() && !glibc.isNullOrEmpty()) {
-            return GeneralCommandLine(linker)
+        return if (!linker.isNullOrEmpty() && !glibc.isNullOrEmpty()) {
+            GeneralCommandLine(linker)
                 .withParameters("--library-path", glibc, nodePath)
                 .also {
                     getLogger<NodeExePatcher>().info { "Using glibc patch: $it" }
                 }
         } else {
-            return GeneralCommandLine(nodePath)
+            GeneralCommandLine(nodePath)
         }
     }
 }
