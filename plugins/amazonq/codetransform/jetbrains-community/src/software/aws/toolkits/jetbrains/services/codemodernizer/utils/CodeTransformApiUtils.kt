3679de38d2d18eee327b615ec4a0a94a712023fb
David Hasani <davhasan@amazon.com>
2025-05-22 00:48:30 -0700

    handle re-auth

--- plugins/amazonq/codetransform/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/utils/CodeTransformApiUtils.kt
+++ plugins/amazonq/codetransform/jetbrains-community/src/software/aws/toolkits/jetbrains/services/codemodernizer/utils/CodeTransformApiUtils.kt
@@ -144,16 +144,10 @@ suspend fun JobId.pollTransformationStatusAndPlan(
                 refreshToken(project)
                 return@waitUntil state
             } catch (e: InvalidGrantException) {
-                CodeTransformMessageListener.instance.onCheckAuth()
+                CodeTransformMessageListener.instance.onReauthStarted()
                 notifyStickyWarn(
                     message("codemodernizer.notification.warn.expired_credentials.title"),
                     message("codemodernizer.notification.warn.expired_credentials.content"),
-                    project,
-                    listOf(
-                        NotificationAction.createSimpleExpiring(message("codemodernizer.notification.warn.action.reauthenticate")) {
-                            CodeTransformMessageListener.instance.onReauthStarted()
-                        }
-                    )
                 )
                 return@waitUntil state
             } finally {
