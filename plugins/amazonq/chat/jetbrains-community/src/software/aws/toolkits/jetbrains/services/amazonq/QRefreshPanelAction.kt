624ac0600b38beddcad1ed6927113e8b4e39d80a
samgst-amazon <samgst@amazon.com>
2025-06-05 16:23:56 -0700

    fix refresh breaking conversationClick for dumped tabs

--- plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/QRefreshPanelAction.kt
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/QRefreshPanelAction.kt
@@ -9,6 +9,9 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.util.messages.Topic
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.AmazonQLspService
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.flareChat.ChatCommunicationManager
+import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.CHAT_TAB_REMOVE
 import software.aws.toolkits.jetbrains.services.amazonq.toolwindow.AmazonQToolWindow
 import software.aws.toolkits.resources.AmazonQBundle
 import java.util.EventListener
@@ -16,6 +19,15 @@ import java.util.EventListener
 class QRefreshPanelAction : DumbAwareAction(AmazonQBundle.message("amazonq.refresh.panel"), null, AllIcons.Actions.Refresh) {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
+        
+        // Notify LSP server about all open tabs being removed
+        val chatManager = ChatCommunicationManager.getInstance(project)
+        chatManager.getAllTabIds().forEach { tabId ->
+            AmazonQLspService.executeIfRunning(project) { server ->
+                rawEndpoint.notify(CHAT_TAB_REMOVE, mapOf("tabId" to tabId))
+            }
+        }
+        
         // recreate chat browser
         AmazonQToolWindow.getInstance(project).disposeAndRecreate()
         // recreate signin browser
