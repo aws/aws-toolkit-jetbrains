62185609a94afbf66b6a4a3b5bb265a63f414b60
Yaofu Zuo <zuoyaofu@amazon.com>
2025-07-08 10:39:47 -0700

    fix ci

--- plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/lsp/AmazonQLanguageClientImpl.kt
+++ plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/lsp/AmazonQLanguageClientImpl.kt
@@ -152,7 +152,7 @@ class AmazonQLanguageClientImpl(private val project: Project) : AmazonQLanguageC
             MessageType.Error -> Level.ERROR
             MessageType.Warning -> Level.WARN
             MessageType.Info, MessageType.Log -> Level.INFO
-            else -> Level.Warn
+            else -> Level.WARN
         }
 
         if (type == Level.ERROR &&
