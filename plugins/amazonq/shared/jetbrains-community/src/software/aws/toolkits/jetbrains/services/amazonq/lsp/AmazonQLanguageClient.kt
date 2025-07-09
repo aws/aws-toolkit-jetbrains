161bf136d5f13d1f540faab6d0995242199cf4c4
Sam Stewart <samgst@amazon.com>
2025-07-09 15:47:06 -0700

    Revert "feat(amazonq): add image context support (#5846)" (#5890)
    This reverts commit 83ac4f51295d77532873b2543c115fd1dd9084ba.

--- plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/lsp/AmazonQLanguageClient.kt
+++ plugins/amazonq/shared/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/lsp/AmazonQLanguageClient.kt
@@ -25,9 +25,7 @@ import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.GET_S
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.GetSerializedChatResult
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.OPEN_FILE_DIFF
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.OpenFileDiffParams
-import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.SHOW_OPEN_FILE_DIALOG_REQUEST_METHOD
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.SHOW_SAVE_FILE_DIALOG_REQUEST_METHOD
-import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.ShowOpenFileDialogParams
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.ShowSaveFileDialogParams
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.chat.ShowSaveFileDialogResult
 import software.aws.toolkits.jetbrains.services.amazonq.lsp.model.aws.credentials.ConnectionMetadata
@@ -46,9 +44,6 @@ interface AmazonQLanguageClient : LanguageClient {
 
     @JsonRequest(SHOW_SAVE_FILE_DIALOG_REQUEST_METHOD)
     fun showSaveFileDialog(params: ShowSaveFileDialogParams): CompletableFuture<ShowSaveFileDialogResult>
-
-    @JsonRequest(SHOW_OPEN_FILE_DIALOG_REQUEST_METHOD)
-    fun showOpenFileDialog(params: ShowOpenFileDialogParams): CompletableFuture<LSPAny>
 
     @JsonRequest(GET_SERIALIZED_CHAT_REQUEST_METHOD)
     fun getSerializedChat(params: LSPAny): CompletableFuture<GetSerializedChatResult>
