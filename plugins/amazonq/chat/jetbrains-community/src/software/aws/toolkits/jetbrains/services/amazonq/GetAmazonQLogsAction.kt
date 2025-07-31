244cffa7f40251e0f220c06f9635073d54e81073
Manodnya Bhoite <manodnyb@amazon.com>
2025-07-31 16:33:07 -0700

    changed tooltip, made icon brighter in light mode

--- plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/GetAmazonQLogsAction.kt
+++ plugins/amazonq/chat/jetbrains-community/src/software/aws/toolkits/jetbrains/services/amazonq/GetAmazonQLogsAction.kt
@@ -12,6 +12,10 @@ import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.util.IconLoader
+import com.intellij.ui.ColorUtil
+import com.intellij.ui.JBColor
+import com.intellij.util.IconUtil
+import com.intellij.util.ui.UIUtil
 import kotlinx.coroutines.runBlocking
 import software.aws.toolkits.jetbrains.utils.notifyInfo
 import software.aws.toolkits.jetbrains.utils.runUnderProgressIfNeeded
@@ -19,10 +23,18 @@ import software.aws.toolkits.resources.AmazonQBundle
 import software.aws.toolkits.resources.AwsCoreBundle
 
 class GetAmazonQLogsAction : DumbAwareAction(
-    AmazonQBundle.message("amazonq.getLogs"),
-    null,
-    IconLoader.getIcon("/META-INF/file.svg", GetAmazonQLogsAction::class.java)
+    AmazonQBundle.message("amazonq.getLogs.tooltip.text")
 ) {
+
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+        val baseIcon = IconLoader.getIcon("/icons/file.svg", GetAmazonQLogsAction::class.java)
+        e.presentation.icon = if (!JBColor.isBright()) {
+            baseIcon
+        } else {
+            IconUtil.colorize(baseIcon, ColorUtil.brighter(UIUtil.getLabelForeground(), 2))
+        }
+    }
 
     override fun getActionUpdateThread() = ActionUpdateThread.BGT
     override fun actionPerformed(e: AnActionEvent) {
