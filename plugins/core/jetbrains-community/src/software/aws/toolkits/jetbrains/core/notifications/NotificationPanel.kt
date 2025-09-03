d0c9049a45aba76c7cf7d5287b68b5f7b51762dd
Richard Li <742829+rli@users.noreply.github.com>
2025-09-03 16:42:55 -0700

    merge: support chat on remote connections (#6008)
    * feat(amazonq): Add support for Amazon Q chat on remote 242+ (#4825)
    
    * fix(amazonq): Bug Fix for "Q: Send to Prompt" doesn't work in split panes on local or remote (#5463)
    
    * fix(amazonq): use https as scheme for local webview resource resolver (#5489)
    
    https://developer.mozilla.org/en-US/docs/Web/API/Clipboard
    `navigator.clipboard` is only accessible from a "secure context", which JCEF does not think we are using,
    because the URI authority is not localhost or https. Since we are hooking onto the CEF resource request handler,
    it does not matter what we use for the scheme/authority as long as there is no conflict with real resources.
    
    Previous implementation used a different load mechanism which somehow met the secure context criteria.
    
    * fix(amazonq): fix agentic asset loading in remote mode (#5734)
    
    local uris cannot be loaded if the host is deemed non-remote
    
    * fix(amazonq): fix code issues tab not appearing in remote (#5737)
    
    on remote, each client gets its own instance of the tool window.
    
    logic needs to handle this case, otherwise it will only make changes against the 'main' IDE,
    and will not be visible to remote clients
    
    * fix(amazonq): use progress bar decorator while loading q chat on remote (#5749)
    
    animated icons do not show up on remote
    
    * fix: provide loading text when chat assets are slow to load (#5973)
    
    on remote, it can take 10+ seconds for chat to be visible.
    
    additionally, delete some unneeded assets to slightly reduce load time
    
    * feat(amazonq): limit remote to amzn-internal compute (#5993)
    
    Per internal discussion, feature deemed not ready for external users due to https://youtrack.jetbrains.com/issue/IJPL-203169
    
    * fix: delete changelog for internal feature (#6009)
    
    * feat: show message to resize on remote chat (#6007)
    
    ---------
    Co-authored-by: Lokesh <lodogga@amazon.com>

--- plugins/core/jetbrains-community/src/software/aws/toolkits/jetbrains/core/notifications/NotificationPanel.kt
+++ plugins/core/jetbrains-community/src/software/aws/toolkits/jetbrains/core/notifications/NotificationPanel.kt
@@ -3,12 +3,10 @@
 
 package software.aws.toolkits.jetbrains.core.notifications
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.application.runInEdt
 import com.intellij.ui.EditorNotificationPanel
 import com.intellij.ui.components.panels.Wrapper
 import com.intellij.util.ui.components.BorderLayoutPanel
-import software.aws.toolkits.resources.AwsCoreBundle
 
 class NotificationPanel : BorderLayoutPanel() {
     private val wrapper = Wrapper()
@@ -23,15 +21,21 @@ class NotificationPanel : BorderLayoutPanel() {
     private fun removeNotificationPanel(notificationId: String) = runInEdt {
         BannerNotificationService.getInstance().removeNotification(notificationId)
         NotificationDismissalState.getInstance().dismissNotification(notificationId)
-        wrapper.removeAll()
+        wrapper.setContent(null)
     }
 
     fun updateNotificationPanel(bannerContent: BannerContent) {
-        val panel = EditorNotificationPanel()
+        val panel = EditorNotificationPanel(
+            when (bannerContent.severity) {
+                NotificationSeverity.CRITICAL -> EditorNotificationPanel.Status.Error
+                NotificationSeverity.WARNING -> EditorNotificationPanel.Status.Warning
+                NotificationSeverity.INFO -> EditorNotificationPanel.Status.Info
+            }
+        )
         panel.text = bannerContent.title
-        panel.icon(AllIcons.General.Error)
+
         val panelWithActions = NotificationManager.buildBannerPanel(panel, bannerContent.actions)
-        panelWithActions.createActionLabel(AwsCoreBundle.message("general.dismiss")) {
+        panelWithActions.setCloseAction {
             removeNotificationPanel(bannerContent.id)
         }
 
