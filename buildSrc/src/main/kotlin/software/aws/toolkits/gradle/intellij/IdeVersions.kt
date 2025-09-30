f3a7ff393ea173f39db3a6b44ad92296c43443cd
Aseem Sharma <aseemxs@amazon.com>
2025-09-30 16:02:12 -0700

    Add 2025.3 builds
    Add the 2025.3 version to the release pipeline. https://blog.jetbrains.com/idea/2025/09/intellij-idea-2025-3-eap/

--- buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
+++ buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
@@ -152,6 +152,50 @@ object IdeVersions {
                 rdGenVersion = "2025.2.2",
                 nugetVersion = "2025.2.0"
             )
+        ),
+        Profile(
+            name = "2025.3",
+            gateway = ProductProfile(
+                sdkVersion = "2025.3",
+                bundledPlugins = listOf("org.jetbrains.plugins.terminal")
+            ),
+            community = ProductProfile(
+                sdkVersion = "2025.3",
+                bundledPlugins = commonPlugins + listOf(
+                    "com.intellij.java",
+                    "com.intellij.gradle",
+                    "org.jetbrains.idea.maven",
+                    "com.jetbrains.codeWithMe",
+                    "com.intellij.properties"
+                ),
+                marketplacePlugins = listOf(
+                    "org.toml.lang:253.22441.34",
+                    "PythonCore:253.22441.33",
+                    "Docker:253.22441.33",
+                    "com.intellij.modules.json:253.520.16"
+                )
+            ),
+            ultimate = ProductProfile(
+                sdkVersion = "2025.3",
+                bundledPlugins = commonPlugins + listOf(
+                    "JavaScript",
+                    "JavaScriptDebugger",
+                    "com.intellij.database",
+                    "com.jetbrains.codeWithMe",
+                ),
+                marketplacePlugins = listOf(
+                    "Pythonid:253.22441.33",
+                    "org.jetbrains.plugins.go:253.22441.33",
+                    "com.intellij.modules.json:253.520.16"
+                )
+            ),
+            rider = RiderProfile(
+                sdkVersion = "2025.3-SNAPSHOT",
+                bundledPlugins = commonPlugins,
+                netFrameworkTarget = "net472",
+                rdGenVersion = "2025.3.0",
+                nugetVersion = "2025.3.0-eap03"
+            )
         )
     ).associateBy { it.name }
 
