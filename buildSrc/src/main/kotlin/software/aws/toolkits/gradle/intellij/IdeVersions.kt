73778bcf107aa315845e569265f4d8c31ccf8422
Aseem Sharma <aseemxs@amazon.com>
2025-10-14 14:27:17 -0700

    Use correct 2025.3 EAP SDK version format
    - Change to '253.25908-EAP-CANDIDATE-SNAPSHOT' from JetBrains snapshots repo
    - Based on senior engineer feedback and official snapshot repository
    - Should resolve IntelliJ Platform dependency resolution

--- buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
+++ buildSrc/src/main/kotlin/software/aws/toolkits/gradle/intellij/IdeVersions.kt
@@ -156,11 +156,11 @@ object IdeVersions {
         Profile(
             name = "2025.3",
             gateway = ProductProfile(
-                sdkVersion = "253.25908.13",
+                sdkVersion = "253.25908-EAP-CANDIDATE-SNAPSHOT",
                 bundledPlugins = listOf("org.jetbrains.plugins.terminal")
             ),
             community = ProductProfile(
-                sdkVersion = "253.25908.13",
+                sdkVersion = "253.25908-EAP-CANDIDATE-SNAPSHOT",
                 bundledPlugins = commonPlugins + listOf(
                     "com.intellij.java",
                     "com.intellij.gradle",
@@ -176,7 +176,7 @@ object IdeVersions {
                 )
             ),
             ultimate = ProductProfile(
-                sdkVersion = "253.25908.13",
+                sdkVersion = "253.25908-EAP-CANDIDATE-SNAPSHOT",
                 bundledPlugins = commonPlugins + listOf(
                     "JavaScript",
                     "JavaScriptDebugger",
