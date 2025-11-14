7928093e83d71ced988aedf158c9e8f4bd97fb3c
Aseem Sharma <aseemxs@amazon.com>
2025-11-13 21:58:30 -0800

    Fix rd.platform dependency for IDE versions < 2025.2
    The intellij.rd.platform bundled module only exists in IDE versions
    2025.2 and later. This change makes the dependency conditional to
    prevent build failures on earlier IDE versions (2024.3, 2025.1).

--- plugins/amazonq/shared/jetbrains-ultimate/build.gradle.kts
+++ plugins/amazonq/shared/jetbrains-ultimate/build.gradle.kts
@@ -13,7 +13,12 @@ intellijToolkit {
 
 dependencies {
     intellijPlatform {
-        bundledModule("intellij.rd.platform")
+        // RD platform is only available in 2025.3 and later
+        when (providers.gradleProperty("ideProfileName").get()) {
+            "2025.2","2025.3" -> {
+                bundledModule("intellij.rd.platform")
+            }
+        }
     }
     compileOnly(project(":plugin-amazonq:shared:jetbrains-community"))
     compileOnly(project(":plugin-core:jetbrains-ultimate"))
