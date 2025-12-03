c2f240db80393d945352f015579116ad3dbb62bb
Sherry Lu <xiaoluk@amazon.com>
2025-12-02 22:30:52 -0800

    feat: add separate generating Changelog for Toolkit

---
+++ buildSrc/src/main/kotlin/software/aws/toolkits/gradle/changelog/tasks/ToolkitChangeLogTask.kt
@@ -0,0 +1,31 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.gradle.changelog.tasks
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileTree
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.Internal
+import software.aws.toolkits.gradle.changelog.GitStager
+
+abstract class ToolkitChangeLogTask : DefaultTask() {
+    @Internal
+    protected val git = GitStager.create(project.rootDir)
+
+    @InputDirectory
+    val changesDirectory: DirectoryProperty = project.objects.directoryProperty().convention(project.layout.projectDirectory.dir(".changes/toolkit"))
+
+    @InputFiles
+    val nextReleaseDirectory: DirectoryProperty = project.objects.directoryProperty().convention(project.layout.projectDirectory.dir(".changes/toolkit/next-release"))
+
+    init {
+        group = "changelog"
+    }
+
+    protected fun DirectoryProperty.jsonFiles(): FileTree = this.asFileTree.matching {
+        include("*.json")
+    }
+}
