3f8d801e9a18c047566ddd7ed2fe98b56fe64232
Zeeshan Ahmed <zeemed@amazon.com>
2026-02-16 12:15:47 -0500

    ensure file is opened for lsp registration and use URI

--- plugins/toolkit/jetbrains-core/src-253+/software/aws/toolkits/jetbrains/services/cfnlsp/CfnClientService.kt
+++ plugins/toolkit/jetbrains-core/src-253+/software/aws/toolkits/jetbrains/services/cfnlsp/CfnClientService.kt
@@ -5,10 +5,15 @@ package software.aws.toolkits.jetbrains.services.cfnlsp
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServer
 import com.intellij.platform.lsp.api.LspServerManager
 import org.eclipse.lsp4j.DidChangeConfigurationParams
+import org.eclipse.lsp4j.DidOpenTextDocumentParams
+import org.eclipse.lsp4j.TextDocumentItem
 import software.aws.toolkits.jetbrains.services.cfnlsp.protocol.CreateStackActionResult
 import software.aws.toolkits.jetbrains.services.cfnlsp.protocol.CreateValidationParams
 import software.aws.toolkits.jetbrains.services.cfnlsp.protocol.DescribeValidationStatusResult
@@ -79,6 +84,23 @@ internal class CfnClientService(project: Project) {
 
     fun getResourceState(params: ResourceStateParams): CompletableFuture<ResourceStateResult?> =
         sendRequest { it.getResourceState(params) }
+
+    fun ensureDocumentOpen(file: VirtualFile, project: Project) {
+        val isOpenInEditor = FileEditorManager.getInstance(project).isFileOpen(file)
+        if (isOpenInEditor) return
+
+        val server = lspServerProvider() ?: return
+        val descriptor = server.descriptor
+        val uri = descriptor.getFileUri(file)
+        val languageId = descriptor.getLanguageId(file)
+        val content = FileDocumentManager.getInstance().getDocument(file)?.text ?: return
+
+        server.sendNotification { lsp ->
+            lsp.textDocumentService.didOpen(
+                DidOpenTextDocumentParams(TextDocumentItem(uri, languageId, 0, content))
+            )
+        }
+    }
 
     fun notifyConfigurationChanged() {
         lspServerProvider()?.sendNotification { lsp ->
--- plugins/toolkit/jetbrains-core/src-253+/software/aws/toolkits/jetbrains/services/cfnlsp/stacks/ValidateAndDeployAction.kt
+++ plugins/toolkit/jetbrains-core/src-253+/software/aws/toolkits/jetbrains/services/cfnlsp/stacks/ValidateAndDeployAction.kt
@@ -9,9 +9,11 @@ import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.vfs.VfsUtil
 import software.aws.toolkits.jetbrains.core.explorer.ExplorerTreeToolWindowDataKeys
+import software.aws.toolkits.jetbrains.services.cfnlsp.CfnClientService
 import software.aws.toolkits.jetbrains.services.cfnlsp.explorer.nodes.StackNode
 import software.aws.toolkits.jetbrains.services.cfnlsp.protocol.CreateValidationParams
 import software.aws.toolkits.jetbrains.services.cfnlsp.server.CFN_SUPPORTED_EXTENSIONS
+import software.aws.toolkits.jetbrains.services.cfnlsp.server.CfnLspServerDescriptor
 import software.aws.toolkits.jetbrains.services.cfnlsp.ui.ValidateAndDeployDialog
 import java.io.File
 import java.util.UUID
@@ -40,9 +42,13 @@ internal class ValidateAndDeployAction : AnAction() {
         val settings = dialog.getSettings()
         val templateVFile = VfsUtil.findFileByIoFile(File(settings.templatePath), true) ?: return
 
+        val clientService = CfnClientService.getInstance(project)
+        clientService.ensureDocumentOpen(templateVFile, project)
+
+        val descriptor = CfnLspServerDescriptor.getInstance(project)
         val params = CreateValidationParams(
             id = UUID.randomUUID().toString(),
-            uri = templateVFile.url,
+            uri = descriptor.getFileUri(templateVFile),
             stackName = settings.stackName,
             keepChangeSet = true,
         )
