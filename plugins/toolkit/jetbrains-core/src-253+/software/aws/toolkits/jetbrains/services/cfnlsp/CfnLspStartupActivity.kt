ba2ba04b903cf12904fdd6f037f127a6c21af67d
Christopher-Neil Mendoza <chrisqm@amazon.com>
2026-03-11 13:00:38 -0400

    fix(cloudformation): Start CloudFormation LSP upon toolkit activation (#6293)

---
+++ plugins/toolkit/jetbrains-core/src-253+/software/aws/toolkits/jetbrains/services/cfnlsp/CfnLspStartupActivity.kt
@@ -0,0 +1,19 @@
+// Copyright 2026 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+package software.aws.toolkits.jetbrains.services.cfnlsp
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import com.intellij.platform.lsp.api.LspServerManager
+import software.aws.toolkits.jetbrains.services.cfnlsp.server.CfnLspServerDescriptor
+import software.aws.toolkits.jetbrains.services.cfnlsp.server.CfnLspServerSupportProvider
+
+internal class CfnLspStartupActivity : StartupActivity {
+    override fun runActivity(project: Project) {
+        LspServerManager.getInstance(project).ensureServerStarted(
+            CfnLspServerSupportProvider::class.java,
+            CfnLspServerDescriptor.getInstance(project)
+        )
+    }
+}
