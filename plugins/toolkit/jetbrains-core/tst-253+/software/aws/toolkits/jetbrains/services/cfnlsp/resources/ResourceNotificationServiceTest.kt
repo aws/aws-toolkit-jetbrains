0914d2de84a60721c9d86f2b2fde07b21f2761ac
Gema Mercado <gemamrcd@amazon.com>
2026-07-09 19:34:22 +0000

    fix(cloudformation): add license header to notification service test

--- plugins/toolkit/jetbrains-core/tst-253+/software/aws/toolkits/jetbrains/services/cfnlsp/resources/ResourceNotificationServiceTest.kt
+++ plugins/toolkit/jetbrains-core/tst-253+/software/aws/toolkits/jetbrains/services/cfnlsp/resources/ResourceNotificationServiceTest.kt
@@ -1,3 +1,6 @@
+// Copyright 2025 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 package software.aws.toolkits.jetbrains.services.cfnlsp.resources
 
 import com.intellij.notification.Notification
