a986c9738489546788f8ddea329b8d91f4874a72
Brandon Rinderle <branrin@amazon.com>
2026-07-23 13:37:26 +0000

    build: drop support for 2025.1 and 2025.2

--- kotlinResolution.settings.gradle.kts
+++ kotlinResolution.settings.gradle.kts
@@ -8,11 +8,7 @@ dependencyResolutionManagement {
 
             // pull value from IJ library list: https://github.com/JetBrains/intellij-community/blob/<mv>/.idea/libraries/kotlinx_coroutines_core.xml
             val coroutinesVersion = when (ideProfileName) {
-                "2025.1" -> {
-                    "1.8.0-intellij-11"
-                }
-
-                "2025.2", "2025.3" -> {
+                "2025.3" -> {
                     "1.10.1-intellij-5"
                 }
 
@@ -28,8 +24,6 @@ dependencyResolutionManagement {
             // https://plugins.jetbrains.com/docs/intellij/using-kotlin.html#bundled-stdlib-versions
             // used to align non-compiler kotlin dependencies (stdlib, reflect) with the IDE's bundled version
             val kotlinStdlibVersion = when (ideProfileName) {
-                "2025.1" -> "2.1.10"
-                "2025.2" -> "2.1.20"
                 "2025.3" -> "2.2.20"
                 "2026.1" -> "2.3.20"
                 "2026.2" -> "2.4.0"
